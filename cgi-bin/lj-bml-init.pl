5a28047e6f831a24211425ea15cc9d91615989f0
Brad Fitzpatrick <brad@danga.com>
2001-06-10 21:52:31 +0000

    initial import
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/lj-bml-init.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#
+
+require 'ljconfig.pl';
+
+&BML::register_block("DOMAIN", "S", $LJ::DOMAIN);
+&BML::register_block("IMGPREFIX", "S", $LJ::IMGPREFIX);
+&BML::register_block("SITEROOT", "S", $LJ::SITEROOT);
+&BML::register_block("SITENAME", "S", $LJ::SITENAME);
+&BML::register_block("ADMIN_EMAIL", "S", $LJ::ADMIN_EMAIL);
+&BML::register_block("SUPPORT_EMAIL", "S", $LJ::SUPPORT_EMAIL);
+
+{
+    my $dl = "<a href=\"$LJ::SITEROOT/files/%%DATA%%\">HTTP</a>";
+    if ($LJ::FTPPREFIX) {
+	$dl .= " - <a href=\"$LJ::FTPPREFIX/%%DATA%%\">FTP</a>";
+    }
+    &BML::register_block("DL", "DS", $dl);
+}
+
+# pre-load common libraries so we don't have to load them in BML files (slow)
+package BMLCodeBlock;
+require 'ljlib.pl';
+require 'ljprotocol.pl';
+require 'cleanhtml.pl';
+require 'emailcheck.pl';
+require 'portal.pl';
+
+1;
