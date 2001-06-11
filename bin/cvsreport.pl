784c0011506fd2b0a6d1a68adc41d4c9ac25debd
Brad Fitzpatrick <brad@danga.com>
2001-06-11 22:31:51 +0000

    initial import
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ bin/cvsreport.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+#
+
+unless (-d $ENV{'LJHOME'}) { 
+    die "$LJHOME not set.\n";
+}
+
+my $maind = $ENV{'LJHOME'};
+my $cvs = "$maind/cvs/livejournal";
+my $cvslocal = "$maind/cvs/local";
+my @toplevel = qw(htdocs cgi-bin bin doc);
+
+my %status = ();   # $relfile -> $status
+
+# checks if files aren't in CVS, are newer than CVS, or if CVS is newer
+scan_main();
+
+# checks to see if new CVS files aren't in the main tree yet
+scan_cvs();
+
+foreach my $file (sort keys %status)
+{
+    printf "%-20s %s\n", $status{$file}, $file;
+}
+
+sub scan_cvs
+{
+    foreach my $repo ("cvs", "local")
+    {
+	my $cdir = $repo eq "cvs" ? $cvs : $cvslocal;
+	my @dirs = @toplevel;
+	while (@dirs)
+	{
+	    my $dir = shift @dirs;
+	    my $fulldir = "$cdir/$dir";
+	    next unless (-e $fulldir);
+	    opendir (MD, $fulldir) or die "Can't open $fulldir.";
+	    while (my $file = readdir(MD)) {
+		next if ($file =~ /CVS/);
+		if (-d "$fulldir/$file") {
+		    unless ($file eq "." || $file eq "..") {
+			unshift @dirs, "$dir/$file";
+		    }
+		} elsif (-f "$fulldir/$file") {
+		    my $relfile = "$dir/$file";
+		    
+		    my $mtime = mtime("$maind/$relfile");
+		    my $ctime = mtime("$cdir/$relfile");
+		    
+		    my $status = "";
+		    if (! $mtime && $ctime) {
+			$status = "main <- $repo";
+		    } 
+
+		    $status{$relfile} = $status if ($status);
+		    
+		} else {
+		    print "WHAT IS THIS? $dir/$file\n";
+		}
+	    }
+	    close MD;
+	}
+    }
+}
+
+sub scan_main
+{
+    my @dirs = @toplevel;
+    while (@dirs)
+    {
+	my $dir = shift @dirs;
+	my $fulldir = "$maind/$dir";
+	opendir (MD, $fulldir) or die "Can't open $fulldir.";
+	while (my $file = readdir(MD)) {
+	    if (-d "$fulldir/$file") {
+		unless ($file eq "." || $file eq "..") {
+		    unshift @dirs, "$dir/$file";
+		}
+	    } elsif (-f "$fulldir/$file") {
+		my $relfile = "$dir/$file";
+
+		my $mtime = mtime("$maind/$relfile");
+		my $ctime = mtime("$cvs/$relfile");
+		my $ltime = mtime("$cvslocal/$relfile");
+
+		my $status = "";
+		if ($mtime && ! $ctime && ! $ltime) {
+		    $status = "main -> ??";
+		} elsif ($mtime > $ctime && $mtime > $ltime) {
+		    if ($ltime) {
+			$status = "main -> local";
+		    } else {
+			$status = "main -> cvs";
+		    }
+		} elsif (! $ltime && $ctime > $mtime) {
+		    $status = "main <- cvs";
+		} elsif ($ltime && $ltime > $ctime) {
+		    $status = "main <- local";
+		}
+
+		$status{$relfile} = $status if ($status);
+		
+	    } else {
+		print "WHAT IS THIS? $dir/$file\n";
+	    }
+	}
+        close MD;
+    }
+
+}
+
+sub mtime
+{
+    my $file = shift;
+    return (stat($file))[9];
+}
