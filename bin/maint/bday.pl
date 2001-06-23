e3094e05efac49a4df6b4b627c81279058d6adaa
Brad Fitzpatrick <brad@danga.com>
2001-06-23 03:16:17 +0000

    Total rewrite.  Privacy bug fixed.  New setting for users if they want bday reminders.  Won't bug communities and inactive users now, too.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@99 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/maint/bday.pl
+++ bin/maint/bday.pl
@@ -1,65 +1,83 @@
 #!/usr/bin/perl
 #
 
+use strict;
+use vars qw(%maint);
+
 $maint{'bdaymail'} = sub
 {
     &connect_db();
-    $sth = $dbh->prepare("SELECT userid, user, name, email, allow_contactshow, timeupdate FROM user WHERE bdate IS NOT NULL AND status='A' AND statusvis='V' AND bdate <> '0000-00-00' AND MONTH(NOW())=MONTH(bdate) AND DAYOFMONTH(NOW())=DAYOFMONTH(bdate) AND timeupdate > DATE_SUB(NOW(), INTERVAL 1 MONTH)");
-    $sth->execute;
+    my $dbh = LJ::get_dbh("slave");  # all read-only operations
+    my $sth;
     
-    %bday = ();
-    my $bdayppl = "";
-    while (my ($userid, $user, $name, $email, $allow_contactshow) = $sth->fetchrow_array)
+    # get everybody whose birthday is today.
+    $sth = $dbh->prepare("SELECT userid, user, name, email ".
+			 "FROM user WHERE bdate IS NOT NULL AND status='A' AND ".
+			 "statusvis='V' AND bdate <> '0000-00-00' AND ".
+			 "MONTH(NOW())=MONTH(bdate) AND DAYOFMONTH(NOW())=DAYOFMONTH(bdate) ".
+			 "AND timeupdate > DATE_SUB(NOW(), INTERVAL 1 MONTH) AND ".
+			 "journaltype='P'");
+    $sth->execute;
+    my @bdays; push @bdays, $_ while ($_ = $sth->fetchrow_hashref);
+    $sth->finish;
+
+    # go through each birthday person and tell them happy birthday.
+    foreach my $bu (@bdays)
     {
+	my ($user, $userid, $name, $email) = map { $bu->{$_} } qw(user userid name email);
 	print "$user ($userid) .. $name .. $email\n";
-	open (MAIL, "|$LJ::SENDMAIL");
-	print MAIL "From: webmaster\@livejournal.com (LiveJournal.com)\n";
-	print MAIL "To: $email ($name)\n";
-	print MAIL "Subject: Happy Birthday!\n\n";
-	print MAIL "Happy Birthday $name!!\n\n";
-	print MAIL "According to our records, today is your birthday... everybody here at LiveJournal.com would like to wish you a happy birthday!\n\n";
-	print MAIL "If you got any interesting birthday stories to share, do let us know!  Or better, email them to us and also update your LiveJournal with them.  :)  And if you have any questions/comments about the LiveJournal service in general, let us know too... we're real people, not a huge corporation, so we read and reply to all email.\n\n";
-	print MAIL "Anyway... the point of this email was originally just HAPPY BIRTHDAY!\n\n";
-	print MAIL "\nSincerely,\nLiveJournal.com Team\n\n--\nLiveJournal\nhttp://www.livejournal.com/\n";
-	close MAIL;
-	$bday{$user} = { 'user' => $user, 
-			 'name' => $name, 
-			 'email' => $email, 
-			 'contactshow' => $allow_contactshow };
-	$bdayppl .= "$userid,";
-    }
 
-    chop $bdayppl;
-    my %friends = ();
-    print $bdayppl, "\n";
-    $sth = $dbh->prepare("SELECT uu.user AS 'user', uf.user AS 'friend' FROM friends f, user uu, user uf WHERE f.userid=uu.userid AND uf.userid=f.friendid AND f.friendid IN ($bdayppl)");
-    $sth->execute;
-    while (($user, $friend) = $sth->fetchrow_array)
-    {
-	print "$user -> $friend\n";
-	push @{$friends{$user}}, $friend;
-    }
+	&LJ::send_mail({
+	    'to' => $email,
+	    'toname' => $name,
+	    'subject' => "Happy Birthday!",
+	    'from' => $LJ::ADMIN_EMAIL,
+	    'fromname' => $LJ::SITENAME,
+	    'body' => ("Happy Birthday $name!!\n\n".
+		       "According to our records, today is your birthday... everybody here ".
+		       "at $LJ::SITENAME would like to wish you a happy birthday!\n\n".
+		       "If you have any interesting birthday stories to share, do let us know!  ".
+		       "Or better, email them to us and also update your LiveJournal with them.  ".
+		       ":)  And if you have any questions/comments about the service in general, ".
+		       "let us know too... we're real people, not a huge corporation, so we read ".
+		       "and try to reply to all email.\n\n".
+		       "Anyway... the point of this email was originally just HAPPY BIRTHDAY!\n\n".
+		       "--\n$LJ::SITENAME\n$LJ::SITEROOT/\n"),
+	});
 
-    $friendppl = join(", ", map { $dbh->quote($_) } keys %friends);
-    $sth = $dbh->prepare("SELECT user, name, email FROM user WHERE status='A' and statusvis='V' AND user IN ($friendppl)");
-    $sth->execute;
-    while (($user, $name, $email) = $sth->fetchrow_array)
-    {
-	foreach $friend (grep { $user ne $_  } @{$friends{$user}})
+	# and now, tell people that list them as friends.
+	$sth = $dbh->prepare("SELECT u.user, u.name, u.email ".
+			     "FROM user u, friends f, userprop up, userproplist upl ".
+			     "WHERE f.friendid=$userid AND f.userid=u.userid AND ".
+			     "up.userid=u.userid AND upl.upropid=up.upropid AND ".
+			     "upl.name='opt_bdaymail' AND up.value='1' AND ".
+			     "u.journaltype='P' AND u.status='A' AND u.statusvis='V' AND ".
+			     "u.timeupdate>DATE_SUB(NOW(), INTERVAL 1 MONTH) AND ".
+			     "u.userid <> $userid");
+	$sth->execute;
+	if ($dbh->err) { die $dbh->errstr; }
+	my @friendof; push @friendof, $_ while ($_ = $sth->fetchrow_hashref);
+
+	# possesive es
+	my $s = ($name =~ /s$/i) ? "'" : "'s";
+
+	foreach my $fu (@friendof)
 	{
-	    print "Mail $user about $friend...\n";
-	    open (MAIL, "|$LJ::SENDMAIL");
-	    print MAIL "From: webmaster\@livejournal.com (LiveJournal.com)\n";
-	    print MAIL "To: $email ($name)\n";
-	    print MAIL "Subject: Birthday Reminder!\n\n";
-	    print MAIL "$name,\n\n";
-	    $s = $bday{$friend}->{'name'} =~ /s$/i ? "'" : "'s";
-	    print MAIL "This is a reminder that today is $bday{$friend}->{'name'}$s birthday (LiveJournal user: $friend).  You have $bday{$friend}->{'name'} listed as a friend in your LiveJournal, so we thought this reminder would be useful.\n";
-	    if ($bday{$friend}->{'contactshow'} eq "Y") {
-		print MAIL "\nIf you'd like to mail this person and wish them a happy birthday, their email address is:\n\n       $bday{$friend}->{'email'}\n\n";
-	    }
-	    print MAIL "\nSincerely,\nLiveJournal.com Team\n\n--\nLiveJournal\nhttp://www.livejournal.com/\n";
-	    close MAIL;
+	    my ($fuser, $fname, $femail) = map { $fu->{$_} } qw(user name email);	    
+	    print "  mail $fuser about $user...\n";
+
+	    &LJ::send_mail({
+		'to' => $femail,
+		'toname' => $fname,
+		'subject' => "Birthday Reminder!",
+		'from' => $LJ::ADMIN_EMAIL,
+		'fromname' => $LJ::SITENAME,
+		'body' => ("This is a reminder that today is $name$s birthday ".
+			   "(LiveJournal user: $user).  You have $name listed as ".
+			   "a friend in your LiveJournal, so we thought this ".
+			   "reminder would be useful.".
+			   "\n\n--\n$LJ::SITENAME\n$LJ::SITEROOT/\n"),
+	    });
 	}
     }
 
