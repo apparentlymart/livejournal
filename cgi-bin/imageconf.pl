c682178934c2d669b0e07f251285d8b88a72f52e
Brad Fitzpatrick <brad@danga.com>
2001-07-21 22:44:19 +0000

    Fix dimensions.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@197 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/imageconf.pl
+++ cgi-bin/imageconf.pl
@@ -6,20 +6,20 @@ package LJ::Img;
 use vars qw(%img);
 
 $img{'btn_up'} = { 'src' => '/btn_up.gif',
-		   'width' => 20,
-		   'height' => 22,
+		   'width' => 22,
+		   'height' => 20,
 		   'alt' => 'Up',
 	       };
 
 $img{'btn_down'} = { 'src' => '/btn_dn.gif',
-		     'width' => 20,
-		     'height' => 22,
+		     'width' => 22,
+		     'height' => 20,
 		     'alt' => 'Up',
 		 };
 
 $img{'btn_del'} = { 'src' => '/btn_del.gif',
-		    'width' => 20,
-		    'height' => 22,
+		    'width' => 22,
+		    'height' => 20,
 		    'alt' => 'Delete',
 		};
 
