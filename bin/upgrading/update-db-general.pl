2984ac0f45bebb0de9a5133dbb4c01f51f6a06ab
Brad Fitzpatrick <brad@danga.com>
2002-03-17 03:42:13 +0000

    let's drop this column tonight at the same time as the utf-8 changes.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@922 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/upgrading/update-db-general.pl
+++ bin/upgrading/update-db-general.pl
@@ -1534,8 +1534,8 @@ register_alter(sub {
     if (column_type("user", "oldenc") eq "") {
         do_alter("user", "ALTER TABLE user ".
                  "ADD oldenc TINYINT DEFAULT 0 NOT NULL, ".
-                 "MODIFY name CHAR(80) NOT NULL"
-                 );
+                 "MODIFY name CHAR(80) NOT NULL, ".
+                 "DROP allow_getpromos");
     }
 
     # widen columns to accomodate larger Unicode names
