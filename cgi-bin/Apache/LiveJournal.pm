2d4c6b95a827a2d14a04ca6ab6f76385db7f0532
Brad Fitzpatrick <brad@danga.com>
2002-05-04 21:16:46 +0000

    more mod_perl support, this time userpic support.
    (yes, still putting off rewriting BML)
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@1067 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/LiveJournal.pm
+++ cgi-bin/Apache/LiveJournal.pm
@@ -4,11 +4,26 @@
 package Apache::LiveJournal;
 
 use strict;
-use Apache::Constants qw(:common REDIRECT);
+use Apache::Constants qw(:common REDIRECT HTTP_NOT_MODIFIED);
 use Apache::File ();
 use CGI;
 
-my $journal_view;
+my %RQ;       # per-request data
+my %USERPIC;  # conf related to userpics
+
+# init handler.
+sub handler
+{
+    my $r = shift;
+
+    $r->set_handlers(PerlTransHandler => [ \&trans ]);
+    $r->push_handlers(PerlCleanupHandler => sub { %RQ = (); });
+    
+    $USERPIC{'cache_dir'} = "$ENV{'LJHOME'}/htdocs/userpics";
+    $USERPIC{'use_disk_cache'} = -d $USERPIC{'cache_dir'};
+
+    return OK;
+}
 
 sub trans
 {
@@ -17,6 +32,8 @@ sub trans
     my $host = $r->header_in("Host");
 
     LJ::start_request();
+
+    return trans_userpic($r, $1) if $uri =~ m!^/userpic/(\d+)$!;
 
     my $redir = sub {
         my $url = shift;
@@ -41,7 +58,7 @@ sub trans
             return $redir->("$LJ::SITEROOT/userinfo.bml?user=$opts->{'user'}");
         }
 
-        $journal_view = $opts;
+        %RQ = %$opts;
         $r->handler("perl-script");
         $r->push_handlers(PerlHandler => \&journal_content);
         return OK;
@@ -84,7 +101,118 @@ sub trans
                                 'user' => $2, });
     }
 
+    return FORBIDDEN if $uri =~ m!^/userpics!;
     return DECLINED;
+}
+
+sub trans_userpic
+{
+    my $r = shift;
+    my $picid = shift;
+
+    # we can safely do this without checking since we never re-use
+    # picture IDs and don't let the contents get modified
+    return HTTP_NOT_MODIFIED if $r->header_in('If-Modified-Since');
+
+    $RQ{'picid'} = $picid;
+
+    my @dirs_make;
+    my $file;
+    if ($picid =~ /^\d*(\d\d)(\d\d\d)$/) {
+        push @dirs_make, ("$USERPIC{'cache_dir'}/$2",
+                          "$USERPIC{'cache_dir'}/$2/$1");
+        $file = "$USERPIC{'cache_dir'}/$2/$1/$picid";
+    } else {
+        my $mod = sprintf("%03d", $picid % 1000);
+        push @dirs_make, "$USERPIC{'cache_dir'}/$mod";
+        $file = "$USERPIC{'cache_dir'}/$mod/p$picid";
+    }
+
+    if ($USERPIC{'use_disk_cache'}) {
+        foreach (@dirs_make) {
+            next if -d $_;
+            mkdir $_, 0777;
+        }
+    }
+
+    # set both, so we can compared later if they're the same,
+    # and thus know if directories were created (if not,
+    # apache will give us a pathinfo)
+    $RQ{'userpicfile'} = $file;
+    $r->filename($file);
+
+    $r->handler("perl-script");
+    $r->push_handlers(PerlHandler => \&content_userpic);
+    return OK;
+}
+
+sub content_userpic
+{
+    my $r = shift;
+    my $file = $r->filename;
+
+    my $picid = $RQ{'picid'};
+
+    # will we try to use disk cache?
+    my $disk_cache = $USERPIC{'use_disk_cache'} && 
+        $file eq $RQ{'userpicfile'};
+
+    my ($pic, $data, $lastmod);
+    my $need_cache;
+
+    # try to get it from disk if in disk-cache mode
+    if ($disk_cache) {
+        if (-s $r->finfo) {
+            $lastmod = (stat _)[9];
+            my $fh = Apache::File->new($file);
+            $data = join('', <$fh>);
+            close $fh;
+        } else {
+            $need_cache = 1;
+        }
+    }
+
+    # else, get it from db.
+    unless ($data) {
+        my $dbr = LJ::get_dbh("slave", "master");
+        my $query = "SELECT p.state, p.userid, p.contenttype, UNIX_TIMESTAMP(p.picdate) ".
+            "AS 'lastmod', u.clusterid, u.dversion FROM userpic p, user u WHERE ".
+            "p.picid=$picid AND u.userid=p.userid";
+        $pic = $dbr->selectrow_hashref($query);
+        return NOT_FOUND unless $pic;
+
+        $lastmod = $pic->{'lastmod'};
+        if ($pic->{'dversion'} >= 2) {
+            my $dbb = LJ::get_cluster_reader($pic->{'clusterid'});
+            return SERVER_ERROR;
+            $data = $dbb->selectrow_array("SELECT imagedata FROM userpicblob2 WHERE ".
+                                          "userid=$pic->{'userid'} AND picid=$picid");
+        } else {
+            $data = $dbr->selectrow_array("SELECT imagedata FROM userpicblob WHERE ".
+                                          "picid=$picid");
+        }
+    }
+
+    return NOT_FOUND unless $data;
+
+    if ($need_cache && open (F, ">$file")) {
+        print F $data;
+        close F;
+    }
+
+    my $mime = "image/jpeg";
+    if ($data =~ /^GIF/) { $mime eq "image/gif"; }
+    elsif ($data =~ /\211PNG/) { $mime eq "image/png"; }
+    
+    $r->content_type($mime);
+    $r->header_out("Content-length", length($data));
+    $r->header_out("Expires", LJ::time_to_http(time()+3000000));
+    $r->header_out("Cache-Control", "no-transform");
+    $r->header_out("Last-Modified", LJ::time_to_http($lastmod));
+    $r->send_http_header();
+    $r->print($data);
+
+    return OK;
 }
 
 sub content
@@ -108,10 +236,10 @@ sub journal_content
 
     my $dbs = LJ::get_dbs();
 
-    if ($journal_view->{'vhost'} eq "users" && 
+    if ($RQ{'vhost'} eq "users" && 
         $uri eq "/robots.txt") 
     {
-        my $u = { 'user' => $journal_view->{'user'} };
+        my $u = { 'user' => $RQ{'user'} };
         LJ::load_user_props($dbs, $u, "opt_blockrobots");
         $r->content_type("text/plain");
         $r->send_http_header();
@@ -137,13 +265,13 @@ sub journal_content
     my %headers = ();
     my $opts = {
 	'headers' => \%headers,
-	'args' => $journal_view->{'args'},
-	'vhost' => $journal_view->{'vhost'},
+	'args' => $RQ{'args'},
+	'vhost' => $RQ{'vhost'},
 	'env' => \%ENV,
     };
 
-    my $user = $journal_view->{'user'};
-    my $html = LJ::make_journal($dbs, $user, $journal_view->{'mode'},
+    my $user = $RQ{'user'};
+    my $html = LJ::make_journal($dbs, $user, $RQ{'mode'},
                                 $remote, $opts);
 
     my $status = $opts->{'status'} || "200 OK";
