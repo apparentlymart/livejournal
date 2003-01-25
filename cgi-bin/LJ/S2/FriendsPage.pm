f262712bc46aa4a93de9b723e0e0ab6717ffd6f6
Brad Fitzpatrick <brad@danga.com>
2003-01-25 22:35:14 +0000

    S2 friendsfriends view using unreadable friends colors http://zilla.livejournal.org/show_bug.cgi?id=509
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@1772 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2/FriendsPage.pm
+++ cgi-bin/LJ/S2/FriendsPage.pm
@@ -163,14 +163,14 @@ sub FriendsPage
     unless ($opts->{'view'} eq "friendsfriends") {
         $sth = $dbr->prepare("SELECT u.user, u.userid, u.clusterid, f.fgcolor, f.bgcolor, u.name, u.defaultpicid, u.opt_showtalklinks, u.moodthemeid, u.statusvis, u.oldenc FROM friends f, user u WHERE u.userid=f.friendid AND f.userid=$u->{'userid'} AND f.friendid IN ($ownersin)");
     } else {
-        $sth = $dbr->prepare("SELECT u.user, u.userid, u.clusterid, '#000000', '#ffffff', u.name, u.defaultpicid, u.opt_showtalklinks, u.moodthemeid, u.statusvis, u.oldenc FROM user u WHERE u.userid IN ($ownersin)");}
-    
+        $sth = $dbr->prepare("SELECT u.user, u.userid, u.clusterid, u.name, u.defaultpicid, u.opt_showtalklinks, u.moodthemeid, u.statusvis, u.oldenc FROM user u WHERE u.userid IN ($ownersin)");}
+
 
     $sth->execute;
     while ($_ = $sth->fetchrow_hashref) {
         next unless ($_->{'statusvis'} eq "V");  # ignore suspended/deleted users.
-        $_->{'fgcolor'} = LJ::color_fromdb($_->{'fgcolor'});
-        $_->{'bgcolor'} = LJ::color_fromdb($_->{'bgcolor'});
+        $_->{'fgcolor'} = LJ::color_fromdb($_->{'fgcolor'}) || "#000000";
+        $_->{'bgcolor'} = LJ::color_fromdb($_->{'bgcolor'}) || "#ffffff";
         $friends{$_->{'userid'}} = $_;
     }
 
