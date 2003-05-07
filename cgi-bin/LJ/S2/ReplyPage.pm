578b33f07be8bd70e8ba7133b9816e497fb4181e
Brad Fitzpatrick <brad@danga.com>
2003-05-07 22:51:07 +0000

    S2:  prettify ReplyForm
    Jesse, it's all you now....
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@2263 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2/ReplyPage.pm
+++ cgi-bin/LJ/S2/ReplyPage.pm
@@ -35,7 +35,7 @@ sub ReplyPage
             return;
         }
         my $dbcs = LJ::get_cluster_set($u);
-        my $sql = "SELECT posterid, state, datepost FROM talk2 ".
+        my $sql = "SELECT jtalkid, posterid, state, datepost FROM talk2 ".
             "WHERE journalid=$u->{'userid'} AND jtalkid=$re_talkid ".
             "AND nodetype='L' AND nodeid=$entry->{'jitemid'}";
         $parpost = LJ::dbs_selectrow_hashref($dbcs, $sql);
@@ -92,6 +92,7 @@ sub ReplyPage
         '_remote' => $remote,
         '_u' => $u,
         '_ditemid' => $ditemid,
+        '_parpost' => $parpost,
     };
 
     return $p;
@@ -123,8 +124,9 @@ sub ReplyForm__print
     my $pics = LJ::Talk::get_subjecticons();
 
     # hidden values
-    my $parent = 0;  # FIXME: need parentid (or 0 if top-level)
-    my $par_subject = "";
+    my $parpost = $form->{'_parpost'};
+    my $parent = $parpost ? $parpost->{'jtalkid'} : 0;
+    my $par_subject = $parpost ? $parpost->{'subject'} : "";
     $ret .= "<input type='hidden' name='parenttalkid' value='$parent' />\n";
     $ret .= "<input type='hidden' name='itemid' value='$form->{'_ditemid'}' />\n";
     $ret .= "<input type='hidden' name='journal' value='$u->{'user'}' />\n";
@@ -158,15 +160,17 @@ sub ReplyForm__print
     }
 
     my $checked = "checked='checked'";
+    my $txt_loggedin = "Logged in user:";
     if ($remote) {
         $ret .= "<tr valign='middle'>";
         $ret .= "<td align='right'>&nbsp;</td>";
         if (LJ::is_banned($dbs, $remote, $u)) {
             $ret .= "<td align='middle'>( )</td>";
-            $ret .= "<td align='left'><font color='#c0c0c0'>" . BML::ml(".opt.loggedin", {'username'=>"<i>$remote->{'user'}</i>"}) . "</font>" . BML::ml(".opt.bannedfrom", {'journal'=>$u->{'user'}}) . "</td>";
+            $ret .= "<td align='left'><span class='ljdeem'><b>$txt_loggedin</b> <i>$remote->{'user'}</i></td>";
+            # FIXME: add trans "You're banned" string somehow.
         } else {
             $ret .= "<td align='middle'><input type='radio' name='usertype' value='cookieuser' id='talkpostfromremote' $checked /></td>";
-            $ret .= "<td align='left'><label for='talkpostfromremote'>" . BML::ml(".opt.loggedin", {'username'=>"<i>$remote->{'user'}</i>"}) . "</label>\n";
+            $ret .= "<td align='left'><label for='talkpostfromremote'><b>$txt_loggedin</b> <i>$remote->{'user'}</i></label>\n";
             $ret .= "<input type='hidden' name='cookieuser' value='$remote->{'user'}' id='cookieuser' />\n";
             if ($u->{'opt_whoscreened'} eq 'A' ||
                 ($u->{'opt_whoscreened'} eq 'F' &&
@@ -180,56 +184,33 @@ sub ReplyForm__print
     }
 
     # ( ) LiveJournal user:
+    my $txt_ljuser = "[[sitenameshort]] user:";
+    $txt_ljuser =~ s/\[\[sitenameshort\]\]/$LJ::SITENAMESHORT/;
     $ret .= "<tr valign='middle'>";
     $ret .= "<td>&nbsp;</td>";
     $ret .= "<td align=middle><input type='radio' name='usertype' value='user' id='talkpostfromlj' $checked />";
-    $ret .= "</td><td align='left'><b><label for='talkpostfromlj'>$ML{'.opt.ljuser'}</label></b> ";
+    $ret .= "</td><td align='left'><b><label for='talkpostfromlj'>$txt_ljuser</label></b> ";
     $ret .= $ML{'.opt.willscreenfriend'} if $u->{'opt_whoscreened'} eq 'F';
     $ret .= $ML{'.opt.willscreen'} if $u->{'opt_whoscreened'} eq 'A';
     $ret .= "</tr>\n";
 
     # Username: [    ] Password: [    ]  Login? [ ]
+    my $txt_loginq = "Login?";
+    my $txt_password = "Password:";
+    my $txt_username = "Username:";
     $ret .= "<tr valign='middle' align='left'><td colspan='2'></td><td>";
-    $ret .= "$ML{'Username'}:&nbsp;<input class='textbox' name='userpost' size='13' maxlength='15' id='username' /> ";
-    $ret .= "$ML{'Password'}:&nbsp;<input class='textbox' name='password' type='password' maxlength='30' size='13' id='password' /> <label for='logincheck'>$ML{'.loginq'}&nbsp;</label><input type='checkbox' name='do_login' id='logincheck' /></td></tr>\n";
+    $ret .= "$txt_username&nbsp;<input class='textbox' name='userpost' size='13' maxlength='15' id='username' /> ";
+    $ret .= "$txt_password&nbsp;<input class='textbox' name='password' type='password' maxlength='30' size='13' id='password' /> <label for='logincheck'>$txt_loginq&nbsp;</label><input type='checkbox' name='do_login' id='logincheck' /></td></tr>\n";
     
     # subject
-    $ret .= "<tr valign='top'><td align='right'>$ML{'.opt.subject'}</td><td colspan='4'><input class='textbox' type='text' size='50' maxlength='100' name='subject' value=\"" . LJ::ehtml($par_subject) . "\" /><br /><?de $ML{'.nosubjecthtml'} de?></td></tr>\n";
-
-    unless ($u->{'opt_notalkicons'})
-    {
-        # spit out a pretty table of all the possible subjecticons
-        $ret .= "<tr valign='middle'><td>&nbsp;</td><td align='left' colspan='4'>\n";
-        $ret .= "<table border='0' cellspacing='3' cellpadding='0'>\n";
-        
-        foreach my $type (@{$pics->{'types'}}) {
-            my $input = "";
-            my $img = "";
-            
-            # go through and make radio button/icon image rows.
-            foreach (@{$pics->{'lists'}->{$type}}) {
-                $input .= "<td valign='middle' align='middle'><input type='radio' name='subjecticon' value='$_->{'id'}' id='talki_$_->{'id'}' /></td>\n";
-                $img .= "<td valign='middle' align='middle'><label for='talki_$_->{'id'}'>" . LJ::Talk::show_image($pics, $_->{'id'}) . "</label></td>\n";
-            }
-            $ret .= "<tr>";
-            
-            # make an option if they don't want an image
-            if ($type eq $pics->{'types'}->[0]) { 
-                $ret .= "<td rowspan='5' valign='middle' align='middle'><input type='radio' name='subjecticon' value='none' checked='1' id='talki_none' /><br /><label for='talki_none'><i>$ML{'.opt.noimage'}</i></label></td><td rowspan='5'>&nbsp;</td>\n"; 
-            }
-            
-            # then splice in the icon & radio button rows...
-            $ret .= "$input</tr>\n";
-            $ret .= "<tr>\n$img</tr>\n";
-        }
-        # end that table, foo!
-        $ret .= "</table>\n";
-        $ret .= "</td></tr>\n";
-    }
+    my $txt_subject = "Subject:";
+    my $txt_nohtml_subject = "No HTML allowed in subject";
+    $ret .= "<tr valign='top'><td align='right'>$txt_subject</td><td colspan='4'><input class='textbox' type='text' size='50' maxlength='100' name='subject' value=\"" . LJ::ehtml($par_subject) . "\" /><div id='ljnohtmlsubj' class='ljdeem'>$txt_nohtml_subject</div></td></tr>\n";
 
     $ret .= "<tr><td align='right'>&nbsp;</td><td colspan='4'>";
-    $ret .= "$ML{'.opt.noautoformat'}<input type='checkbox' value='1' name='prop_opt_preformatted' />";
-    $ret .= LJ::help_icon("noautoformat", " ");
+    my $txt_noautoformat = "Don't auto-format ";
+    $ret .= "<label for='ljreplynoauto'>$txt_noautoformat</label> ".
+        "<input type='checkbox' value='1' id='ljreplynoauto' name='prop_opt_preformatted' />";
     
     my %res;
     if ($remote) {
@@ -240,15 +221,15 @@ sub ReplyForm__print
                            }, \%res, { "noauth" => 1, "userid" => $remote->{'userid'} });
       }
     if ($res{'pickw_count'}) {
-        $ret .= BML::ml('.label.picturetouse',{'username'=>$remote->{'user'}});
+        my $txt_pickw = "Picture to use:";
+        $ret .= "$txt_pickw ";
         my @pics;
         for (my $i=1; $i<=$res{'pickw_count'}; $i++) {
             push @pics, $res{"pickw_$i"};
         }
         @pics = sort { lc($a) cmp lc($b) } @pics;
         $ret .= LJ::html_select({'name' => 'prop_picture_keyword', },
-                                ("", $ML{'.opt.defpic'}, map { ($_, $_) } @pics));
-        $ret .= LJ::help_icon("userpics", " ");
+                                ("", "", map { ($_, $_) } @pics));
     }
     $ret .= "</td></tr>\n";
 
@@ -260,7 +241,7 @@ sub ReplyForm__print
     ## preview stuff
     $ret .= "<input type='submit' name='submitpreview' value='$txt_preview' />\n";
     if ($LJ::SPELLER) {
-        $ret .= "<input type='checkbox' name='do_spellcheck' value='1' id='spellcheck' /> <label for='spellcheck'>$ML{'talk.spellcheck'}</label>";
+        $ret .= "<input type='checkbox' name='do_spellcheck' value='1' id='spellcheck' /> <label for='spellcheck'>$txt_spell</label>";
     }
 
     if ($u->{'opt_logcommentips'} eq "A") {
@@ -271,14 +252,6 @@ sub ReplyForm__print
         $ret .= "<br />$ML{'.loganonip'}";
         $ret .= LJ::help_icon("iplogging", " ");
     }
-
-
-    $ret .= "<br />$ML{'.paraformat'}<br /> <span class='ljdeem'>$txt_allowedhtml ";
-    foreach (sort &LJ::CleanHTML::get_okay_comment_tags()) {
-        $ret .= "&lt;$_&gt; ";
-    }
-    $ret .= "</span>";
-
 
     $ret .= "</td></tr></table>\n";
 
