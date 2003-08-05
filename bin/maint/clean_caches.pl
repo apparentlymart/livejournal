d28fccad7126247ad259947658add361a545ce09
Brad Fitzpatrick <brad@danga.com>
2003-08-05 07:53:42 +0000

    kill ancient cleaning code, no longer used
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@2795 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/maint/clean_caches.pl
+++ bin/maint/clean_caches.pl
@@ -27,51 +27,6 @@ $maint{'clean_caches'} = sub
         if ($dbh->err) { print $dbh->errstr; }
         print "    deleted ", $sth->rows, "\n";
     } while ($sth->rows && ! $sth->err);
-
-    ## clean the recent_* tables now (3 week cache on the slave dbs)
-    return unless ($LJ::USE_RECENT_TABLES);
-
-    my $maxid;
-
-    print "-I- Cleaning recent_logtext.\n";    
-    $sth = $dbh->prepare("SELECT itemid FROM log WHERE logtime > DATE_SUB(NOW(), INTERVAL $LJ::RECENT_DAYS DAY) LIMIT 1");
-    $sth->execute;
-    ($maxid) = $sth->fetchrow_array;
-    
-    ## only do cleaning if there's cleaning to be done:
-    if ($maxid) {
-        print "-I-   Cleaning all recent_logtext with itemids < $maxid\n";
-        my $rows;
-        do
-        {
-            $sth = $dbh->prepare("DELETE FROM recent_logtext WHERE itemid < $maxid LIMIT 500");
-            $sth->execute;
-            $rows = $sth->rows;
-            print "-I-    - deleted $rows rows\n";
-            sleep 1;
-        } while ($rows);
-    }
-
-    print "-I- Cleaning recent_talktext.\n";    
-    $sth = $dbh->prepare("SELECT talkid FROM talk WHERE datepost > DATE_SUB(NOW(), INTERVAL $LJ::RECENT_DAYS DAY) LIMIT 1");
-    $sth->execute;
-    ($maxid) = $sth->fetchrow_array;
-    
-    ## only do cleaning if there's cleaning to be done:
-    if ($maxid) {
-        print "-I-   Cleaning all recent_talktext with talkids < $maxid\n";
-        my $rows;
-        do
-        {
-            $sth = $dbh->prepare("DELETE FROM recent_talktext WHERE talkid < $maxid LIMIT 500");
-            $sth->execute;
-            $rows = $sth->rows;
-            print "-I-    - deleted $rows rows\n";
-            sleep 1;
-        } while ($rows);
-    }
-
-
 };
 
 1;
