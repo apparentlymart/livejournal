4fe5b34b05f96eebb1bb3bd12d0719214b5e7e7e
Anatoly Vorobey <avorobey@gmail.com>
2003-11-11 04:29:31 +0000

    Cleans up modperl.pl and makes LJ able to survive restats and graceful restarts.
    - moves the bulk of modperl.pl into a new file, modperl_subs.pl, and
    organises it into three groups: uses/requires, sub setup_start() to be
    called just once on init, and sub setup_restart() to be called after
    every restart. This last group includes all the Apache config directives.
    - puts everything in modperl.pl and modperl_subs.pl into a new package
    LJ::ModPerl . Gets rid of superfluous BEGIN blocks.
    - modperl.pl now delets its own entry in %INC to force the PerlRequire
    directive in the apache config to reload modperl.pl after restarts.
    
    The visible effect of all this should be that apachectl graceful and
    apachectl restart (or, equivalently, sending SIGUSR1 or SIGHUP,
    respectively, to the master process) should now work with LJ. No
    modification of the apache config file is required.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@3200 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/modperl.pl
+++ cgi-bin/modperl.pl
@@ -1,88 +1,23 @@
 #!/usr/bin/perl
 #
 
+package LJ::ModPerl;
 use strict;
 use lib "$ENV{'LJHOME'}/cgi-bin";
 use Apache;
-use Apache::LiveJournal;
-use Apache::CompressClientFixup;
-use Apache::BML;
-use LJ::SpellCheck;
-use LJ::TextMessage;
-use Digest::MD5;
-use MIME::Words;
-use Text::Wrap ();
-use LWP::UserAgent ();
-use Storable;
-use Image::Size ();
 
-BEGIN {
-    require "$ENV{'LJHOME'}/cgi-bin/ljlang.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/ljpoll.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/cleanhtml.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/htmlcontrols.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/imageconf.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/propparse.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/supportlib.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/cleanhtml.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/portal.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/talklib.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/ljtodo.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/directorylib.pl";
-    require "$ENV{'LJHOME'}/cgi-bin/ljemailgateway.pl";
-}
+# pull in libraries and do per-start initialization once.
+require "modperl_subs.pl";
 
-# auto-load some stuff before fork:
-Storable::thaw(Storable::freeze({}));
-foreach my $minifile ("GIF89a", "\x89PNG\x0d\x0a\x1a\x0a", "\xFF\xD8") {
-    Image::Size::imgsize(\$minifile);
-}
-DBI->install_driver("mysql");
+# do per-restart initialization
+LJ::ModPerl::setup_restart();
 
-# setup httpd.conf things for the user:
-Apache->httpd_conf("DocumentRoot $LJ::HTDOCS")
-    if $LJ::HTDOCS;
-Apache->httpd_conf("ServerAdmin $LJ::ADMIN_EMAIL")
-    if $LJ::ADMIN_EMAIL;
+# delete itself from %INC to make sure this file is run again
+# when apache is restarted
 
-Apache->httpd_conf(qq{
+delete $INC{"$ENV{'LJHOME'}/cgi-bin/modperl.pl"};
 
-# This interferes with LJ's /~user URI, depending on the module order
-<IfModule mod_userdir.c>
-  UserDir disabled
-</IfModule>
-
-PerlInitHandler Apache::LiveJournal
-PerlInitHandler Apache::SendStats
-PerlFixupHandler Apache::CompressClientFixup
-PerlCleanupHandler Apache::SendStats
-PerlChildInitHandler Apache::SendStats
-DirectoryIndex index.html index.bml
-});
-
-if ($LJ::BML_DENY_CONFIG) {
-    Apache->httpd_conf("PerlSetVar BML_denyconfig \"$LJ::BML_DENY_CONFIG\"\n");
-}
-
-unless ($LJ::SERVER_TOTALLY_DOWN)
-{
-    Apache->httpd_conf(qq{
-# BML support:
-<Files ~ "\\.bml\$">
-  SetHandler perl-script
-  PerlHandler Apache::BML
-</Files>
-
-# User-friendly error messages
-ErrorDocument 404 /404-error.html
-ErrorDocument 500 /500-error.html
-
-});
-}
-
-# set this before we fork
-$LJ::CACHE_CONFIG_MODTIME = (stat("$ENV{'LJHOME'}/cgi-bin/ljconfig.pl"))[9];
-
+# other packages
 package BMLCodeBlock;
 require "$ENV{'LJHOME'}/cgi-bin/emailcheck.pl";
 
---
+++ cgi-bin/modperl_subs.pl
@@ -0,0 +1,99 @@
+#!/usr/bin/perl
+#
+
+# to be require'd by modperl.pl
+
+package LJ::ModPerl;
+
+use strict;
+
+# pull in a lot of useful stuff before we fork children
+
+use Apache;
+use Apache::LiveJournal;
+use Apache::CompressClientFixup;
+use Apache::BML;
+use LJ::SpellCheck;
+use LJ::TextMessage;
+use Digest::MD5;
+use MIME::Words;
+use Text::Wrap ();
+use LWP::UserAgent ();
+use Storable;
+use Image::Size ();
+
+require "$ENV{'LJHOME'}/cgi-bin/ljlang.pl";
+require "$ENV{'LJHOME'}/cgi-bin/ljpoll.pl";
+require "$ENV{'LJHOME'}/cgi-bin/cleanhtml.pl";
+require "$ENV{'LJHOME'}/cgi-bin/htmlcontrols.pl";
+require "$ENV{'LJHOME'}/cgi-bin/imageconf.pl";
+require "$ENV{'LJHOME'}/cgi-bin/propparse.pl";
+require "$ENV{'LJHOME'}/cgi-bin/supportlib.pl";
+require "$ENV{'LJHOME'}/cgi-bin/cleanhtml.pl";
+require "$ENV{'LJHOME'}/cgi-bin/portal.pl";
+require "$ENV{'LJHOME'}/cgi-bin/talklib.pl";
+require "$ENV{'LJHOME'}/cgi-bin/ljtodo.pl";
+require "$ENV{'LJHOME'}/cgi-bin/directorylib.pl";
+require "$ENV{'LJHOME'}/cgi-bin/ljemailgateway.pl";
+
+sub setup_start {
+
+    # auto-load some stuff before fork:
+    Storable::thaw(Storable::freeze({}));
+    foreach my $minifile ("GIF89a", "\x89PNG\x0d\x0a\x1a\x0a", "\xFF\xD8") {
+        Image::Size::imgsize(\$minifile);
+    }
+    DBI->install_driver("mysql");
+
+    # set this before we fork
+    $LJ::CACHE_CONFIG_MODTIME = (stat("$ENV{'LJHOME'}/cgi-bin/ljconfig.pl"))[9];
+}
+
+sub setup_restart {
+
+    # setup httpd.conf things for the user:
+    Apache->httpd_conf("DocumentRoot $LJ::HTDOCS")
+        if $LJ::HTDOCS;
+    Apache->httpd_conf("ServerAdmin $LJ::ADMIN_EMAIL")
+        if $LJ::ADMIN_EMAIL;
+
+    Apache->httpd_conf(qq{
+
+# This interferes with LJ's /~user URI, depending on the module order
+<IfModule mod_userdir.c>
+  UserDir disabled
+</IfModule>
+
+PerlInitHandler Apache::LiveJournal
+PerlInitHandler Apache::SendStats
+PerlFixupHandler Apache::CompressClientFixup
+PerlCleanupHandler Apache::SendStats
+PerlChildInitHandler Apache::SendStats
+DirectoryIndex index.html index.bml
+});
+
+    if ($LJ::BML_DENY_CONFIG) {
+        Apache->httpd_conf("PerlSetVar BML_denyconfig \"$LJ::BML_DENY_CONFIG\"\n");
+    }
+
+    unless ($LJ::SERVER_TOTALLY_DOWN)
+    {
+        Apache->httpd_conf(qq{
+# BML support:
+<Files ~ "\\.bml\$">
+  SetHandler perl-script
+  PerlHandler Apache::BML
+</Files>
+
+# User-friendly error messages
+ErrorDocument 404 /404-error.html
+ErrorDocument 500 /500-error.html
+
+});
+    }
+
+}
+
+setup_start();
+
+1;
