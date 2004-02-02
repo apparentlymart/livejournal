7ab63a579b53313fe8b0cc12ccbaae72628ee001
Brad Fitzpatrick <brad@danga.com>
2004-02-02 07:49:03 +0000

    - cache socks
    - support sending to multiple addresses (to phase in testing of ljstatscasterd
      and multicast, or as an alternative to multicast if ljstatscasterd doesn't
      help much.)  but in the meantime we can keep broadcasting to the internal
      network and also broadcast to localhost for ljstatscasterd, which'll
      multicast.  once we're comfortable at least one of the load balancers is
      working fine with the multicast traffic, we can turn off the broadcast.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@3604 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/SendStats.pm
+++ cgi-bin/Apache/SendStats.pm
@@ -21,6 +21,8 @@ if ($LJ::HAVE_INLINE && $LJ::FREECHILDREN_BCAST) {
     }
 }
 
+use vars qw(%udp_sock);
+
 sub handler
 {
     my $r = shift;
@@ -36,20 +38,31 @@ sub handler
     $free += $cleanup;
     $free += $childinit;
     $active -= $cleanup if $active;
-    if ($LJ::FREECHILDREN_BCAST =~ /^(\S+):(\d+)$/) {
+
+    my $list = ref $LJ::FREECHILDREN_BCAST ?
+        $LJ::FREECHILDREN_BCAST : [ $LJ::FREECHILDREN_BCAST ];
+
+    foreach my $host (@$list) {
+        next unless $host =~ /^(\S+):(\d+)$/;
         my $bcast = $1;
         my $port = $2;
-        my $sock = IO::Socket::INET->new(Proto => 'udp');
-        $r->log_error("SendStats: couldn't create socket") unless $sock;
-        if ($sock) {
-            $sock->sockopt(SO_BROADCAST, 1);
-            my $ipaddr = inet_aton($bcast);
-            my $portaddr = sockaddr_in($port, $ipaddr);
-            my $message = "bcast_ver=1\nfree=$free\nactive=$active\n";
-            my $res = $sock->send($message, 0, $portaddr);
-            $r->log_error("SendStats: couldn't broadcast") 
-                unless $res;
+        my $sock = $udp_sock{$host};
+        unless ($sock) {
+            $udp_sock{$host} = $sock = IO::Socket::INET->new(Proto => 'udp');
+            if ($sock) {
+                $sock->sockopt(SO_BROADCAST, 1);
+            } else {
+                $r->log_error("SendStats: couldn't create socket: $host");
+                next;
+            } 
         }
+
+        my $ipaddr = inet_aton($bcast);
+        my $portaddr = sockaddr_in($port, $ipaddr);
+        my $message = "bcast_ver=1\nfree=$free\nactive=$active\n";
+        my $res = $sock->send($message, 0, $portaddr);
+        $r->log_error("SendStats: couldn't broadcast") 
+            unless $res;
     }
 
     return OK;
