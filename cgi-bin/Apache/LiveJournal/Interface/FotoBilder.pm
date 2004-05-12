5bee2c5779ef85796aff893630138e4a78a90d7c
Brad Whitaker <brad.whitaker@gmail.com>
2004-05-12 23:30:46 +0000

    Allow for calling of RPC functions in non-http context
    -- move most functionality into run_method() function
    
    -- individual handlers no longer take an $r or print, but just return
       key/value pairs in a hashref
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@4057 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/LiveJournal/Interface/FotoBilder.pm
+++ cgi-bin/Apache/LiveJournal/Interface/FotoBilder.pm
@@ -7,39 +7,52 @@ use strict;
 use Apache::Constants qw(:common REDIRECT HTTP_NOT_MODIFIED
                          HTTP_MOVED_PERMANENTLY BAD_REQUEST);
 
+sub run_method
+{
+    my $cmd = shift;
+
+    # Available functions for this interface.
+    my $interface = {
+        'checksession'       => \&checksession,
+        'get_user_info'      => \&get_user_info,
+        'makechals'          => \&makechals,
+        'set_quota'          => \&set_quota,
+        'user_exists'        => \&user_exists,
+        'get_auth_challenge' => \&get_auth_challenge,
+    };
+    return undef unless $interface->{$cmd};
+
+    return $interface->{$cmd}->(@_);
+}
+
 sub handler
 {
     my $r = shift;
     my $uri = $r->uri;
     return 404 unless $uri =~ m#^/interface/fotobilder(?:/(\w+))?$#;
     my $cmd = $1;
-    return BAD_REQUEST if $cmd && $r->method ne "POST";
 
-    # Available functions for this interface.
-    my $interface = {
-        'checksession'  => \&checksession,
-        'get_user_info' => \&get_user_info,
-        'makechals'     => \&makechals,
-        'set_quota'     => \&set_quota,
-        'user_exists'   => \&user_exists,
-        'get_auth_challenge' => \&get_auth_challenge,
-    };
-
-    return BAD_REQUEST unless ref $interface->{$cmd} eq 'CODE';
+    return BAD_REQUEST unless $r->method eq "POST";
 
     $r->content_type("text/plain");
     $r->send_http_header();
-    $r->print("fotobilder-interface-version: 1\n");
 
     my %POST = $r->content;
-    return $interface->{$cmd}->($r, \%POST) || OK;
+    my $res = run_method($cmd, \%POST)
+        or return BAD_REQUEST;
+
+    $res->{"fotobilder-interface-version"} = 1;
+
+    $r->print(join("\n", map { "$_: $res->{$_}" } keys %$res));
+
+    return OK;
 }
 
 # Is there a current LJ session?
 # If so, return info.
 sub get_user_info
 {
-    my ($r, $POST) = @_;
+    my $POST = shift;
     BML::reset_cookies();
     $LJ::_XFER_REMOTE_IP = $POST->{'remote_ip'};
 
@@ -52,28 +65,26 @@ sub get_user_info
     } else {
         $u = LJ::get_remote();
     }
-    return OK unless $u && $u->{'journaltype'} eq 'P';
+    return {} unless $u && $u->{'journaltype'} eq 'P';
 
-    $r->print("user: $u->{'user'}\n");
-    $r->print("userid: $u->{'userid'}\n");
-
-    $r->print("can_upload: " . (can_upload($u)) . "\n");
-    $r->print("gallery_enabled: 1\n"); # future toggle
-    my $quota      = LJ::get_cap($u, 'disk_quota') << 10;
-    my $totalusage = LJ::Blob::get_disk_usage($u) >> 10;
-
-    $r->print("diskquota: $quota\n");
-    $r->print("totalusage: $totalusage\n");
+    my %ret = (
+               user            => $u->{user},
+               userid          => $u->{userid},
+               can_upload      => can_upload($u),
+               gallery_enabled => 1, # future toggle
+               diskquota       => LJ::get_cap($u, 'disk_quota') << 10,
+               totalusage      => LJ::Blob::get_disk_usage($u) >> 10,
+               );
 
     # now optional site-specific info
     if (LJ::are_hooks("fb_rpc_user_info")) {
         my $inf = LJ::run_hook("fb_rpc_user_info", $u);
         while (my ($key, $val) = each %$inf) {
-            $r->print("$key: $val\n");
+            $ret{$key} = $val;
         }
     }
 
-    return OK;
+    return \%ret;
 }
 
 # get_user_info above used to be called 'checksession', maintain
@@ -83,38 +94,35 @@ sub checksession { get_user_info(@_); }
 # Pregenerate a list of challenge/responses.
 sub makechals
 {
-    my ($r, $POST) = @_;
+    my $POST = shift;
     my $count = int($POST->{'count'}) || 1;
     if ($count > 50) { $count = 50; }
     my $u = LJ::load_user($POST->{'user'});
-    return OK unless $u;
+    return {} unless $u;
 
-    $r->print("count: $count\n");
+    my %ret = ( count => $count );
+
     for (my $i=1; $i<=$count; $i++) {
         my $chal = LJ::rand_chars(40);
         my $resp = Digest::MD5::md5_hex($chal . Digest::MD5::md5_hex($u->{'password'}));
-        $r->print("chal_$i: $chal\nresp_$i: $resp\n");
+        $ret{"chal_$i"} = $chal;
+        $ret{"resp_$i"} = $resp;
     }
-    return OK;
+
+    return \%ret;
 }
 
 # Does the user exist?
 sub user_exists
 {
-    my ($r, $POST) = @_;
+    my $POST = shift;
     my $u = LJ::load_user($POST->{'user'});
-    if ($u) {
-        $r->print("exists: 1\n");
-        $r->print("can_upload: " . (can_upload($u)) . "\n");
-    }
-    return OK;
-}
+    return {} unless $u;
 
-# Does the user have upload access?  (ignoring quota restrictions)
-sub can_upload
-{
-    my $u = shift;
-    return LJ::get_cap($u, 'fb_account') ? 1 : 0;
+    return { 
+        exists => 1,
+        can_upload => can_upload($u),
+    };
 }
 
 # Mirror FB quota information over to LiveJournal.
@@ -122,30 +130,42 @@ sub can_upload
 # 'used' - FB disk usage in bytes
 sub set_quota
 {
-    my ($r, $POST) = @_;
+    my $POST = shift;
     my $u = LJ::load_userid($POST->{'uid'});
-    return OK unless $u && defined $POST->{'used'};
-
-    my $used = $POST->{'used'} << 10;  # Kb -> bytes
+    return {} unless $u && defined $POST->{'used'};
 
     my $dbcm = LJ::get_cluster_master($u);
-    return OK unless $dbcm;
+    return {} unless $dbcm;
 
+    my $used = $POST->{'used'} << 10;  # Kb -> bytes
     my $result = $dbcm->do('REPLACE INTO userblob SET ' .
                            'domain=?, length=?, journalid=?, blobid=0',
                            undef, LJ::get_blob_domainid('picpix_quota'),
                            $used, $u->{'userid'});
 
-    $r->print("status: " . ($result ? 1 : 0) . "\n");
-    return OK;
+    return {
+        status => ($result ? 1 : 0),
+    };
 }
 
 sub get_auth_challenge
 {
-    my ($r, $POST) = @_;
-    my $goodfor = $POST->{'goodfor'} + 0;
-    $r->print("chal: " . LJ::challenge_generate($goodfor) . "\n");
-    return OK;
+    my $POST = shift;
+
+    return {
+        chal => LJ::challenge_generate($POST->{goodfor}+0),
+    };
+}
+
+#########################################################################
+# non-interface helper functions
+#
+
+# Does the user have upload access?  (ignoring quota restrictions)
+sub can_upload
+{
+    my $u = shift;
+    return LJ::get_cap($u, 'fb_account') ? 1 : 0;
 }
 
 1;
