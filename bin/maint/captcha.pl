173ec568e4232ded0bcd3efb427a366a36a494e2
Brad Fitzpatrick <brad@danga.com>
2004-05-30 05:45:05 +0000

    no serialization necessary for captcha generation
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@4146 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/maint/captcha.pl
+++ bin/maint/captcha.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use vars qw(%maint);
+use vars qw(%maint %maintinfo);
 
 use LJ::Captcha qw{};
 use LJ::Blob    qw{};
@@ -54,6 +54,7 @@ sub gencode ($) {
 #####################################################################
 ### M A I N T E N A N C E   T A S K S
 #####################################################################
+$maintinfo{gen_audio_captchas}{opts}{no_locking} = 1;
 $maint{gen_audio_captchas} = sub {
     my (
         $u,                     # Fake user record for Blob::put
@@ -156,7 +157,7 @@ $maint{gen_audio_captchas} = sub {
     return 1;
 };
 
-
+$maintinfo{gen_image_captchas}{opts}{no_locking} = 1;
 $maint{gen_image_captchas} = sub {
     my (
         $u,                     # Fake user record for Blob::put
