fbe62947664a00e95e59858126a84232e554d8c4
Mark Smith <mark@qq.is>
2005-02-17 19:37:44 +0000

    add s2compiled migration flag documentation.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@4864 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljdefaults.pl
+++ cgi-bin/ljdefaults.pl
@@ -228,6 +228,7 @@
 
     # maximum size to cache s2compiled data
     $MAX_S2COMPILED_CACHE_SIZE ||= 7500; # bytes
+    $S2COMPILED_MIGRATION_DONE ||= 0;    # turn on after s2compiled2 migration
 
 }
 
