ae9851c3668a6b76eeb83d436305d800bb3ad47b
Brad Fitzpatrick <brad@danga.com>
2005-04-12 19:09:31 +0000

    rewrite Apache::DebateSuicide to not use Inline and not depend on the current Apache's ABI.
    so this is still not portable (Linux only) but more portable than before.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@4964 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/DebateSuicide.pm
+++ cgi-bin/Apache/DebateSuicide.pm
@@ -3,36 +3,19 @@
 
 package Apache::DebateSuicide;
 
-BEGIN {
-    $LJ::HAVE_INLINE = eval q{
-        use Inline (C => 'DATA',
-                    DIRECTORY => $ENV{LJ_INLINE_DIR} ||"$ENV{'LJHOME'}/Inline",
-                    );
-        1;
-    };
-}
 use strict;
 use Apache::Constants qw(:common);
 
-if ($LJ::HAVE_INLINE && ($LJ::SUICIDE_LOAD || $LJ::SUICIDE)) {
-    eval {
-        Inline->init();
-    };
-    if ($@ && ! $LJ::JUST_COMPILING) {
-        print STDERR "Warning: You seem to have Inline.pm, but you haven't run \$LJHOME/bin/lj-inline.pl.  " .
-            "Continuing without it, but stats won't broadcast.\n";
-        $LJ::HAVE_INLINE = 0;
-    }
-}
-
 use vars qw($gtop);
+our %known_parent;
+our $ppid;
 
 # oh btw, this is totally linux-specific.  gtop didn't work, so so much for portability.
 sub handler
 {
     my $r = shift;
     return OK if $r->main;
-    return OK unless $LJ::HAVE_INLINE && $LJ::HAVE_GTOP && $LJ::SUICIDE;
+    return OK unless $LJ::HAVE_GTOP && $LJ::SUICIDE;
 
     my $meminfo;
     return OK unless open (MI, "/proc/meminfo");
@@ -50,7 +33,14 @@ sub handler
     my $goodfree = $LJ::SUICIDE_UNDER{$LJ::SERVER_NAME} || $LJ::SUICIDE_UNDER || 150_000;
     return OK if $memfree > $goodfree;
 
-    my @pids = (getppid(), get_sibling_pids());
+    unless ($ppid) {
+        my $self = pid_info($$);
+        $ppid = $self->[3];
+    }
+
+    my $pids = child_info($ppid);
+    my @pids = keys %$pids;
+
     $gtop ||= GTop->new;
 
     my %stats;
@@ -61,71 +51,46 @@ sub handler
         $sum_uniq += $stats{$pid}->[0];
     }
 
-    @pids = sort { $stats{$a}->[0] <=> $stats{$b}->[0] } @pids;
+    @pids = (sort { $stats{$b}->[0] <=> $stats{$a}->[0] } @pids, 0, 0);
 
-    if (grep { $$ == $_ } @pids[-2..-1]) {
+    my $my_pid = $$;
+    if (grep { $my_pid == $_ } @pids[0,1]) {
         my $my_use_k = $stats{$$}[0] >> 10;
         $r->log_error("Suicide [$$]: system memory free = ${memfree}k; i'm big, using ${my_use_k}k") if $LJ::DEBUG{'suicide'};
         Apache::LiveJournal::db_logger($r) unless $r->pnotes('did_lj_logging');
-        CORE::exit();
+        $r->child_terminate;
     }
 
     return OK;
 }
 
-1;
+sub pid_info {
+    my $pid = shift;
 
-__DATA__
-__C__
-
-extern unsigned char *ap_scoreboard_image;
-
-/* 
- * the following structure is for Linux on i32 ONLY! It makes certan
- * choices where apache's scoreboard.h has #ifdef's. See scoreboard.h
- * for real declarations, here we only name a few things we actually need.
- */
-
-/* total length of struct should be 164 bytes */
-typedef struct {
-    int foo1;
-    short foo2;
-    unsigned char status;
-    int foo3[39];
-} short_score;
-
-/* length should be 16 bytes */
-typedef struct {
-    int pid;
-    int foo[3];
-} parent_score;
-
-static int hard_limit = 512; /* array size on debian */
-
-/* 
- * Scoreboard is laid out like this: array of short_score structs,
- * then array of parent_score structs, then one int, the generation
- * number. Both arrays are of size HARD_SERVERS_LIMIT, 256 by default
- * on Unixes.
- */
-
-
-void get_sibling_pids () {
-    int i;
-    short_score *ss;
-    parent_score *ps;
-    Inline_Stack_Vars;
-
-    ss = (short_score *)ap_scoreboard_image;
-    ps = (parent_score *) ((unsigned char *)ap_scoreboard_image + sizeof(short_score)*hard_limit);
-
-    Inline_Stack_Reset;
-    for (i=0; i<hard_limit; i++) {
-        /* ready or busy */
-        if(ss[i].status >= 2) {
-            Inline_Stack_Push(newSViv(ps[i].pid));
-        }
-    }
-    Inline_Stack_Done;
-    return;
+    open (F, "/proc/$pid/stat") or next;
+    $_ = <F>;
+    close(F);
+    my @f = split;
+    return \@f;
 }
+
+sub child_info {
+    my $ppid = shift;
+    opendir(D, "/proc") or return undef;
+    my @pids = grep { /^\d+$/ } readdir(D);
+    closedir(D);
+
+    my %ret;
+    foreach my $p (@pids) {
+        next if (defined $known_parent{$p} &&
+                 $known_parent{$p} != $ppid);
+        my $ary = pid_info($p);
+        my $this_ppid = $ary->[3];
+        $known_parent{$p} = $this_ppid;
+        next unless $this_ppid == $ppid;
+        $ret{$p} = $ary;
+    }
+    return \%ret;
+}
+
+1;
