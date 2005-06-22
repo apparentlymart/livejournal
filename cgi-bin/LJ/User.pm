baeb53187969a2fc30ae8f895040960eb1224c0b
David Recordon <recordond@gmail.com>
2005-06-22 20:23:27 +0000

    Use rel='nofollow' on identity offsite profile links
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@5095 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -629,7 +629,7 @@ sub ljuser_display {
         $url = LJ::ehtml($url);
         $name = LJ::ehtml($name);
 
-        return "<span class='ljuser' style='white-space: nowrap;'><a href='$LJ::SITEROOT/userinfo.bml?userid=$u->{userid}&amp;t=I$andfull'><img src='$img/openid-profile.gif' alt='[info]' width='16' height='16' style='vertical-align: bottom; border: 0;' /></a><a href='$url'><b>$name</b></a></span>";
+        return "<span class='ljuser' style='white-space: nowrap;'><a href='$LJ::SITEROOT/userinfo.bml?userid=$u->{userid}&amp;t=I$andfull'><img src='$img/openid-profile.gif' alt='[info]' width='16' height='16' style='vertical-align: bottom; border: 0;' /></a><a href='$url' rel='nofollow'><b>$name</b></a></span>";
 
     } else {
         return "<b>????</b>";
