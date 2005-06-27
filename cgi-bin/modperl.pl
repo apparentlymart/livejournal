f7843ae5429af74bc8d2e11bbe08ad2cf8d2e47e
Brad Fitzpatrick <brad@danga.com>
2005-06-27 23:16:43 +0000

    always note library modtime (for diagnostic tools)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@5134 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/modperl.pl
+++ cgi-bin/modperl.pl
@@ -17,14 +17,12 @@ LJ::ModPerl::setup_restart();
 delete $INC{"$ENV{'LJHOME'}/cgi-bin/modperl.pl"};
 
 # remember modtime of all loaded libraries
-if ($LJ::IS_DEV_SERVER) {
-    %LJ::LIB_MOD_TIME = ();
-    while (my ($k, $file) = each %INC) {
-        next if $LJ::LIB_MOD_TIME{$file};
-        next unless $file =~ m!^\Q$LJ::HOME\E!;
-        my $mod = (stat($file))[9];
-        $LJ::LIB_MOD_TIME{$file} = $mod;
-    }
+%LJ::LIB_MOD_TIME = ();
+while (my ($k, $file) = each %INC) {
+    next if $LJ::LIB_MOD_TIME{$file};
+    next unless $file =~ m!^\Q$LJ::HOME\E!;
+    my $mod = (stat($file))[9];
+    $LJ::LIB_MOD_TIME{$file} = $mod;
 }
 
 # compatibility with old location of LJ::email_check:
