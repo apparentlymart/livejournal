711a979944bbfc49f6a202ad7c6f17a83137b3dd
Brad Fitzpatrick <brad@danga.com>
2005-08-30 22:47:26 +0000

    [int: javascript security fix]
    has to be doubly escaped.  first js, then html.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@5401 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -879,7 +879,7 @@ sub make_qr_link
 
         $basesubject =~ s/^(Re:\s*)*//i;
         $basesubject = "Re: $basesubject" if $basesubject;
-        $basesubject = LJ::ejs($basesubject);
+        $basesubject = LJ::ehtml(LJ::ejs($basesubject));
         my $onclick = "return quickreply(\"$dtid\", $pid, \"$basesubject\")";
         return "<a onclick='$onclick' href='$replyurl' >$linktext</a>";
     } else { # QR Disabled
