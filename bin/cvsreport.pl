022800b71b7590432f8875fa454c0b5f79eb4010
David Recordon <recordond@gmail.com>
2005-09-27 23:23:53 +0000

    Don't let you run LJ's cvsreport.pl if you're in FBHOME
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@5589 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/cvsreport.pl
+++ bin/cvsreport.pl
@@ -7,6 +7,10 @@ unless (-d $ENV{'LJHOME'}) {
     die "\$LJHOME not set.\n";
 }
 
+if (defined $ENV{'FBHOME'} && $ENV{'PWD'} =~ /^$ENV{'FBHOME'}/i) {
+    die "You are running this LJ script while working in FBHOME";
+}
+
 # strip off paths beginning with LJHOME
 # (useful if you tab-complete filenames)
 $_ =~ s!\Q$ENV{'LJHOME'}\E/?!! foreach (@ARGV);
