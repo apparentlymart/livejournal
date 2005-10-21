32e95cb3c0e1746d02d4baa95063a18aa9bc6989
David Recordon <recordond@gmail.com>
2005-10-21 19:16:54 +0000

    No poll for now
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@5787 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -1051,7 +1051,7 @@ sub entry_form {
         $show .= "<select name='insobjsel' id='insobjsel' onchange='InOb.handleInsertSelect()'>";
         $show .= "<option value='insert' style='padding-left: 20px'>Insert...</option>";
         $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_image.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>Image</option>";
-        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_poll.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>Poll</option>";
+#        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_poll.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>Poll</option>";
         $show .= "</select>";
 
         $insobjout = "<script> if (document.getElementById) { document.write(\"" . LJ::ejs($show) . "\"); } </script>";
