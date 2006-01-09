3ac328b625a2df357492d0a4c2d11a3c488bd04e
David Recordon <recordond@gmail.com>
2006-01-09 23:16:55 +0000

    fallback=bml is temporary, so instead create format=light which means the viewer wants a page that is largely text for use in a browser such as lynx or a mobile device.  Also persist this option in links on talkread and talkpost.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6017 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -3213,7 +3213,7 @@ sub make_journal
     }
 
     # signal to LiveJournal.pm that we can't handle this
-    if (($stylesys == 1 || $geta->{'fallback'} eq 'bml') && (({ entry=>1, reply=>1, month=>1, tag=>1 }->{$view}) || ($view eq 'lastn' && $geta->{tag}))) {
+    if (($stylesys == 1 || $geta->{'format'} eq 'light') && (({ entry=>1, reply=>1, month=>1, tag=>1 }->{$view}) || ($view eq 'lastn' && $geta->{tag}))) {
 
         # pick which fallback method (s2 or bml) we'll use by default, as configured with
         # $S1_SHORTCOMINGS
@@ -3229,6 +3229,14 @@ sub make_journal
         # type won't let them, or they really want this to fallback to bml
         if ($remote && $geta->{'style'} eq 'mine') {
             $fallback = 'bml';
+        }
+
+        # If they specified ?format=light, it means they want a page easy
+        # to deal with text-only or on a mobile device.  For now that means
+        # render it in the lynx site scheme.
+        if ($geta->{'format'} eq 'light') {
+            $fallback = 'bml';
+            $r->notes('bml_use_scheme' => 'lynx');
         }
 
         # there are no BML handlers for these views, so force s2
