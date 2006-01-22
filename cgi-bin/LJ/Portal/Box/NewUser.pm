c1e27b088b668986c06e4fbc6df6b5d6270274c8
Brad Fitzpatrick <brad@danga.com>
2006-01-22 21:03:31 +0000

    right links
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6163 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Portal/Box/NewUser.pm
+++ cgi-bin/LJ/Portal/Box/NewUser.pm
@@ -14,22 +14,25 @@ sub generate_content {
     my $pboxid = $self->pboxid;
     my $u = $self->{'u'};
 
+    my $profile_url = $u->profile_url;
+    my $base = $u->journal_base;
+
     $content .= qq {
         <table style="width: 100%; border: 0px;">
             <tr>
                 <td style="width: 50%;">
                     1) <a href="$LJ::SITEROOT/update.bml">Write</a> a journal entry<br />
                     2) <a href="$LJ::SITEROOT/editpics.bml">Upload</a> userpics<br />
-                    3) <a href="$LJ::SITEROOT/editinfo.bml">Fill out</a> your <a href="$LJ::SITEROOT/userinfo.bml?user=$u->{user}">userinfo</a> page<br />
+                    3) <a href="$LJ::SITEROOT/editinfo.bml">Fill out</a> your <a href="$profile_url">userinfo</a> page<br />
                 </td>
                 <td style="width: 50%;">
-                     4) <a href="$LJ::SITEROOT/customize">Customize</a> the look of your journal<br />
+                     4) <a href="$LJ::SITEROOT/customize/">Customize</a> the look of your journal<br />
                      5) <a href="$LJ::SITEROOT/interests.bml">Find</a> friends and communities by interests<br />
-                     6) <a href="$LJ::SITEROOT/users/$u->{user}/friends">Read</a> your Friends page<br />
+                     6) <a href="$base/friends">Read</a> your Friends page<br />
                 </td>
             </tr>
         </table>
-    <span class="NewUserMoreLink"><a href="$LJ::SITEROOT/manage">more</a></span>
+    <span class="NewUserMoreLink"><a href="$LJ::SITEROOT/manage/">more</a></span>
     };
 
     return $content;
