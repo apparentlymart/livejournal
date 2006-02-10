261ee149d5017197cc0d67b6e25961b4fee2a3db
Janine Costanzo <jcostanzo@iparadigms.com>
2006-02-10 23:30:49 +0000

    * use new BML::register_block() dynamic ability to define LJUSER, LJCOMM,   LJUSERF, and NEEDLOGIN
    JUNIOR JANINE TEAM EXTREME
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6363 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/lj-bml-blocks.pl
+++ cgi-bin/lj-bml-blocks.pl
@@ -12,6 +12,23 @@ BML::register_block("ADMIN_EMAIL", "S", $LJ::ADMIN_EMAIL);
 BML::register_block("SUPPORT_EMAIL", "S", $LJ::SUPPORT_EMAIL);
 BML::register_block("CHALRESPJS", "", $LJ::COMMON_CODE{'chalresp_js'});
 
+# dynamic blocks to implement calling our ljuser function to generate HTML
+#    <?ljuser banana ljuser?>
+#    <?ljcomm banana ljcomm?>
+#    <?ljuserf banana ljuserf?>
+BML::register_block("LJUSER", "DS", sub { LJ::ljuser($_[0]->{DATA}); });
+BML::register_block("LJCOMM", "DS", sub { LJ::ljuser($_[0]->{DATA}); });
+BML::register_block("LJUSERF", "DS", sub { LJ::ljuser($_[0]->{DATA}, { full => 1 }); });
+
+# dynamic needlogin block, needs to be dynamic so we can get at the full URLs and
+# so we can translate it
+BML::register_block("NEEDLOGIN", "", sub {
+    return "<?h1 <?_ml bml.needlogin.head _ml?> h1?>\n<?p " .
+           BML::ml('bml.needlogin.body4', { aopts1 => "href='$LJ::SITEROOT/login.bml?ret=1'",
+                                            aopts2 => "href='$LJ::SITEROOT/create.bml'" }) .
+           " p?>";
+});
+
 {
     my $dl = "<a href=\"$LJ::SITEROOT/files/%%DATA%%\">HTTP</a>";
     BML::register_block("DL", "DR", $dl);
