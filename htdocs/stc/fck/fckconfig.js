e47877d8e8e47ab9c8aac9c8fc1ca4e622e08d99
David Recordon <recordond@gmail.com>
2006-02-10 01:52:23 +0000

    - Don't have <script> tags as "protected"  - Convert \n to <br /> before invoking the editor
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6356 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/stc/fck/fckconfig.js
+++ htdocs/stc/fck/fckconfig.js
@@ -38,7 +38,7 @@ FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
 // FCKConfig.Plugins.Add( 'placeholder', 'de,en,fr,it,pl' ) ;
 FCKConfig.Plugins.Add('livejournal');
 
-FCKConfig.ProtectedSource.Add( /<script[\s\S]*?\/script>/gi ) ;	// <SCRIPT> tags.
+// FCKConfig.ProtectedSource.Add( /<script[\s\S]*?\/script>/gi ) ;	// <SCRIPT> tags.
 // FCKConfig.ProtectedSource.Add( /<%[\s\S]*?%>/g ) ;	// ASP style server side code <%...%>
 // FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code <?...?>
 // FCKConfig.ProtectedSource.Add( /(<asp:[^\>]+>[\s|\S]*?<\/asp:[^\>]+>)|(<asp:[^\>]+\/>)/gi ) ;	// ASP.Net style tags <asp:control>
