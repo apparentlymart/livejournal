a231cdd73b8101922052670ab504b9bb8e59dc27
Janine Costanzo <jcostanzo@iparadigms.com>
2006-03-15 00:13:50 +0000

    English stripping of the "Insert" drop-down on /update.bml.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6519 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -1113,9 +1113,9 @@ sub entry_form {
         my $show;
 
         $show .= "<select name='insobjsel' id='insobjsel' onchange='InOb.handleInsertSelect()'>";
-        $show .= "<option value='insert' style='padding-left: 20px'>Insert...</option>";
-        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_image.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>Image</option>";
-#        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_poll.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>Poll</option>";
+        $show .= "<option value='insert' style='padding-left: 20px'>$BML::ML{'entryform.insert.header'}</option>";
+        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_image.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>$BML::ML{'entryform.insert.image'}</option>";
+#        $show .= "<option value='image' style='background: url($LJ::IMGPREFIX/insobj_poll.gif) no-repeat; background-color: #fff; background-position: 0px 1px; padding-left: 20px;'>$BML::ML{'entryform.insert.poll'}</option>";
         $show .= "</select>";
 
         $insobjout = "<script> if (document.getElementById) { document.write(\"" . LJ::ejs($show) . "\"); } </script>";
