ce07056e61fa7607cca73ac66ec6d00411851c1e
Brad Whitaker <brad.whitaker@gmail.com>
2006-04-25 17:26:44 +0000

    -- new sub SMS notification module using LJ::SMS
    (need to factor out some of this duplicated code if
     it turns out that most modules will be $u-based)
    
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@6932 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/NotificationMethod/SMS.pm
@@ -0,0 +1,72 @@
+package LJ::NotificationMethod::SMS;
+
+use strict;
+use Carp qw/ croak /;
+use base 'LJ::NotificationMethod';
+
+sub can_digest { 0 };
+
+# positional parameters: ->new($u, $event1, $event2, ...)
+sub new {
+    my $class = shift;
+    my $u = shift;
+    croak "invalid user object passed"
+        unless LJ::isu($u);
+
+    my $self = { u => $u };
+
+    return bless $self, $class;
+}
+
+sub title { 'SMS Notification' }
+
+sub new_from_subscription {
+    my $class = shift;
+    my $subs = shift;
+
+    return $class->new($subs->owner);
+}
+
+sub u {
+    my $self = shift;
+    croak "'u' is an object method"
+        unless ref $self eq __PACKAGE__;
+
+    if (my $u = shift) {
+        croak "invalid 'u' passed to setter"
+            unless LJ::isu($u);
+
+        $self->{u} = $u;
+    }
+    croak "superfluous extra parameters"
+        if @_;
+
+    return $self->{u};
+}
+
+# notify a single event
+sub notify {
+    my $self = shift;
+    croak "'notify' is an object method"
+        unless ref $self eq __PACKAGE__;
+
+    my $u = $self->u;
+
+    croak "'notify' requires an event"
+        unless @_;
+
+    my $ev = shift @events;
+        croak "invalid event passed" 
+            unless ref $ev;
+
+    croak "SMS can only accept one event at a time"
+        if @_;
+
+    my $sms_obj = LJ::SMS->new
+        ( to   => $u,
+          text => $ev->as_sms );
+    $sms->send;
+
+}
+
+1;
