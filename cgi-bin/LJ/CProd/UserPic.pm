d1baec059c9545e4b4d546db415724dd4ce01a0f
Beth Coffelt <bcoffelt@sixapart.com>
2006-05-09 23:51:09 +0000

    Userpic prodding
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7147 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/CProd/UserPic.pm
@@ -0,0 +1,16 @@
+package LJ::CProd::UserPic;
+use base 'LJ::CProd';
+
+sub applicable {
+    my ($class, $u) = @_;
+    return 0 if ($u->{defaultpicid});
+    return 1;
+}
+
+sub render {
+    my ($class, $u) = @_;
+    return "<p><div style=\"float: left; padding: 5px;\"><img border=\"1\" src=\"$LJ::SITEROOT/img/userpics.gif\" /></div>". LJ::ljuser($u) . ", did you know you can have a ". 
+        $class->clickthru_link("$LJ::SITEROOT/editpics.bml","userpic") . " to compliment your entries?</p>";
+}
+
+1;
