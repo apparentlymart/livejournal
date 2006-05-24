9b4170e3b2874cfe57a22f57215930154623359d
Beth Coffelt <bcoffelt@sixapart.com>
2006-05-24 21:03:53 +0000

    Forgot to commit Dystopia Nav class in previous commit.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7374 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/CProd/DystopiaNav.pm
@@ -0,0 +1,21 @@
+package LJ::CProd::DystopiaNav;
+use base 'LJ::CProd';
+
+sub applicable {
+    my ($class, $u) = @_;
+    return 0 if $u->in_class('paid') || $u->in_class('perm');
+    return 1;
+}
+
+sub render {
+    my ($class, $u, $version) = @_;
+    my $user = LJ::ljuser($u);
+    my $link = $class->clickthru_link('cprod.dystopia.nav.link', $version, style => 'class="navlinks"');
+    return BML::ml($class->get_ml($version), { "user" => $user, "link" => $link });
+}
+
+sub ml { 'cprod.dystopia.nav.text' }
+sub link { "$LJ::SITEROOT/manage/payments/" }
+sub button_text { "Upgrade" }
+
+1;
