81fa38ff82f2d8cc22e7c84811bc8f67d3597a77
Beth Coffelt <bcoffelt@sixapart.com>
2006-05-30 17:53:12 +0000

    Moved CProd class for FriendsFriendsInline out of ljcom
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7446 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/CProd/FriendsFriendsInline.pm
@@ -0,0 +1,24 @@
+package LJ::CProd::FriendsFriendsInline;
+use base 'LJ::CProd';
+
+sub applicable {
+    my ($class, $u) = @_;
+    return 0 if LJ::get_cap($u, "friendsfriendsview");
+    return 1;
+}
+
+sub render {
+    my ($class, $u, $version) = @_;
+    my $user = LJ::ljuser($u);
+
+    my $link = $class->clickthru_link('cprod.friendsfriendsinline.link2', $version);
+
+    return "<p>$icon ".BML::ml($class->get_ml($version), { "user" => $user,
+                                                 "link" => $link }) . "</p>";
+
+}
+
+sub ml { 'cprod.friendsfriendsinline.text2' }
+sub link { "$LJ::SITEROOT/paidaccounts/" }
+
+1;
