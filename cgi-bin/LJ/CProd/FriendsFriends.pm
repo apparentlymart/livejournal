daed4b42ab2fea2b1acd336dcb79a7dbd06a87e3
Beth Coffelt <bcoffelt@sixapart.com>
2006-05-31 17:09:00 +0000

    Moved FriendsFriends out of ljcom
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7466 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/CProd/FriendsFriends.pm
@@ -0,0 +1,31 @@
+package LJ::CProd::FriendsFriends;
+use base 'LJ::CProd';
+
+sub applicable {
+    my ($class, $u) = @_;
+    return 0 unless LJ::get_cap($u, "friendsfriendsview");
+    return 1;
+}
+
+sub render {
+    my ($class, $u, $version) = @_;
+    my $user = LJ::ljuser($u);
+
+    my $icon = "<div style=\"float: left; padding: 5px;\">
+               <img border=\"1\" src=\"$LJ::SITEROOT/img/friendgroup.gif\" /></div>";
+    my $link = $class->clickthru_link('cprod.friendsfriends.link', $version);
+
+    return "<p>$icon ".BML::ml($class->get_ml($version), { "user" => $user,
+                                                 "link" => $link }) . "</p>";
+
+}
+
+sub ml { 'cprod.friendsfriends.text' }
+sub link {
+    my $remote = LJ::get_remote()
+        or return "$LJ::SITEROOT/login.bml";
+    return $remote->friendsfriends_url . "/";
+}
+sub button_text { "Friends of Friends" }
+
+1;
