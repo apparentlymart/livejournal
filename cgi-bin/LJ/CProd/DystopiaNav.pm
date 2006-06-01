ee4a6a2ef5208ea609e5ff31fd399c4ad079883c
Beth Coffelt <bcoffelt@sixapart.com>
2006-06-01 18:12:39 +0000

    Copy changes as per RT #1940
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7499 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/CProd/DystopiaNav.pm
+++ cgi-bin/LJ/CProd/DystopiaNav.pm
@@ -9,12 +9,20 @@ sub applicable {
 
 sub render {
     my ($class, $u, $version) = @_;
+
+    # versions based on class
+    if ($u->in_class('plus')) {
+        $version = 2;
+    } else {
+        $version = 1;
+    }
+
     my $user = LJ::ljuser($u);
-    my $link = $class->clickthru_link('cprod.dystopia.nav.link2', $version, style => 'class="navlinks"');
+    my $link = $class->clickthru_link('cprod.dystopia.nav.link3', $version, style => 'class="navlinks"');
     return BML::ml($class->get_ml($version), { "user" => $user, "link" => $link });
 }
 
-sub ml { 'cprod.dystopia.nav.text' }
+sub ml { 'cprod.dystopia.nav.text2' }
 sub link { "$LJ::SITEROOT/manage/payments/modify.bml" }
 sub button_text { "Upgrade" }
 
