ea240229df6f101810f9d5f604fa9f6b9955c251
Beth Coffelt <bcoffelt@sixapart.com>
2006-06-01 18:58:05 +0000

    Copy changes as per RT#1928
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7501 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/CProd/TextMessaging.pm
+++ cgi-bin/LJ/CProd/TextMessaging.pm
@@ -10,13 +10,15 @@ sub applicable {
 sub render {
     my ($class, $u, $version) = @_;
     my $user = LJ::ljuser($u);
-    my $link = $class->clickthru_link('cprod.textmessaging.link', $version);
 
-    return "<p>" . BML::ml($class->get_ml($version), { "user" => $user, "link" => $link }) . "</p>";
+    my $link = $class->clickthru_link('cprod.textmessaging.link', $version);
+    my $link2 = BML::ml('cprod.textmessaging.link2-2.v'.$version, { aopts => "href='$LJ::SITEROOT/manage/payments/'" });
+
+    return "<p>" . BML::ml($class->get_ml($version), { "user" => $user, "link" => $link, "link2" => $link2 }) . "</p>";
 
 }
 
-sub ml { 'cprod.textmessaging.text' }
+sub ml { 'cprod.textmessaging.text2' }
 sub link { "$LJ::SITEROOT/manage/payments/modify.bml" }
 sub button_text { "Upgrade" }
 
