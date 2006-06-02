1fff7cf6419b1f7835f3a55fef9c8ad70674ea7a
Brad Fitzpatrick <brad@danga.com>
2006-06-02 20:22:14 +0000

    LJ::Setting::BoolSetting base class to remove duplicated code and ease writing of new boolean setting plugins.
    also adds a _all tag to view all settings, mostly for debuggin
    
    
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7524 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Setting/MangleEmail.pm
+++ cgi-bin/LJ/Setting/MangleEmail.pm
@@ -1,30 +1,23 @@
 package LJ::Setting::MangleEmail;
-use base 'LJ::Setting';
+use base 'LJ::Setting::BoolSetting';
 use strict;
 use warnings;
 
 sub tags { qw(email hide obscure mangle spam) }
 
-sub as_html {
-    my ($class, $u, $errs) = @_;
-    my $key = $class->pkgkey;
+sub label {
     local $BML::ML_SCOPE = "/editinfo.bml";
-    my $ret = LJ::html_check({ 'type' => 'check', 'name' => "${key}opt_mangleemail",
-                               'id' => 'opt_mangleemail',
-                               'selected' => $u->prop("opt_mangleemail") eq 'Y' });
-    $ret .= $class->errdiv($errs, "opt_mangleemail");
-    $ret .= " <label for='opt_mangleemail'>$BML::ML{'.mangleaddress.header'}</label><br />";
-    $ret .= $BML::ML{'.mangleaddress.about'};
-    return $ret;
+    return $BML::ML{'.mangleaddress.header'};
 }
 
-sub save {
-    my ($class, $u, $args) = @_;
-    my $arg = $args->{opt_mangleemail} ? 'Y' : 'N';
-    return 1 if $arg eq ($u->prop('opt_mangleemail') || 'N');
-    $class->errors("opt_mangleemail" => "Invalid option")  unless $arg =~ /^[YN]$/;
-    LJ::update_user($u, { opt_mangleemail => $arg} );
+sub des {
+    local $BML::ML_SCOPE = "/editinfo.bml";
+    return $BML::ML{'.mangleaddress.about'};
 }
+
+sub user_field { "opt_mangleemail" }
+sub checked_value { "Y" }
+sub unchecked_value { "N" }
 
 1;
 
