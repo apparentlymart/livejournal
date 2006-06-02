3de6f0802f2bfd3fa72a4746f2fb8bedbf877f02
Beth Coffelt <bcoffelt@sixapart.com>
2006-06-02 22:17:46 +0000

    EditPics needed get_cap
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7528 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/CProd/EditPicsMax.pm
+++ cgi-bin/LJ/CProd/EditPicsMax.pm
@@ -3,7 +3,11 @@ use base 'LJ::CProd';
 
 sub applicable {
     my ($class, $u) = @_;
-    return 0 if $u->in_class('paid') || $u->in_class('perm');
+    my $bit = $LJ::Pay::bonus{'userpic'}->{'cap'};
+    my $has_cap = $u->{'caps'} & 1 << $bit;
+    my $userpicsaddon = defined $bit && $has_cap;
+
+    return 0 if $userpicsaddon;
     return 1;
 }
 
@@ -11,18 +15,36 @@ sub render {
     my ($class, $u, $version) = @_;
     my $user = LJ::ljuser($u);
 
+    my $free = 1 << LJ::class_bit('free');
+    my $plus = 1 << LJ::class_bit('plus');
+    my $paid = 1 << LJ::class_bit('paid');
+    my $freenum = LJ::get_cap($free, 'userpics', { no_hook => 1 });
+    my $plusnum = LJ::get_cap($plus, 'userpics', { no_hook => 1 });
+    my $paidnum = LJ::get_cap($paid, 'userpics', { no_hook => 1 });
+
+    my $num = LJ::get_cap($u, 'userpics', { no_hook => 1 });
+
     # versions based on class
-    $version = 1 if $u->in_class('free');
-    $version = 2 if $u->in_class('plus');
+    if ($num == $freenum) {
+        $version = 1;
+    } elsif ($num == $plusnum) {
+        $version = 2;
+    } elsif ($num == $paidnum) {
+        $version = 3;
+    } else {
+        $version = 5;
+    }
 
     my $link = $class->clickthru_link('cprod.editpicsmax.link', $version);
-    my $link2 = BML::ml('cprod.editpicsmax.link2-2.v'.$version, { aopts => "href='$LJ::SITEROOT/manage/payments/'" });
+    my $link2 = BML::ml('cprod.editpicsmax.link2-3.v'.$version, { aopts => "href='$LJ::SITEROOT/manage/payments/'" });
+    my $link3 = BML::ml('cprod.editpicsmax.link3.v'.$version, { aopts => "href='$LJ::SITEROOT/shop/view.bml?item=userpics'" });
 
-    return "<p>" . BML::ml($class->get_ml($version), { "user" => $user, "link" => $link, "link2" => $link2 }) . "</p>";
 
+
+    return "<p>" . BML::ml($class->get_ml($version), { "user" => $user, "link" => $link, "link2" => $link2, "link3" => $link3, "num" => $num, "plusnum" => $plusnum, "paidnum" => $paidnum }) . "</p>";
 }
 
-sub ml { 'cprod.editpicsmax.text2' }
+sub ml { 'cprod.editpicsmax.text5' }
 sub link { "$LJ::SITEROOT/manage/payments/modify.bml" }
 sub button_text { "Upgrade" }
 
