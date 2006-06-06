427eae56eca6c9eb7ac435761f0f79a8649570a7
Brad Fitzpatrick <brad@danga.com>
2006-06-06 17:47:54 +0000

    support detecting when keys can't be used, and removing them when doing web access logs.
    this lets us automatically work with either mysql 4.x or mysql 5 with archive which can't have keys.
    
    
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7560 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/LiveJournal.pm
+++ cgi-bin/Apache/LiveJournal.pm
@@ -259,7 +259,7 @@ sub trans
             $uniq ||= LJ::rand_chars(15);
 
             my $uniq_value = "$uniq:$now";
-            $uniq_value    = LJ::run_hook('transform_ljuniq_value', 
+            $uniq_value    = LJ::run_hook('transform_ljuniq_value',
                                           { value => $uniq_value,
                                             extra => $uniq_extra }) || $uniq_value;
 
@@ -1534,52 +1534,56 @@ sub db_logger
     my $table = sprintf("access%04d%02d%02d%02d", $now[5]+1900,
                         $now[4]+1, $now[3], $now[2]);
 
-    unless ($LJ::CACHED_LOG_CREATE{"$table"}++) {
-        my $sql = "(".
-                 "whn TIMESTAMP(14) NOT NULL,".
-                 "INDEX(whn),".
-                 "server VARCHAR(30),".
-                 "addr VARCHAR(15) NOT NULL,".
-                 "ljuser VARCHAR(15),".
-                 "remotecaps INT UNSIGNED,".
-                 "journalid INT UNSIGNED,". # userid of what's being looked at
-                 "journaltype CHAR(1),".   # journalid's journaltype
-                 "remoteid INT UNSIGNED,". # remote user's userid
-                 "codepath VARCHAR(80),".  # protocol.getevents / s[12].friends / bml.update / bml.friends.index
-                 "anonsess INT UNSIGNED,".
-                 "langpref VARCHAR(5),".
-                 "uniq VARCHAR(15),".
-                 "method VARCHAR(10) NOT NULL,".
-                 "uri VARCHAR(255) NOT NULL,".
-                 "args VARCHAR(255),".
-                 "status SMALLINT UNSIGNED NOT NULL,".
-                 "ctype VARCHAR(30),".
-                 "bytes MEDIUMINT UNSIGNED NOT NULL,".
-                 "browser VARCHAR(100),".
-                 "clientver VARCHAR(100),".
-                 "secs TINYINT UNSIGNED,".
-                 "ref VARCHAR(200),".
-                 "pid SMALLINT UNSIGNED,".
-                 "cpu_user FLOAT UNSIGNED,".
-                 "cpu_sys FLOAT UNSIGNED,".
-                 "cpu_total FLOAT UNSIGNED,".
-                 "mem_vsize INT,".
-                 "mem_share INT,".
-                 "mem_rss INT,".
-                 "mem_unshared INT) DELAY_KEY_WRITE = 1";
+    if ($dbl && ! $LJ::CACHED_LOG_CREATE{"$table"}++) {
+        my $index = "INDEX(whn),";
+        my $delaykeywrite = "DELAY_KEY_WRITE = 1";
+        my $sql;
+        my $gen_sql = sub {
+            $sql = "(".
+                "whn TIMESTAMP(14) NOT NULL, $index".
+                "server VARCHAR(30),".
+                "addr VARCHAR(15) NOT NULL,".
+                "ljuser VARCHAR(15),".
+                "remotecaps INT UNSIGNED,".
+                "journalid INT UNSIGNED,". # userid of what's being looked at
+                "journaltype CHAR(1),".   # journalid's journaltype
+                "remoteid INT UNSIGNED,". # remote user's userid
+                "codepath VARCHAR(80),".  # protocol.getevents / s[12].friends / bml.update / bml.friends.index
+                "anonsess INT UNSIGNED,".
+                "langpref VARCHAR(5),".
+                "uniq VARCHAR(15),".
+                "method VARCHAR(10) NOT NULL,".
+                "uri VARCHAR(255) NOT NULL,".
+                "args VARCHAR(255),".
+                "status SMALLINT UNSIGNED NOT NULL,".
+                "ctype VARCHAR(30),".
+                "bytes MEDIUMINT UNSIGNED NOT NULL,".
+                "browser VARCHAR(100),".
+                "clientver VARCHAR(100),".
+                "secs TINYINT UNSIGNED,".
+                "ref VARCHAR(200),".
+                "pid SMALLINT UNSIGNED,".
+                "cpu_user FLOAT UNSIGNED,".
+                "cpu_sys FLOAT UNSIGNED,".
+                "cpu_total FLOAT UNSIGNED,".
+                "mem_vsize INT,".
+                "mem_share INT,".
+                "mem_rss INT,".
+                "mem_unshared INT) $delaykeywrite";
+        };
 
-        if ($dbl) {
+        $gen_sql->();
+        $dbl->do("CREATE TABLE IF NOT EXISTS $table $sql");
+
+        # too many keys specified.  (archive table engine)
+        if ($dbl->err == 1069) {
+            $index = "";
+            $gen_sql->();
             $dbl->do("CREATE TABLE IF NOT EXISTS $table $sql");
-            $r->log_error("error creating log table ($table), perhaps due to old MySQL not supporting delayed key writes?  Error is: " .
-                          $dbl->errstr) if $dbl->err;
         }
 
-        foreach my $rec (@dinsertd_socks) {
-            my $sock = $rec->[1];
-            my $url = LJ::eurl("CREATE TABLE IF NOT EXISTS [tablename] $sql");
-            print $sock "SET_NOTE lj_create_table $url\r\n";
-            my $res = <$sock>;
-        }
+        $r->log_error("error creating log table ($table): Error is: " .
+                      $dbl->err . ": ". $dbl->errstr) if $dbl->err;
     }
 
     my $remote = eval { LJ::load_user($rl->notes('ljuser')) };
