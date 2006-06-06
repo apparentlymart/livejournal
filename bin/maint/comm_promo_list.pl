b82c7aa14791d920e4c829e18af53b90770555d5
Mischa Spiegelmock <revmischa@cpan.org>
2006-06-06 17:44:12 +0000

    Maint task to weight communities based on watchers CommPromo module to display promos and test
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7559 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ bin/maint/comm_promo_list.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/perl
+#
+
+use strict;
+use lib "$ENV{LJHOME}/cgi-bin";
+
+require "statslib.pl";
+
+$maint{comm_promo_list} = sub
+{
+    # block size is 1_000 except for testing
+    my $BLOCK_SIZE = $LJ::T_BLOCK_SIZE || 1_000;
+
+    my $get_dbslo = sub {
+        $LJ::DBIRole->clear_req_cache();
+        return LJ::get_dbh("slow")
+            or die "could not contact slow role";
+    };
+
+    my $dbslo = $get_dbslo->();
+
+    # first get a list of all communities
+    my @comm_uids = @{ $dbslo->selectcol_arrayref("SELECT userid FROM community") || [] };
+
+    # go through each group of 1000 communities and find the total number
+    # of watchers
+    my $ct = 0;
+    my $total_watchers = 0;
+    my %promo_comms = (); # hash of jid => member count
+    my @to_load = ();    # array of jids for which friends should be loaded
+
+    while (my @curr_uid = splice(@comm_uids, 0, $BLOCK_SIZE)) {
+        print STDERR "[" . ++$ct . "] processing " . scalar(@curr_uid) . " users...\n";
+
+        # get u objects
+        my $curr_u = LJ::load_userids(@curr_uid) || {};
+
+        # how many of these communities are plus or paid w/ opt-in?
+        while (my ($uid, $u) = each %$curr_u) {
+            next unless $u->{journaltype} eq 'C';
+            next unless $u->{statusvis} eq 'V';
+
+            # we care about users who are either sponsored plus or paid and have
+            # opted in
+            if ($u->in_class('paid')) {
+                next unless $u->prop('opt_comm_promo');
+            } elsif (! $u->in_class('plus')) {
+                next;
+            }
+
+            push @to_load, $u->{userid};
+        }
+    }
+
+    # now iterate over all the users whose friend counts we'd like to load,
+    # querying the slow role
+    while (my @curr_uid = splice(@to_load, 0, $BLOCK_SIZE)) {
+        $dbslo = $get_dbslo->();
+
+        my $bind = join(",", map { "?" } @curr_uid);
+        my $sth = $dbslo->prepare("SELECT friendid, COUNT(userid) FROM friends WHERE friendid IN ($bind) GROUP BY 1");
+        $sth->execute(@curr_uid);
+        die $dbslo->errstr if $dbslo->err;
+        $sth->{mysql_use_result} = 1;
+
+        while (my ($comm_id, $watcher_ct) = $sth->fetchrow_array) {
+            $promo_comms{$comm_id} += $watcher_ct;
+            $total_watchers += $watcher_ct;
+        }
+    }
+
+    # no div by zero
+    die "no community watchers found..."
+        unless $total_watchers > 0;
+
+    # now find what range in the table each community takes up
+    my $curr_pos = 0;
+    my @vals = ();
+    my $max_int = $LJ::MAX_32BIT_SIGNED;
+    while ( my ($jid, $watcher_ct) = each %promo_comms ) {
+        my $weight = POSIX::ceil($watcher_ct / $total_watchers * $max_int);
+
+        push @vals, ($jid, $curr_pos, $curr_pos + $weight);
+        $curr_pos += $weight + 1;
+    }
+
+    # now insert all values in batch
+    if (scalar @vals) {
+
+        my $dbh = LJ::get_db_writer()
+            or die "unable to contact global master";
+
+        $dbh->begin_work;
+        $dbh->do("DELETE FROM comm_promo_list");
+
+
+        my $bind = join(",", map { "(?,?,?)" } keys %promo_comms);
+        $dbh->do("INSERT INTO comm_promo_list (journalid, r_start, r_end) VALUES $bind", undef, @vals);
+        $dbh->commit;
+    }
+};
+
+1;
---
+++ cgi-bin/LJ/CommPromo.pm
@@ -0,0 +1,83 @@
+#
+# LiveJournal Community Promo
+#
+
+package LJ::CommPromo;
+
+use strict;
+use Carp qw ( croak );
+
+sub render_for_comm {
+    my ($class, $comm) = @_;
+
+    return $class->_render($comm);
+}
+
+# constructor given target community, loads a CommPromo object
+# which can then be rendered
+# Class method
+sub grab_promo {
+    my $class = shift;
+    my $comm  = LJ::want_user(shift)
+        or croak "Invalid community";
+
+    # choose a community promo to show
+    my $dbr = LJ::get_db_reader()
+        or die "No db reader";
+
+    my $jid;
+    for (1..6) {
+        # pick a random number between zero and 2^31 - 1
+        my $rand = int(rand($LJ::MAX_32BIT_SIGNED));
+
+        $jid = $dbr->selectrow_array
+            ("SELECT journalid FROM comm_promo_list " .
+             "WHERE r_start < ? AND r_end > ? AND journalid != ? LIMIT 1",
+             undef, $rand, $rand, $comm->{userid});
+
+        die $dbr->errstr if $dbr->err;
+        last if $jid;
+    }
+
+    return undef unless $jid;
+
+    return LJ::load_userid($jid);
+}
+
+sub _render {
+    my ($class, $comm) = @_;
+
+    return undef unless $comm;
+
+    # find which community to link to
+    my $target = $class->grab_promo($comm);
+    return undef unless $target;
+
+    # get the ljuser link
+    my $commljuser = $target->ljuser_display;
+
+    # link to journal
+    my $journal_base = $target->journal_base;
+
+    # get default userpic if any
+    my $userpic = $target->userpic;
+    my $userpic_html = '';
+    if ($userpic) {
+        my $userpic_url = $userpic->url;
+        $userpic_html = qq { <a href="$journal_base"><img src="$userpic_url" /></a> };
+    }
+
+    my $blurb = "bluby blurb";
+
+    my $html = qq {
+        <div class="CommunityPromoBox">
+            <div class="CommLink">$commljuser</div>
+            <div class="Userpic">$userpic_html</div>
+            <div class="Blurb">$blurb</div>
+        </div>
+    };
+
+    return $html;
+}
+
+1;
