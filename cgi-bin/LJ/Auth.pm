a14846629f4aa7bfc6c3ccf499ad636db79ba39b
Mischa Spiegelmock <revmischa@cpan.org>
2006-06-28 21:57:21 +0000

    Module for doing authentication stuff Add methods for creating and checking AJAX auth tokens
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7872 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Auth.pm
@@ -0,0 +1,40 @@
+# This is the LiveJournal Authentication module.
+# It contains useful authentication methods.
+
+package LJ::Auth;
+use strict;
+use Digest::HMAC_SHA1 qw(hmac_sha1_hex);
+use Digest::SHA1 qw(sha1_hex);
+use Carp qw (croak);
+
+# Generate an auth token for AJAX requests to use.
+# Arguments: ($remote, $action, %postvars)
+#   $remote: remote user object
+#   $uri: what uri this is for
+#   %postvars: the expected post variables
+# Returns: Auth token good for the current hour
+sub ajax_auth_token {
+    my ($class, $remote, $uri, %postvars) = @_;
+
+    $remote = LJ::want_user($remote) or croak "Invalid remote";
+    croak "No URI specified" unless $uri;
+
+    my ($stime, $secret) = LJ::get_secret();
+    my $postvars = join('&', map { $postvars{$_} } sort keys %postvars);
+    my $authstring = sha1_hex(qq {$stime:$secret:$remote->{userid}:$uri:$postvars});
+    return hmac_sha1_hex($authstring);
+}
+
+# Checks an auth token sent by an ajax request
+# Arguments: $remote, $uri, %POST variables
+# Returns: bool whether or not key is good
+sub check_ajax_auth_token {
+    my ($class, $remote, $uri, %postvars) = @_;
+
+    $remote = LJ::want_user($remote) or croak "Invalid remote";
+    my $authtoken = delete $postvars{auth_token} or return 0;
+
+    return $authtoken eq LJ::Auth->ajax_auth_token($remote, $uri, %postvars);
+}
+
+1;
