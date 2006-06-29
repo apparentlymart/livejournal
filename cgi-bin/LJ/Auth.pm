11369f7fb718d1124d62e52d2473c3da8f04dd70
Mischa Spiegelmock <revmischa@cpan.org>
2006-06-29 00:08:28 +0000

    Auth module v2.0 Explicit verification of individual components
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@7877 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Auth.pm
+++ cgi-bin/LJ/Auth.pm
@@ -16,14 +16,18 @@ use Carp qw (croak);
 sub ajax_auth_token {
     my ($class, $remote, $uri, %postvars) = @_;
 
-    $remote = LJ::want_user($remote) or croak "Invalid remote";
+    $remote = LJ::want_user($remote) || LJ::get_remote()
+        or croak "Invalid remote";
+
     croak "No URI specified" unless $uri;
 
     my ($stime, $secret) = LJ::get_secret();
     my $postvars = join('&', map { $postvars{$_} } sort keys %postvars);
     my $remote_session_id = $remote && $remote->session ? $remote->session->id : '';
-    my $authstring = sha1_hex(qq {$stime:$secret:$remote->{userid}:$remote_session_id:$uri:$postvars});
-    return hmac_sha1_hex($authstring);
+
+    my $chalbare = qq {ajax:$stime:$remote->{userid}:$remote_session_id:$uri:$postvars};
+    my $chalsig = sha1_hex($chalbare, $secret);
+    return qq{$chalbare:$chalsig};
 }
 
 # Checks an auth token sent by an ajax request
@@ -32,10 +36,38 @@ sub ajax_auth_token {
 sub check_ajax_auth_token {
     my ($class, $remote, $uri, %postvars) = @_;
 
-    $remote = LJ::want_user($remote) or croak "Invalid remote";
-    my $authtoken = delete $postvars{auth_token} or return 0;
+    $remote = LJ::want_user($remote) || LJ::get_remote()
+        or croak "Invalid remote";
 
-    return $authtoken eq LJ::Auth->ajax_auth_token($remote, $uri, %postvars);
+    # get auth token out of post vars
+    my $auth_token = delete $postvars{auth_token} or return 0;
+
+    # recompute post vars
+    my $postvars = join('&', map { $postvars{$_} } sort keys %postvars);
+
+    # get vars out of token string
+    my ($c_ver, $stime, $remoteid, $sessid, $chal_uri, $chal_postvars, $chalsig) = split(':', $auth_token);
+
+    # get secret based on $stime
+    my $secret = LJ::get_secret($stime);
+
+    # no time?
+    return 0 unless $stime && $secret;
+
+    # right version?
+    return 0 unless $c_ver eq 'ajax';
+
+    # do signitures match?
+    my $chalbare = qq {$c_ver:$stime:$remoteid:$sessid:$chal_uri:$chal_postvars};
+    my $realsig = sha1_hex($chalbare, $secret);
+    return 0 unless $realsig eq $chalsig;
+
+    # do other vars match?
+    return 0 unless $remoteid == $remote->{userid} &&
+        $remote && $remote->session && $sessid == $remote->session->id &&
+        $uri eq $chal_uri && $postvars eq $chal_postvars;
+
+    return 1;
 }
 
 1;
