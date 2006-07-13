cf12b283b490db2e1036a609151469e6e8c2eb62
Mischa Spiegelmock <revmischa@cpan.org>
2006-07-13 17:37:28 +0000

    hide notificationmethod checkboxes if sub is disabled
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8062 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -2658,7 +2658,7 @@ sub subscribe_interface {
 
             # print out notification options for this subscription (hidden if not subscribed)
             $cat_html .= "<td>&nbsp;</td>";
-            my $hidden = $subscribed ? '' : 'style="visibility: hidden;"';
+            my $hidden = $subscribed && $pending_sub->active ? '' : 'style="visibility: hidden;"';
 
             foreach my $note_class (@notify_classes) {
                 my $ntypeid = eval { $note_class->ntypeid } or next;
