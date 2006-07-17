73f5316063829f46d9e5018ddd061d4a1e1d800d
Mischa Spiegelmock <revmischa@cpan.org>
2006-07-17 18:19:15 +0000

    emails
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8086 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Event/CommunityJoinRequest.pm
+++ cgi-bin/LJ/Event/CommunityJoinRequest.pm
@@ -20,18 +20,50 @@ sub new {
 
 sub is_common { 0 }
 
+sub comm {
+    my $self = shift;
+    return LJ::load_userid($self->arg2);
+}
+
+sub requestor {
+    my $self = shift;
+    return LJ::load_userid($self->arg1);
+}
+
 sub as_html {
     my $self = shift;
     return sprintf("The user %s has requested to join the community %s.",
-                   LJ::load_userid($self->arg1)->ljuser_display,
-                   LJ::load_userid($self->arg2)->ljuser_display);
+                   $self->requestor->ljuser_display,
+                   $self->comm->ljuser_display);
 }
 
 sub as_string {
     my $self = shift;
     return sprintf("The user %s has requested to join the community %s.",
-                   LJ::load_userid($self->arg1)->display_username,
-                   LJ::load_userid($self->arg2)->display_username);
+                   $self->requestor->display_username,
+                   $self->comm->display_username);
+}
+
+sub as_email_subject {
+    my $self = shift;
+
+    return $self->comm->display_username . " Membership Request by " . $self->requestor->display_username;
+}
+
+sub as_email_string {
+    my $self = shift;
+
+    return sprintf "Dear %s,\n\n" .
+                   "The user \"%s\" has requested to join the \"%s\" community.  If you wish " .
+                   "to manage this community's outstanding requests, please click this link:\n\n" .
+                   "\t$LJ::SITEROOT/community/pending.bml?comm=%s\n\n" .
+                   "You may also ignore this e-mail.  The request to join will expire after a period of 30 days.\n\n" .
+                   "Regards,\n$LJ::SITENAME Team\n",
+
+                   $self->u->display_username,
+                   $self->requestor->display_username,
+                   $self->comm->name;
+
 }
 
 sub as_sms {
