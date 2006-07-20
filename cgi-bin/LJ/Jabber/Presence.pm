93c030b87b91eb90d91ac38e2b1e6246e63b2060
Jonathan Steinert <github@hachi.kuiki.net>
2006-07-20 04:36:00 +0000

    Add Jabber Presence API module for artur, props to mischa for help.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8137 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Jabber/Presence.pm
@@ -0,0 +1,305 @@
+package LJ::Jabber::Presence;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+
+use Carp qw(croak);
+
+use Digest::MD5 qw(md5_base64);
+use LJ::MemCache;
+
+=head1 PACKAGE METHODS
+
+=head2 $obj = LJ::Jabber::Presence->new( $u, $resource )
+
+Loads a Jabber presence object from Memcache, or failing that from the database.
+
+Requires two arguments, a LJ::User object (or something that's similar) and a resource
+(string) for the Jabber user instance
+
+=cut
+
+sub new {
+    my ($class, $u, $resource) = @_;
+
+    $u = LJ::want_user($u);
+
+    croak "No user" unless $u;
+    croak "No resource" unless $resource;
+
+    my $self = {
+	u        => $u,
+	resource => $resource,
+    };
+
+    bless $self, $class;
+
+    my %fields = $self->_load;
+
+    return undef unless scalar keys %fields;
+    %$self = (%$self, %fields);
+
+    return $self;
+}
+
+=head2 $obj = LJ::Jabber::Presence->create( %opts );
+
+Creates a Jabber::Presence object from %opts and saves it to the database.
+
+Options are as follows:
+
+=over
+
+=item u
+
+LJ::User object (or similar) representing the user this presence applies to
+
+=item resource
+
+String of Jabber resource for this user's particular presence.
+
+=item cluster
+
+LJ::Jabber::Cluster object representing the cluster node which this user has connected to.
+
+=item presence
+
+Raw XML string of presence data for this user's presence.
+
+=item flags
+
+Integer freeform flags field for storing anything you want. Please document it.
+
+=back
+
+=cut
+
+sub create {
+    my $class = shift;
+    my %opts = @_;
+
+    my $raw_u    = delete( $opts{u} )        or croak "No user";
+    my $resource = delete( $opts{resource} ) or croak "No resource";
+    my $cluster  = delete( $opts{cluster} )  or croak "No cluster";
+    my $presence = delete( $opts{presence} ) or croak "No presence";
+    my $flags    = delete( $opts{flags} ) || 0;
+
+    my $u = LJ::want_user( $raw_u );
+    my $clusterid = _cluster_id( $cluster );
+
+    my $self = bless {
+		      u         => $u,
+		      resource  => $resource,
+		      cluster   => $cluster,
+		      clusterid => $clusterid,
+		      presence  => $presence,
+		      flags     => $flags,
+		     }, $class;
+
+    my $dbh = LJ::get_db_writer() or die "No db";
+
+    my $sth = $dbh->prepare( "INSERT INTO jabpresence (userid, reshash, resource, clusterid, presence, flags) VALUES (?, ?, ?, ?, ?, ?)" );
+    $sth->execute( $u->id, $self->reshash, $resource, $clusterid, $presence, $flags );
+
+    if ($dbh->errstr) {
+	warn "Insertion error: " . $dbh->errstr;
+	return;
+    }
+
+    return $self;
+}
+
+=head1 OBJECT METHODS
+
+=head2 $obj->u
+
+=head2 $obj->resource
+
+=head2 $obj->cluster
+
+=head2 $obj->presence
+
+=head2 $obj->flags
+
+=head2 $obj->reshash
+
+General purpose accessors for attributes on these objects.
+
+=cut
+
+sub u        { $_[0]->{u} }
+sub resource { $_[0]->{resource} }
+sub presence { $_[0]->{presence} }
+sub flags    { $_[0]->{flags} }
+
+sub clusterid {
+    my $self = shift;
+    return ($self->{clusterid} ||= _cluster_id( $self->{cluster} ));
+}
+
+sub cluster  {
+    my $self = shift;
+    return ($self->{cluster} ||= _cluster_address( $self->{clusterid} ));
+}
+
+sub reshash  {
+    my $self = shift;
+    return ($self->{reshash} ||= md5_base64( $self->{resource} ));
+}
+
+
+=head2 $obj->set_presence( $val )
+
+=head2 $obj->set_flags( $val )
+
+Setters for values on these objects.
+
+=cut
+
+sub set_presence {
+    my $self = shift;
+    my $val = shift;
+
+    croak( "Didn't pass in a defined value to set" )
+	unless defined $val;
+
+    $self->{presence} = $val;
+
+    $self->_save( 'presence' );
+
+    return $val;
+}
+
+sub set_flags {
+    my $self = shift;
+    my $val = shift;
+
+    croak "Didn't pass in a defined value to set"
+	unless defined $val;
+
+    $self->_save( 'flags' );
+
+    return $val;
+}
+
+# Internal functions
+
+sub _load {
+    my $self = shift;
+
+    my $userid = $self->u->id;
+    my $reshash = $self->reshash;
+
+    my $memcached = LJ::MemCache::get( [$userid, "jabpresence:$userid:$reshash"] );
+
+    return %$memcached if $memcached;
+
+    my $dbh = LJ::get_db_reader() or die "No db";
+
+    my @fields = qw(clusterid presence flags);
+
+    my $row = $dbh->selectrow_hashref("SELECT " . join(',', @fields) . " FROM jabpresence WHERE userid=? AND reshash=? AND resource=?", undef,
+				      $self->u->id, $self->reshash, $self->resource);
+
+    die $dbh->errstr if $dbh->errstr;
+
+    return undef unless $row;
+
+    return %$row;
+}
+
+my %savable_cols = map { ($_, 1) } qw(presence flags);
+
+sub _save {
+    my $self = shift;
+
+    return unless @_;
+
+    my @bad_cols = grep {!$savable_cols{$_}} @_;
+    die "Cannot save cols " . join( ',', @bad_cols ) . "."
+	if @bad_cols;
+
+    my $userid = $self->u->id;
+    my $reshash = $self->reshash;
+
+    my $dbh = LJ::get_db_writer() or die "No db";
+
+    $dbh->do( "UPDATE jabpresence SET " . join( ', ', map { "$_ = ?" } @_ ) .
+	      " WHERE userid=? AND reshash=? AND resource=?", undef,
+	      (map { $self->{$_} } @_), $userid, $reshash, $self->resource );
+
+    die "Database update failed: " . $dbh->errstr
+	if $dbh->errstr;
+
+    LJ::MemCache::delete( [$userid, "jabpresence:$userid:$reshash"], 0 );
+}
+
+my %cluster_address;
+
+sub _cluster_address {
+    my $id = shift;
+    die "No id" unless $id;
+
+    if (my $address = $cluster_address{$id}) {
+        return $address;
+    }
+
+    my $address = LJ::MemCache::get( "jabclusterid:$id" );
+
+    return $cluster_address{$id} = $address
+        if $address;
+
+    my $dbh = LJ::get_db_reader() or die "No db";
+
+    $address = $dbh->selectrow_array( "SELECT address FROM jabcluster WHERE clusterid=?", undef, $id )
+        or return;
+
+    LJ::MemCache::set( "jabclusterid:$id", $address );
+
+    return $address;
+}
+
+my %cluster_id;
+
+sub _cluster_id {
+    my $address = shift;
+    die "No address" unless $address;
+
+    if (my $id = $cluster_id{$address}) {
+        return $id;
+    }
+
+    my $id = LJ::MemCache::get( "jabclusteraddr:$address" );
+
+    return $cluster_id{$address} = $id
+        if $id;
+
+    # Loop over this action a few times so INSERT/SELECT races don't cause us to bail
+    my $tries = 0;
+    my $dbr = LJ::get_db_reader() or die "No db";
+    my $dbh;
+
+    until ($id or $tries > 3)
+    {
+        if ($tries++ > 2) {
+	    die "Insert failed, and address was not found in DB";
+	}
+
+	$id = $dbr->selectrow_array( "SELECT clusterid FROM jabcluster WHERE address=?",
+				     undef, $address );
+	last if $id;
+
+	$dbh ||= LJ::get_db_writer() or die "No db";
+
+	$dbh->do( "INSERT INTO jabcluster (address) VALUES (?)", undef, $address )
+	    or next;
+
+        $id = $dbh->{mysql_insertid};
+    }
+
+    LJ::MemCache::set( "jabclusteraddr:$address", $id );
+    return $id;
+}
+
+1;
