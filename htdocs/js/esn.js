dbef32b18699da0f5144dcafa7ef4db5b920d572
Mischa Spiegelmock <revmischa@cpan.org>
2006-07-21 00:51:53 +0000

    start of esn ajax
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8155 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/esn.js
+++ htdocs/js/esn.js
@@ -3,7 +3,102 @@ var ESN = new Object();
 DOM.addEventListener(window, "load", function (evt) {
   ESN.initCheckAllBtns();
   ESN.initEventCheckBtns();
+  ESN.initTrackBtns();
 });
+
+// attach event handlers to all track buttons
+ESN.initTrackBtns = function () {
+    // don't do anything if no remote
+    if (!LJVAR || !LJVAR.has_remote) return;
+
+    // attach to all ljuser head icons
+    var domObjects = document.getElementsByTagName("*");
+    var trackBtns = DOM.filterElementsByClassName(domObjects, "TrackButton") || [];
+
+    trackBtns.forEach(function (trackBtn) {
+        if (!trackBtn || !trackBtn.getAttribute) return;
+
+        if (!trackBtn.getAttribute("lj:subid") && !trackBtn.getAttribute("lj:journalid")) return;
+
+        DOM.addEventListener(trackBtn, "click", function (evt) {
+            Event.stop(evt);
+
+            var btnInfo = {};
+
+            Array('arg1', 'arg2', 'etypeid', 'journalid', 'subid', 'auth_token').forEach(function (arg) {
+                btnInfo[arg] = trackBtn.getAttribute("lj:" + arg);
+            });
+
+            ESN.toggleSubscription(btnInfo, evt, trackBtn);
+            return false;
+        });
+    });
+};
+
+// (Un)subscribes to an event
+ESN.toggleSubscription = function (subInfo, evt, btn) {
+    var action = "";
+    var params = {};
+
+    if (subInfo.subid) {
+        // subscription exists
+        action = "delsub";
+        params.subid = subInfo.subid;
+    } else {
+        // create a new subscription
+        action = "addsub";
+
+        Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
+            if (subInfo[param])
+                params[param] = parseInt(subInfo[param]);
+        });
+    }
+
+    params.action     = action;
+    params.auth_token = subInfo.auth_token;
+
+    var reqInfo = {
+        "method": "POST",
+        "url":    "/__rpc_esn",
+        "data":   HTTPReq.formEncoded(params)
+    };
+
+    var gotInfoCallback = function (info) {
+        if (! info) return LJ_IPPU.showNote("Error saving subscription", btn);
+
+        if (info.error) return LJ_IPPU.showNote(info.error, btn);
+
+        if (info.success) {
+            if (info.msg)
+                LJ_IPPU.showNote(info.msg, btn);
+
+            if (info.subscribed) {
+                btn.src = LJVAR.imgprefix + "/btn_tracking.gif";
+
+                btn.setAttribute("lj:subid", info.subid);
+
+                Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
+                    btn.setAttribute("lj:" + param, null);
+                });
+            } else {
+                btn.src = LJVAR.imgprefix + "/btn_track.gif";
+
+                btn.setAttribute("lj:subid", null);
+
+                Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
+                    btn.setAttribute("lj:" + param, info[param]);
+                });
+            }
+
+            btn.setAttribute("lj:auth_token", info.auth_token);
+        }
+    };
+
+    reqInfo.onData = gotInfoCallback;
+    reqInfo.onError = function (err) { LJ_IPPU.showNote("Error: " + err) };
+
+    HTTPReq.getJSON(reqInfo);
+};
 
 // When page loads, set up "check all" checkboxes
 ESN.initCheckAllBtns = function () {
