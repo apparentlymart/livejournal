3228a98d8adc79d1520b44dd0fccf8ae35dabd71
Mischa Spiegelmock <revmischa@cpan.org>
2006-07-26 22:11:35 +0000

    Expand/collapse notification contents
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8231 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/esn_inbox.js
+++ htdocs/js/esn_inbox.js
@@ -2,6 +2,7 @@ var ESN_Inbox = new Object();
 
 DOM.addEventListener(window, "load", function (evt) {
   ESN_Inbox.initTableSelection();
+  ESN_Inbox.initContentExpandButtons();
 });
 
 // Set up table events
@@ -18,10 +19,39 @@ ESN_Inbox.initTableSelection = function () {
     if (!selectedRows) return;
 
     selectedRows.addWatcher(ESN_Inbox.selectedRowsChanged);
-}
+};
 
 // Callback for when selected rows of the inbox change
 ESN_Inbox.selectedRowsChanged = function (rows) {
 
-}
+};
+
+// set up event handlers on expand buttons
+ESN_Inbox.initContentExpandButtons = function () {
+    var domElements = document.getElementsByTagName("*");
+    var buttons = DOM.filterElementsByClassName(domElements, "InboxItem_Expand") || [];
+
+    Array.prototype.forEach.call(buttons, function (button) {
+        DOM.addEventListener(button, "click", function (evt) {
+            // find content div
+            var parent = DOM.getFirstAncestorByClassName(button, "InboxItem_Row");
+            var children = parent.getElementsByTagName("div");
+            var contentContainers = DOM.filterElementsByClassName(children, "InboxItem_Content");
+            var contentContainer = contentContainers[0];
+
+            if (!contentContainer) return true;
+
+            if (contentContainer.style.display == "none") {
+                contentContainer.style.display = "block";
+                button.src = LJVAR.imgprefix + "/expand.gif";
+            } else {
+                contentContainer.style.display = "none";
+                button.src = LJVAR.imgprefix + "/collapse.gif";
+            }
+
+            Event.stop(evt);
+            return false;
+        });
+    });
+};
 
