c5d354ac1aec9537f6e10eefbe3c47ad50687db1
Mischa Spiegelmock <revmischa@cpan.org>
2006-07-31 18:28:56 +0000

    override is_selected to just return the prop accessor value
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8274 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Setting/CtxPopup.pm
+++ cgi-bin/LJ/Setting/CtxPopup.pm
@@ -14,6 +14,11 @@ sub des {
     return "Show contextual popup when hovering over user icons and userpics.";
 }
 
+sub is_selected {
+    my ($class, $u) = @_;
+    return $u->prop('opt_ctxpopup');
+}
+
 sub prop_name { "opt_ctxpopup" }
 sub checked_value { "Y" }
 sub unchecked_value { "N" }
