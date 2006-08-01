1cd8900385055b29a57656bcca7705797f7286fc
Brad Whitaker <brad.whitaker@gmail.com>
2006-08-01 03:47:27 +0000

    More LJ::SMS work... it's getting there!
    -- doc updates
    
    -- use LJ::Typemap (yay)
    
    -- add new fields: type, body_raw, and timecreate
    
    -- change text => body_text
    
    -- some deaths should be croaks
    
    -- fix some bugs in constructor
    
    -- require owner object, but try to infer one when
       construction is done via dsms... outgoing owner
       is any mapping for the 'to' address, and vice
       versa
    
    -- make number normalization a class method
    
    -- to/from should be to_num/from_num
    
    -- new method: save_to_db which is called when a 
       message is sent or received
    
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/sms@8295 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/SMS.pm
+++ cgi-bin/LJ/SMS.pm
@@ -3,7 +3,11 @@ package LJ::SMS;
 use strict;
 use Carp qw(croak);
 
-use Class::Autouse qw(IO::Socket::INET XML::Simple);
+use Class::Autouse qw(
+                      IO::Socket::INET 
+                      XML::Simple
+                      LJ::Typemap
+                      );
 
 # LJ::SMS object
 #
@@ -16,17 +20,22 @@ use Class::Autouse qw(IO::Socket::INET XML::Simple);
 #    from_num:   phone number of sender
 #    to_uid:     userid of the recipient
 #    to_num:     phone number of recipient
-#    text:       text body of message
+#    timecreate: timestamp when message was created
+#    type:       'incoming' or 'outgoing' from LJ's perspective
+#    body_text:  decoded text body of message
+#    body_raw:   raw text body of message
 #    meta:       hashref of metadata key/value pairs
 #
 # synopsis:
 #
-#    my $sms = LJ::SMS->new($u,
-#                           from => $num_or_u,
-#                           to   => $num_or_u,
-#                           text => $utf8_text,
-#                           meta => { k => $v },
+#    my $sms = LJ::SMS->new($owneru,
+#                           from      => $num_or_u,
+#                           to        => $num_or_u,
+#                           body_text => $utf8_text,
+#                           meta      => { k => $v },
 #                           );
+#
+#    my $sms = LJ::SMS->new_from_dsms($owneru, $dsms_msg);
 #
 # accessors:
 #
@@ -35,7 +44,10 @@ use Class::Autouse qw(IO::Socket::INET XML::Simple);
 #    $sms->from_num;
 #    $sms->to_u;
 #    $sms->from_u;
-#    $sms->text;
+#    $sms->type;
+#    $sms->body_text;
+#    $sms->raw_text;
+#    $sms->timecreate;
 #    $sms->meta;
 #    $sms->meta($k);
 
@@ -45,9 +57,8 @@ sub schwartz_capabilities {
 
 sub new {
     my ($class, %opts) = @_;
-    die "new is a class method"
+    croak "new is a class method"
         unless $class eq __PACKAGE__;
-
 
     my $self = bless {}, $class;
 
@@ -56,7 +67,7 @@ sub new {
     # specified for each valid from/to arg
     foreach my $k (qw(from to)) {
         my $val = delete $opts{$k};
-        next unless $opts{$k};
+        next unless $val;
 
         # extract fields from $u object
         if (LJ::isu($val)) {
@@ -64,15 +75,34 @@ sub new {
             $self->{"${k}_uid"} = $u->{userid};
             $self->{"${k}_num"} = $u->sms_number
                 or croak "'$k' user has no mapped number";
-        } elsif ($val !~ /^\d+$/) {
-            croak "invalid numeric argument '$k': $val";
+            next;
+        }
+
+        if ($val =~ /^\d+$/) {
             $self->{"${k}_uid"} = $self->get_uid($val);
             $self->{"${k}_num"} = $val;
+            next;
         }
+
+        croak "invalid numeric argument '$k': $val";
     }
 
+    { # owner argument
+        my $owner_arg = delete $opts{owner};
+        croak "owner argument must be a valid user object"
+            unless LJ::isu($owner_arg);
+
+        $self->{owner_uid} = $owner_arg->{userid};
+    }
+
+    # type: incoming/outgoing
+    $self->{type} = lc(delete $opts{type});
+    croak "type must be one of 'incoming' or 'outgoing', from the server's perspective"
+        unless $self->{type} =~ /^(?:incoming|outgoing)$/;
+    
     # omg we need text eh?
-    $self->{text} = delete $opts{text};
+    $self->{body_text} = delete $opts{body_text};
+    $self->{body_raw}  = exists $opts{body_raw} ? delete $opts{body_raw} : $self->{body_text};
 
     { # any metadata the user would like to pass through
         $self->{meta} = delete $opts{meta};
@@ -82,62 +112,94 @@ sub new {
         $self->{meta} ||= {};
     }
 
-    # save this message to the db
-    #$self->save_to_db;
+    # set timecreate stamp for this object
+    $self->{timecreate} = time();
 
-    die "foo" if %opts;
+    die "invalid argument: " . join(", ", keys %opts)
+        if %opts;
+
     return $self;
 }
 
 sub new_from_dsms {
     my ($class, $dsms_msg) = @_;
-    die "new_from_dsms is a class method"
+    croak "new_from_dsms is a class method"
         unless $class eq __PACKAGE__;
 
-    die "invalid dsms_msg argument: $dsms_msg"
+    croak "invalid dsms_msg argument: $dsms_msg"
         unless ref $dsms_msg eq 'DSMS::Message';
 
-    # strip full msisdns down to us phone numbers
-    my $normalize = sub {
-        my $arg = shift;
-        $arg = ref $arg ? $arg->[0] : $arg;
+    my $owneru = undef;
+    {
+        my $owner_num = $dsms_msg->is_incoming ?
+            $dsms_msg->from : $dsms_msg->to;
 
-        # FIXME: handle shortcodes
-        $arg =~ s/^(?:\+1)(\d{10})$/$1/;
-        return $arg;
-    };
+        $owner_num = $class->normalize_num($owner_num);
+
+        my $uid = $class->get_uid($owner_num)
+            or croak "invalid owner id from number: $owner_num";
+
+        $owneru = LJ::load_userid($uid);
+        croak "invalid owner u from number: $owner_num"
+            unless LJ::isu($owneru);
+    }
+
 
     # construct a new LJ::SMS 
     my $msg = $class->new
-        ( from => $normalize->($dsms_msg->from),
-          to   => $normalize->($dsms_msg->to),
-          text => $dsms_msg->body_text,
-          meta => $dsms_msg->meta,
+        ( owner     => $owneru,
+          from      => $class->normalize_num($dsms_msg->from),
+          to        => $class->normalize_num($dsms_msg->to),
+          type      => $dsms_msg->type,
+          body_text => $dsms_msg->body_text,
+          body_raw  => $dsms_msg->body_raw,
+          meta      => $dsms_msg->meta, 
           );
+
+    return $msg;
+}
+
+# strip full msisdns down to us phone numbers
+sub normalize_num {
+    my $class = shift;
+    my $arg = shift;
+    $arg = ref $arg ? $arg->[0] : $arg;
+
+    # FIXME: handle shortcodes
+    $arg =~ s/^(?:\+1)(\d{10})$/$1/;
+    return $arg;
 }
 
 sub meta {
     my $self = shift;
     my $key  = shift;
 
-    my $meta = $self->{meta};
+    my $meta = $self->{meta} || {};
     return $key ? $meta->{$key} : $meta;
 }
 
-sub to {
+sub owner_u {
     my $self = shift;
-    return $self->{to};
+
+    # load user obj if valid uid and return
+    my $uid = $self->{owner_uid};
+    return $uid ? LJ::load_userid($uid) : undef;
+}
+
+sub to_num {
+    my $self = shift;
+    return $self->{to_num};
 }
 
 sub to_u {
     my $self = shift;
 
     # load userid from db unless the cache key exists
-    $self->{_to_uid} = $self->get_uid($self->{to})
-        unless exists $self->{_to_uid};
+    $self->{to_uid} = $self->get_uid($self->{to_num})
+        unless exists $self->{to_uid};
 
     # load user obj if valid uid and return
-    my $uid = $self->{_to_uid};
+    my $uid = $self->{to_uid};
     return $uid ? LJ::load_userid($uid) : undef;
 }
 
@@ -150,9 +212,9 @@ sub get_uid {
         ("SELECT userid FROM smsusermap WHERE number=?", undef, $num);
 }
 
-sub from {
+sub from_num {
     my $self = shift;
-    return $self->{from};
+    return $self->{from_num};
 }
 
 sub from_u {
@@ -167,21 +229,68 @@ sub from_u {
     return $uid ? LJ::load_userid($uid) : undef;
 }
 
-sub text {
+sub type {
     my $self = shift;
-    return $self->{text};
+    return $self->{type};
 }
 
-sub log_to_db {
+sub timecreate {
+    my $self = shift;
+    return $self->{timecreate};
+}
+
+sub body_text {
+    my $self = shift;
+    return $self->{body_text};
+}
+
+sub body_raw {
+    my $self = shift;
+    return $self->{body_raw};
+}
+
+sub save_to_db {
     my $self = shift;
 
+    my $u = $self->owner_u
+        or die "no owner object found";
+    my $uid = $u->{userid};
+
+    # allocate a user counter id for this messaGe
+    my $msgid = LJ::alloc_user_counter($u, "G")
+        or die "Unable to allocate msgid for user: " . $self->owner_u->{user};
     
+    # insert main sms_msg row
+    $u->do("INSERT INTO sms_msg SET userid=?, msgid=?, type=?, to_number=?, from_number=?, " . 
+           "msg_raw=?, timecreate=UNIX_TIMESTAMP()", undef, 
+           $uid, $msgid, $self->type, $self->to_num, $self->from_num,
+           $self->body_raw);
+    die $u->errstr if $u->err;
+
+    my $tm = LJ::Typemap->new
+        ( table      => 'sms_msgproplist',
+          classfield => 'name',
+          idfield    => 'propid',
+          );
+
+    my @vals = ();
+    while (my ($propname, $propval) = each %{$self->meta}) {
+        my $propid = $tm->class_to_typeid($propname);
+        push @vals => $uid, $msgid, $propid, $propval;
+    }
+    my $bind = join(",", map { "(?,?,?,?)" } (1..@vals/4));
+
+    $u->do("INSERT INTO sms_msgprop (userid, msgid, propid, propval) VALUES $bind",
+           undef, @vals);
+    die $u->errstr if $u->err;
+
+    return 1;
 }
 
 # enqueue an incoming SMS for processing
 sub enqueue_as_incoming {
     my $class = shift;
-    die "enqueue_as_incoming is a class method"
+    croak "enqueue_as_incoming is a class method"
         unless $class eq __PACKAGE__;
 
     my $msg = shift;
@@ -201,13 +310,6 @@ sub enqueue_as_incoming {
 
 sub should_enqueue { 1 }
 
-# process an incoming SMS
-sub worker_process_incoming {
-    my $self = shift;
-
-
-}
-
 sub send {
     my $self = shift;
     if (my $cv = $LJ::_T_SMS_SEND) {
@@ -217,7 +319,16 @@ sub send {
         return $self->send_jabber_dev_server;
     }
 
-    warn "LJ::SMS->send() not implemented yet";
+    my $gw = LJ::sms_gateway()
+        or die "unable to instantiate SMS gateway object";
+
+    my $rv = $gw->send_msg($self);
+
+    # this message has been sent, log it to the db
+    # FIXME: this the appropriate time?
+    $self->save_to_db;
+
+    return 1;
 }
 
 sub send_jabber_dev_server {
@@ -230,8 +341,8 @@ sub send_jabber_dev_server {
     my $okay = <$sock>;
     return 0 unless $okay =~ /^OK/;
 
-    my $to = $self->{to} . '@' . $LJ::DOMAIN;
-    my $msg = $self->{text};
+    my $to = $self->to_num . '@' . $LJ::DOMAIN;
+    my $msg = $self->body_text;
     my $xml = qq{<message type='chat' to='$to' from='sms\@$LJ::DOMAIN'><x xmlns='jabber:x:event'><composing/></x><body>$msg</body><html xmlns='http://jabber.org/protocol/xhtml-im'><body xmlns='http://www.w3.org/1999/xhtml'><html>$msg</html></body></html></message>};
     print $sock ("send_xml $to " . LJ::eurl($xml) . "\n");
     return 1;
@@ -239,7 +350,7 @@ sub send_jabber_dev_server {
 
 sub as_string {
     my $self = shift;
-    return "from=$self->{from}, text=$self->{text}\n";
+    return "from=$self->{from}, text=$self->{body_text}\n";
 }
 
 # is sms sending configured?
@@ -272,6 +383,10 @@ sub work {
         $job->failed;
         return;
     }
+
+    # save msg to the db
+    $msg->save_to_db
+        or die "unable to save message to db";
 
     use Data::Dumper;
     print "msg: " . Dumper($msg);
