25cebdb305b4ed7697d5ab387c0ee69c460cbcd0
Janine Costanzo <jcostanzo@iparadigms.com>
2006-08-15 02:38:48 +0000

    Rearrange the logic for can_use_layer to not automatically give all layers to those with the s2styles cap if you have the s2_can_use_layer hook defined.
    Thanks to <lj user="xb95"> for the help!
    
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8545 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2.pm
+++ cgi-bin/LJ/S2.pm
@@ -1455,11 +1455,18 @@ sub get_policy
 sub can_use_layer
 {
     my ($u, $uniq) = @_;  # $uniq = redist_uniq value
-    return 1 if LJ::get_cap($u, "s2styles");
-    return 1 if LJ::run_hook('s2_can_use_layer', {
+
+    # use hook value if return value defined
+    my $val = LJ::run_hook('s2_can_use_layer', {
         u => $u,
         uniq => $uniq,
     });
+    return $val if defined $val;
+
+    # by default, allow if they have the cap
+    return 1 if LJ::get_cap($u, "s2styles");
+
+    # finally, fall back to checking the policy
     my $pol = get_policy();
     my $can = 0;
 
