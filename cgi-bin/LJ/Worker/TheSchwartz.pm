e7eb87f55d105b7f564f132948590157c74b7af9
Brad Fitzpatrick <brad@danga.com>
2006-08-28 19:54:10 +0000

    add a pre-work hook
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@8805 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Worker/TheSchwartz.pm
+++ cgi-bin/LJ/Worker/TheSchwartz.pm
@@ -19,13 +19,15 @@ $SIG{TERM} = sub {
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(schwartz_decl schwartz_work schwartz_on_idle schwartz_on_afterwork);
+@EXPORT = qw(schwartz_decl schwartz_work schwartz_on_idle schwartz_on_afterwork schwartz_on_prework);
 
 my $sclient = LJ::theschwartz();
 $sclient->set_verbose($verbose);
 
 my $on_idle = sub {};
 my $on_afterwork = sub {};
+
+my $on_prework = sub { 1 };  # return 1 to proceed and do work
 
 sub schwartz_decl {
     my ($classname) = @_;
@@ -42,13 +44,22 @@ sub schwartz_on_afterwork {
     $on_afterwork = $code;
 }
 
+# coderef to return 1 to proceed, 0 to sleep
+sub schwartz_on_prework {
+    my ($code) = @_;
+    $on_prework = $code;
+}
+
 sub schwartz_work {
     my $sleep = 0;
     while (1) {
         LJ::start_request();
-        my $did_work = $sclient->work_once;
-        exit 0 if $quit_flag;
-        $on_afterwork->($did_work);
+        my $did_work = 0;
+        if ($on_prework->()) {
+            $did_work = $sclient->work_once;
+            $on_afterwork->($did_work);
+            exit 0 if $quit_flag;
+        }
         if ($did_work) {
             $sleep = 0;
             next;
