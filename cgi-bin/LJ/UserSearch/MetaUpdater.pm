dc6a1790ef78a80a830371943cff8ea98e87e9c7
Jonathan Steinert <github@hachi.kuiki.net>
2006-09-22 22:42:16 +0000

    Make these subroutines actually work.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@9029 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/UserSearch/MetaUpdater.pm
+++ cgi-bin/LJ/UserSearch/MetaUpdater.pm
@@ -9,8 +9,6 @@ use LJ::User;
 sub update_user {
     my $u = LJ::want_user(shift) or die "No userid specified";
 
-    my %fields = @_;
-
     my $dbh = LJ::get_db_writer() or die "No db";
 
     my $oldpack = $dbh->selectrow_array("SELECT packed FROM usersearch WHERE userid=? AND good_until < ?",
@@ -18,23 +16,25 @@ sub update_user {
 
     die $dbh->errstr if $dbh->errstr;
     
-    if (!defined $oldpack) {
-        # Populate full user data
-        # return
-    }
+    my $lastmod = $dbh->selectrow_array("SELECT UNIX_TIMESTAMP()-UNIX_TIMESTAMP(timeupdate) ".
+                                        "AS 'secondsold' FROM userusage ".
+                                        "WHERE userid=?", undef, $u->id);
 
-    my ($lastmod, $age) = unpack("NCxxx", $oldpack);
+    $lastmod ||= 0;
+    
+    my ($age, $good_until) = $u->age_with_expire;
 
-    $lastmod = $fields{lastmod} if exists($fields{lastmod});
-    $age = $fields{age} if exists($fields{age});
+    $age ||= 0;
 
     my $newpack = pack("NCxxx", $lastmod, $age);
 
-    if ($newpack ne $oldpack) {
-        my $rv = $dbh->do("UPDATE usersearch SET packed=? mtime=unix_timestamp() WHERE userid=?",
-                          undef, $newpack, $u->id);
-        die "Update count wrong '$rv' should be 1" unless $rv == 1;
-    }
+    return if (defined $oldpack and $newpack eq $oldpack);
+    
+    my $rv = $dbh->do("REPLACE INTO usersearch (userid, packed, good_until, mtime) ".
+                      "VALUES (?, ?, ?, unix_timestamp())", undef, $u->id, $newpack, $good_until);
+
+    die "DB Error: " . $dbh->errstr if $dbh->errstr;
+    die "Update count wrong '$rv' should be 1" unless $rv == 1;
 }
 
 sub update_file {
@@ -42,30 +42,38 @@ sub update_file {
 
     my $dbh = LJ::get_db_reader() or die "No db";
 
-    sysopen(my $fh, O_RDWR, $filename) or die "Couldn't open file '$filename' for read/write: $!";
+    sysopen(my $fh, $filename, O_RDWR | O_CREAT) or die "Couldn't open file '$filename' for read/write: $!";
 
     sysseek($fh, 0, SEEK_SET) or die "Couldn't seek: $!";
-    sysread($fh, my $header, 8) == 8 or die "Couldn't read 8 byte header: $!";
 
-    my ($file_lastmod, $file_count) = unpack("NN", $header);
+    my ($file_lastmod, $file_count) = (0, 0);
+    
+    if (-s $filename) {
+        sysread($fh, my $header, 8) == 8 or die "Couldn't read 8 byte header: $!";
+        ($file_lastmod, $file_count) = unpack("NN", $header);
+    }
 
     my $db_count = $dbh->selectrow_array("SELECT count(*) FROM usersearch WHERE mtime=?",
                                          undef, $file_lastmod);
 
-    my $sth = $dbh->prepare("SELECT userid, pack, mtime WHERE mtime=? ORDER BY mtime");
+    my $sth = $dbh->prepare("SELECT userid, packed, mtime FROM usersearch WHERE mtime >= ? AND ".
+                            "(good_until IS NULL OR good_until > unix_timestamp()) ORDER BY mtime");
 
     $sth->execute($file_lastmod);
+
+    die "DB Error: " . $sth->errstr if $sth->errstr;
 
     my $modcount = 0;
     my $mtime = 0;
     while (my $row = $sth->fetchrow_hashref) {
-        my $pack = $row->{pack};
-        unless (length($pack) == 8) {
+        warn "Processing row";
+        my $packed = $row->{packed};
+        unless (length($packed) == 8) {
             die "Pack length was incorrect";
         }
         my $offset = $row->{userid} * 8;
         sysseek($fh, $offset, SEEK_SET) or die "Couldn't seek: $!";
-        syswrite($fh, $pack) == 8 or die "Syswrite failed to complete: $!";
+        syswrite($fh, $packed) == 8 or die "Syswrite failed to complete: $!";
 
         if ($mtime == $row->{mtime}) {
             $modcount++;
@@ -82,6 +90,6 @@ sub update_file {
     return;
 }
 
-# CREATE TABLE usersearch (userid int(10) unsigned NOT NULL, packed CHAR(8) BINARY, mtime INT UNSIGNED NOT NULL, good_until INT UNSIGNED NOT NULL, INDEX(good_until));
+# CREATE TABLE usersearch (userid int(10) unsigned NOT NULL PRIMARY KEY, packed CHAR(8) BINARY, mtime INT UNSIGNED NOT NULL, good_until INT UNSIGNED, INDEX(good_until));
 
 1;
