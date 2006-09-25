6c6a4cfb6b12e2a8df25b04e9b32c8eb9400c026
Brad Fitzpatrick <brad@danga.com>
2006-09-25 19:20:30 +0000

    fix some bugs, rename some variables, eat less memory
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@9036 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/UserSearch/MetaUpdater.pm
+++ cgi-bin/LJ/UserSearch/MetaUpdater.pm
@@ -15,13 +15,13 @@ sub update_user {
                                     undef, $u->id, time);
 
     die $dbh->errstr if $dbh->errstr;
-    
+
     my $lastmod = $dbh->selectrow_array("SELECT UNIX_TIMESTAMP()-UNIX_TIMESTAMP(timeupdate) ".
                                         "AS 'secondsold' FROM userusage ".
                                         "WHERE userid=?", undef, $u->id);
 
     $lastmod ||= 0;
-    
+
     my ($age, $good_until) = $u->age_with_expire;
 
     $age ||= 0;
@@ -29,7 +29,7 @@ sub update_user {
     my $newpack = pack("NCxxx", $lastmod, $age);
 
     return if (defined $oldpack and $newpack eq $oldpack);
-    
+
     my $rv = $dbh->do("REPLACE INTO usersearch (userid, packed, good_until, mtime) ".
                       "VALUES (?, ?, ?, unix_timestamp())", undef, $u->id, $newpack, $good_until);
 
@@ -43,51 +43,68 @@ sub update_file {
     my $dbh = LJ::get_db_reader() or die "No db";
 
     sysopen(my $fh, $filename, O_RDWR | O_CREAT) or die "Couldn't open file '$filename' for read/write: $!";
-
-    sysseek($fh, 0, SEEK_SET) or die "Couldn't seek: $!";
-
-    my ($file_lastmod, $file_count) = (0, 0);
-    
-    if (-s $filename) {
-        sysread($fh, my $header, 8) == 8 or die "Couldn't read 8 byte header: $!";
-        ($file_lastmod, $file_count) = unpack("NN", $header);
+    unless (-s $filename >= 8) {
+        my $zeros = "\0" x 8;
+        syswrite($fh, $zeros);
     }
 
-    my $db_count = $dbh->selectrow_array("SELECT count(*) FROM usersearch WHERE mtime=?",
-                                         undef, $file_lastmod);
+    while (! update_file_partial($dbh, $fh)) {
+        # do more.
+    }
+    return 1;
+}
 
+sub update_file_partial {
+    my ($dbh, $fh) = @_;
+    sysseek($fh, 0, SEEK_SET) or die "Couldn't seek: $!";
+
+    sysread($fh, my $header, 8) == 8 or die "Couldn't read 8 byte header: $!";
+    my ($file_lastmod, $nr_disk_thatmod) = unpack("NN", $header);
+
+    # the on-disk file and database only keeps second granularity.  if
+    # the number of records changed in that particular second changed,
+    # step back in time one second and we'll redo a few records, but
+    # be sure not to miss any.
+    my $nr_db_thatmod = $dbh->selectrow_array("SELECT COUNT(*) FROM usersearch WHERE mtime=?",
+                                              undef, $file_lastmod);
+    if ($nr_db_thatmod != $nr_disk_thatmod) {
+        $file_lastmod--;
+    }
+
+    my $limit_num = 10000;
     my $sth = $dbh->prepare("SELECT userid, packed, mtime FROM usersearch WHERE mtime >= ? AND ".
-                            "(good_until IS NULL OR good_until > unix_timestamp()) ORDER BY mtime");
-
+                            "(good_until IS NULL OR good_until > unix_timestamp()) ORDER BY mtime LIMIT $limit_num");
     $sth->execute($file_lastmod);
 
     die "DB Error: " . $sth->errstr if $sth->errstr;
 
-    my $modcount = 0;
-    my $mtime = 0;
-    while (my $row = $sth->fetchrow_hashref) {
+    my $nr_with_highest_mod = 0;
+    my $last_mtime = 0;
+    my $rows = 0;
+
+    while (my ($userid, $packed, $mtime) = $sth->fetchrow_array) {
         warn "Processing row";
-        my $packed = $row->{packed};
         unless (length($packed) == 8) {
             die "Pack length was incorrect";
         }
-        my $offset = $row->{userid} * 8;
+        my $offset = $userid * 8;
         sysseek($fh, $offset, SEEK_SET) or die "Couldn't seek: $!";
         syswrite($fh, $packed) == 8 or die "Syswrite failed to complete: $!";
+        $rows++;
 
-        if ($mtime == $row->{mtime}) {
-            $modcount++;
+        if ($last_mtime == $mtime) {
+            $nr_with_highest_mod++;
         } else {
-            $modcount = 0;
-            $mtime = $row->{mtime};
+            $nr_with_highest_mod = 1;
+            $last_mtime          = $mtime;
         }
     }
 
     sysseek($fh, 0, SEEK_SET) or die "Couldn't seek: $!";
     my $newheader = pack("NN", $mtime, $modcount);
     syswrite($fh, $newheader) == 8 or die "Couldn't write header: $!";
-    
-    return;
+
+    return ($rows == $limit_num) ? 0 : 1;
 }
 
 # CREATE TABLE usersearch (userid int(10) unsigned NOT NULL PRIMARY KEY, packed CHAR(8) BINARY, mtime INT UNSIGNED NOT NULL, good_until INT UNSIGNED, INDEX(good_until));
