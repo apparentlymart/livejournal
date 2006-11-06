bac39121cd6d5448d09d74482c6ab106f28b9d2f
Jonathan Steinert <github@hachi.kuiki.net>
2006-11-06 22:26:48 +0000

    Yank out all the child watching stuff.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@9409 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Worker.pm
+++ cgi-bin/LJ/Worker.pm
@@ -1,10 +1,6 @@
 package LJ::Worker;
 
 use IO::Socket::UNIX;
-use POSIX ":sys_wait_h";
-
-my %child_pids;
-    # Key is PID, value is time when the prcess was forked (technically a tiny bit after)
 
 sub setup_mother {
     my $class = shift;
@@ -21,8 +17,6 @@ sub setup_mother {
 
     die "Error creating listening unix socket at '$sock_path': $!" unless $sock;
 
-    local $SIG{CHLD} = \&reap_children;
-
     while (my $input = <$sock>) {
         chomp $input;
 
@@ -34,24 +28,11 @@ sub setup_mother {
     }
 }
 
-sub reap_children {
-    local $!;
-    local $?;
-
-    while (my $pid = waitpid(-1, WNOHANG)) {
-        return unless $pid > 0;
-
-        my $startup_time = $child_pids{$pid};
-        return unless $startup_time; # Not my child?
-    }
-}
-
 sub MANAGE_SHUTDOWN {
     exit;
 }
 
 sub MANAGE_FORK {
-    local $SIG{CHLD} = "DEFAULT"; # Don't race
     my $pid = fork();
 
     unless (defined $pid) {
@@ -60,9 +41,6 @@ sub MANAGE_FORK {
     }
 
     if ($pid) {
-        $child_pids{$pid} = time; # Shove the time in, because we don't have anything better to track at the moment.
-        reap_children();
-        $0 = "$0 [C:" . scalar(keys %child_pids) . "]";
         return 1; # continue the management loop because we're the parent
     } else {
         return 0; # we're a child process, the management loop should cleanup and end because we want to start up the main worker loop.
