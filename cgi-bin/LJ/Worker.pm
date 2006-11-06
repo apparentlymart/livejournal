7bc8e44503bebd828dfce964fcfa7808f4c25831
Jonathan Steinert <github@hachi.kuiki.net>
2006-11-06 23:52:23 +0000

    Necessary evil and logic to make this work properly.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@9412 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Worker.pm
+++ cgi-bin/LJ/Worker.pm
@@ -1,27 +1,44 @@
 package LJ::Worker;
 
 use IO::Socket::UNIX;
+use Socket;
+
+BEGIN {
+    my $debug = $ENV{DEBUG} ? 1 : 0;
+    eval "sub DEBUG () { $debug }";
+}
+
+##############################
+# Child and forking management
 
 sub setup_mother {
     my $class = shift;
 
     return unless $ENV{SETUP_MOTHER};
-    my $sock_path = "/var/run/workers/$0.sock";
+    my ($function) = $0 =~ m{([^/]+)$};
+    my $sock_path = "/var/run/workers/$function.sock";
 
-    if (my $sock = IO::Socket::UNIX->new(Peer => $sock_path)) {
+    warn "Checking for existing mother at $sock_path" if DEBUG;
+
+    if (my $sock = IO::Socket::UNIX->new(Peer => $sock_path, Type => SOCK_DGRAM)) {
+        warn "Asking other mother to stand down. We're in charge now" if DEBUG;
         print $sock "SHUTDOWN\n";
+    } else {
+        warn "Other mother didn't exist: $!";
     }
 
     unlink $sock_path; # No error trap, the file may not exist
-    my $sock = IO::Socket::UNIX->new(Local => $sock_path, Listen => 1);
+    my $sock = IO::Socket::UNIX->new(Local => $sock_path, Listen => 1, Type => SOCK_DGRAM);
 
     die "Error creating listening unix socket at '$sock_path': $!" unless $sock;
 
+    warn "Waiting for input" if DEBUG;
     while (my $input = <$sock>) {
         chomp $input;
 
         my $method = "MANAGE_" . uc($input);
         if (my $cv = $class->can($method)) {
+            warn "Executing '$method' function" if DEBUG;
             my $rv = $cv->($class);
             return unless $rv; #return value of command handlers determines if the loop stays running.
         }
@@ -48,3 +65,5 @@ sub MANAGE_FORK {
 
     return $pid;
 }
+
+1;
