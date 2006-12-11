46c373bb2f39fd551879042faadfc72ec828ed1b
Abe Hassan <burr86@gmail.com>
2006-12-11 05:47:41 +0000

    And don't clobber $mode if it's specified. Revert, with the exception of "or" becoming "and".
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@9633 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Poll.pm
+++ cgi-bin/LJ/Poll.pm
@@ -616,12 +616,11 @@ sub render {
     my $dbr = LJ::get_db_reader();
 
     # update the mode if we need to
-    if ($remote) {
+    $mode = 'results' if !$remote && !$mode;
+    if ($remote && !$mode) {
         my $time = $dbr->selectrow_array('SELECT datesubmit FROM pollsubmission '.
                                          'WHERE pollid=? AND userid=?', undef, $pollid, $remote->userid);
         $mode = $time ? 'results' : $can_vote ? 'enter' : 'results';
-    } else {
-        $mode = 'results';
     }
 
     ### load all the questions
