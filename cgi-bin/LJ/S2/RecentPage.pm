4990dc1a8a0025bcdf636d37ea9275166463450f
Martin Atkins <mart@degeneration.co.uk>
2007-01-15 22:22:14 +0000

    When presenting a tag-filtered RecentPage, link to the tag-filtered RSS and Atom feeds in the data links.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10070 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2/RecentPage.pm
+++ cgi-bin/LJ/S2/RecentPage.pm
@@ -15,7 +15,7 @@ sub RecentPage
 
     my $datalink = sub {
         my ($what, $caption) = @_;
-        return Link($p->{'base_url'} . "/data/$what",
+        return Link($p->{'base_url'} . "/data/$what" . ($opts->{tags} ? "?tag=".join(",", map({ LJ::eurl($_) } @{$opts->{tags}})) : ""),
                     $caption,
                     Image("$LJ::IMGPREFIX/data_$what.gif", 32, 15, $caption));
     };
