6107961837721fdc3426ddfb40f4cbaa0126c3a8
Janine Costanzo <jcostanzo@iparadigms.com>
2007-02-09 02:54:32 +0000

    Widget updates.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10348 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/RecentEntry.pm
+++ cgi-bin/LJ/Widget/RecentEntry.pm
@@ -7,54 +7,65 @@ use Carp qw(croak);
 sub render_body {
     my $class = shift;
 
-    my %opts    = @_;
-    my $show    = $opts{show} || 1;
-    my $journal = $opts{journal};
+    my %opts          = @_;
+    my $num_to_show   = $opts{num_to_show} || 1;
+    my $show_comments = exists $opts{show_comments} ? $opts{show_comments} : 1;
+    my $journal       = $opts{journal};
     croak "no journal specified"
         unless $journal;
     my $journalu = LJ::load_user($journal);
     croak "invalid journal: $journal"
         unless LJ::isu($journalu);
 
-    my $ret = "<em>RecentEntry:</em> ";
+    my $ret = "<h1>" . $journalu->name_html . "</h1>";
 
     my @items = LJ::get_recent_items({
         'userid'        => $journalu->id,
         'clusterid'     => $journalu->clusterid,
         'clustersource' => 'slave',
         'order'         => 'logtime',
-        'itemshow'      => 1,
+        'itemshow'      => $num_to_show,
         'dateformat'    => 'S2',
     });
-    my $item = $items[0];
 
-    # silly, there's no journalid in the hashref
-    # returned here, so we'll shove it in to
-    # construct an LJ::Entry object.  : (
-    $item->{journalid} = $journalu->id;
+    my $entry;
+    foreach my $item (@items) {
+        # silly, there's no journalid in the hashref
+        # returned here, so we'll shove it in to
+        # construct an LJ::Entry object.  : (
+        $item->{journalid} = $journalu->id;
 
-    my $entry = LJ::Entry->new_from_item_hash($items[0]);
-    return "" unless $entry->event_text;
+        $entry = LJ::Entry->new_from_item_hash($item);
+        next unless $entry->event_text;
 
-    # Display date as YYYY-MM-DD
-    my $date = substr($entry->eventtime_mysql, 0, 10);
+        # Display date as YYYY-MM-DD
+        my $date = substr($entry->eventtime_mysql, 0, 10);
 
-    $ret .= "<span class='date'>$date</span><br/>";
-    $ret .= "<span class='subject'>" . $entry->subject_html . "</span><br /><br/>";
-    $ret .= $entry->event_html;
+        $ret .= "<div class='entry'>";
 
-    my $link = $entry->url;
+        $ret .= "<div class='date'>$date</div>";
+        $ret .= "<div class='subject'>" . $entry->subject_html . "</div>";
+        $ret .= "<div class='text'>" . $entry->event_html . "</div>";
 
-    $ret .= "<div style='text-align: right'>( ";
-    if (my $reply_ct = $entry->prop('replycount')) {
-        $ret .= "<a href='$link'><b>" . ($reply_ct == 1 ? "1 comment" : "$reply_ct comments") . "</b></a>";
-    } else {
-        $ret .= "<a href='$link'><b>Link</b></a>";
+        my $link = $entry->url;
+
+        $ret .= "<div class='comments'>";
+        if ($show_comments) {
+            if (my $reply_ct = $entry->prop('replycount')) {
+                $ret .= "<a href='$link'><strong>" . ($reply_ct == 1 ? "1 comment" : "$reply_ct comments") . "</strong></a>";
+            } else {
+                $ret .= "<a href='$link'><strong>Link</strong></a>";
+            }
+            unless ($entry->prop('opt_nocomments')) {
+                $ret .= " | <a href='$link?mode=reply'><strong>Leave a comment</strong></a>";
+            }
+        } else {
+            $ret .= "<a href='$link'><strong>Link</strong></a>";
+        }
+        $ret .= "</div>";
+
+        $ret .= "</div>";
     }
-    unless ($entry->prop('opt_nocomments')) {
-        $ret .= " | <a href='$link?mode=reply'><b>Leave a comment</b></a>";
-    }
-    $ret .= " )</div>";
 
     return $ret;
 }
