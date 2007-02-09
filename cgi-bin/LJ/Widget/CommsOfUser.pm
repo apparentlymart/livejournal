649107f1c322ea09f32cd3aba1ca2e4de70b66b5
Janine Costanzo <jcostanzo@iparadigms.com>
2007-02-09 03:43:13 +0000

    Two new widgets: FriendInterests: checkboxes of interests for the given user, so you can choose your friend's interests (basically like enmasse mode on interests.bml) CommsOfUser: Prints out communities of a given user using the logic in LJ::User->notable_communities.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10355 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Widget/CommsOfUser.pm
@@ -0,0 +1,35 @@
+package LJ::Widget::CommsOfUser;
+
+use strict;
+use base qw(LJ::Widget);
+use Carp qw(croak);
+
+sub render_body {
+    my $class = shift;
+    my %opts = @_;
+    my $ret;
+
+    return "" unless $opts{user};
+
+    my $u = LJ::isu($opts{user}) ? $opts{user} : LJ::load_user($opts{user});
+    return "" unless $u;
+
+    my $remote = LJ::get_remote();
+    return "" if $u->id == $remote->id;
+
+    my $max_comms = $opts{max_comms} || 3;
+    my @notable_comms = $u->notable_communities($max_comms);
+    return "" unless @notable_comms;
+
+    $ret .= "<h1>Communities " . $u->ljuser_display . " has added</h1>";
+    $ret .= "<ul>";
+    foreach my $comm (@notable_comms) {
+        $ret .= "<li>" . $comm->ljuser_display . " - " . $comm->name_html  . "</li>";
+    }
+    $ret .= "</ul>";
+    $ret .= "<p>&raquo; <a href='" . $u->profile_url . "'>View " . $u->display_username . "'s profile</a></p>";
+
+    return $ret;
+}
+
+1;
