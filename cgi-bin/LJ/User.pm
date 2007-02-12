e60dbda6b3d1d6af8c775fe32f96b9e7f03a6043
Mischa Spiegelmock <revmischa@cpan.org>
2007-02-12 20:08:12 +0000

    add is_banned method don't fire friended event if user is b& from the journal
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10366 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -3187,6 +3187,12 @@ sub postreg_completed {
     return 1;
 }
 
+# return if $target is banned from $u's journal
+sub is_banned {
+    my ($u, $target) = @_;
+    return LJ::is_banned($target->userid, $u->userid);
+}
+
 package LJ;
 
 use Carp;
@@ -5171,10 +5177,11 @@ sub add_friend
         if ($sclient) {
             my @jobs;
 
-            # only fire event if the friender is a person and not banned
+            # only fire event if the friender is a person and not banned and visible
             my $friender = LJ::load_userid($userid);
-            if ($friender->is_person && $friender->is_visible) {
-                push @jobs, LJ::Event::Befriended->new(LJ::load_userid($fid), $friender)->fire_job
+            my $friendee = LJ::load_userid($fid);
+            if ($friender->is_person && $friender->is_visible && $friendee->is_banned($friender)) {
+                push @jobs, LJ::Event::Befriended->new($friendee, $friender)->fire_job
                     unless $LJ::DISABLED{esn};
             }
 
