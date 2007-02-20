44811e8b3a9544ac138c706da4ea750012af1e1c
Brad Fitzpatrick <brad@danga.com>
2007-02-20 10:30:59 +0000

    return an entry object from a URL
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10432 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/HostedComments.pm
+++ cgi-bin/LJ/HostedComments.pm
@@ -15,5 +15,23 @@ sub journal_of_url {
     return LJ::load_user($user);
 }
 
+sub entry_of_url {
+    my ($class, $url) = @_;
+    my $u = $class->journal_of_url($url) or return undef;
+
+    # TODO: clean URL, removing &lj.foo params
+    my $clean_url = $url;
+
+    my $prop = LJ::get_prop("log", "syn_link") or die "no syn_link prop";
+
+    my $jitemid = LJ::MemCache::get_or_set("jit_of_url:$url", sub {
+        $u->selectrow_array("SELECT jitemid FROM logprop2 ".
+                            "WHERE journalid=? AND propid=? AND value=?",
+                            undef, $u->id, $prop->{id}, $clean_url);
+    });
+    return undef unless $jitemid;
+    return LJ::Entry->new($u, jitemid => $jitemid);
+}
+
 
 1;
