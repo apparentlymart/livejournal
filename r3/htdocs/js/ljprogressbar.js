fb35de9e8cdf2bad9711df3e8aab586b710a8641
Brad Whitaker <brad.whitaker@gmail.com>
2007-03-19 19:19:25 +0000

    -- wrong place
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@10784 1dfba125-df10-0410-8a77-af1a8d6d074f

--- r3/htdocs/js/ljprogressbar.js
+++
@@ -1,9 +0,0 @@
-LJProgressBar = new Class(ProgressBar, {
-  init: function () {
-    LJProgressBar.superClass.init.apply(this, arguments);
-
-    this.containerClassName = "lj_progresscontainer";
-    this.indefiniteClassName = "lj_progressindefinite";
-    this.overlayClassName = "lj_progressoverlay";
-  }
-});
--- r3/src/xmlrpc-client/README.txt
+++
@@ -1,4 +0,0 @@
-This is a simple XML-RPC client, useful for testing (or everyday use).
-
-It's entirely console-based, creating a temp file and invoking $EDITOR
-for input.
