2f917c73c665d578baa8aa707487646e98c2f346
Mischa Spiegelmock <revmischa@cpan.org>
2007-03-29 20:28:12 +0000

    fix bugs for safari and IE add popup for more tracking options
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r4@10876 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/esn.js
+++ htdocs/js/esn.js
@@ -76,20 +76,55 @@ ESN.initTrackBtns = function () {
 
         if (!trackBtn.getAttribute("lj_subid") && !trackBtn.getAttribute("lj_journalid")) return;
 
-        DOM.addEventListener(trackBtn, "click", function (evt) {
-            Event.stop(evt);
-
-            var btnInfo = {};
-
-            Array('arg1', 'arg2', 'etypeid', 'journalid', 'subid', 'auth_token').forEach(function (arg) {
-                btnInfo[arg] = trackBtn.getAttribute("lj_" + arg);
-            });
-
-            ESN.toggleSubscription(btnInfo, evt, trackBtn);
-            return false;
-        });
+        DOM.addEventListener(trackBtn, "click",
+                             ESN.trackBtnClickHandler.bindEventListener(trackBtn));
     });
 };
+
+ESN.trackBtnClickHandler = function (evt) {
+    var trackBtn = evt.target;
+    if (! trackBtn || trackBtn.tagName.toLowerCase() != "img") return true;
+
+    Event.stop(evt);
+
+    var btnInfo = {};
+
+    Array('arg1', 'arg2', 'etypeid', 'journalid', 'subid', 'auth_token').forEach(function (arg) {
+        btnInfo[arg] = trackBtn.getAttribute("lj_" + arg);
+    });
+
+    // pop up little dialog to either track by inbox/email or go to more options
+    var dlg = document.createElement("div");
+    var defTrackBtn = document.createElement("input");
+    defTrackBtn.type = "button";
+    dlg.appendChild(defTrackBtn);
+    defTrackBtn.value = Number(btnInfo["subid"]) ? "Stop tracking"
+        : "Track with email notifications";
+
+    var custTrackBtn = document.createElement("input");
+    custTrackBtn.type = "button";
+    dlg.appendChild(custTrackBtn);
+    custTrackBtn.value = "More tracking options...";
+
+    // global trackPopup so we can only have one
+    if (ESN.trackPopup) {
+        ESN.trackPopup.hide();
+        ESN.trackPopup = null;
+    }
+    ESN.trackPopup = new LJ_IPPU.showNoteElement(dlg, trackBtn, 0);
+
+    DOM.addEventListener(defTrackBtn, "click", function () {
+        ESN.toggleSubscription(btnInfo, evt, trackBtn);
+        if (ESN.trackPopup) ESN.trackPopup.hide();
+    });
+
+    DOM.addEventListener(custTrackBtn, "click", function () {
+        document.location.href = trackBtn.parentNode.href;
+        if (ESN.trackPopup) ESN.trackPopup.hide();
+    });
+
+    return false;
+}
 
 // (Un)subscribes to an event
 ESN.toggleSubscription = function (subInfo, evt, btn) {
@@ -105,7 +140,7 @@ ESN.toggleSubscription = function (subInfo, evt, btn) {
         action = "addsub";
 
         Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
-            if (subInfo[param])
+            if (Number(subInfo[param]))
                 params[param] = parseInt(subInfo[param]);
         });
     }
@@ -134,14 +169,14 @@ ESN.toggleSubscription = function (subInfo, evt, btn) {
                 DOM.setElementAttribute(btn, "lj_subid", info.subid);
 
                 Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
-                    DOM.setElementAttribute(btn, "lj_" + param, null);
+                    DOM.setElementAttribute(btn, "lj_" + param, 0);
                 });
 
                 DOM.setElementAttribute(btn, "title", 'Untrack This');
             } else {
                 btn.src = Site.imgprefix + "/btn_track.gif";
 
-                DOM.setElementAttribute(btn, "lj_subid", null);
+                DOM.setElementAttribute(btn, "lj_subid", 0);
 
                 Array("journalid", "arg1", "arg2", "etypeid").forEach(function (param) {
                     DOM.setElementAttribute(btn, "lj_" + param, info[param]);
