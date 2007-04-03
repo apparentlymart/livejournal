ac5b70a2f47e751e3b761a3d1321b0244b2b2142
Mischa Spiegelmock <revmischa@cpan.org>
2007-04-03 22:04:02 +0000

    notification for when an account gets purged
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r5@10925 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Event/UserExpunged.pm
@@ -0,0 +1,55 @@
+package LJ::Event::UserExpunged;
+use strict;
+use base 'LJ::Event';
+use Carp qw(croak);
+
+sub new {
+    my ($class, $u) = @_;
+    croak "No $u" unless $u;
+
+    return $class->SUPER::new($u);
+}
+
+sub as_string {
+    my $self = shift;
+
+    return $self->event_journal->display_username . " has been purged.";
+}
+
+sub as_email_string {
+    my ($self, $u) = @_;
+
+    my $username = $u->user;
+    my $purgedname = $self->event_journal->display_username;
+
+    my $email =
+qq {Dear $username,
+
+We've recently purged another set of deleted accounts:
+
+"$purgedname" is now available for $15 via a rename token
+
+To rename your journal, visit the Account Rename Service for more
+details:
+
+$LJ::SITEROOT/rename/
+};
+
+    return $email;
+}
+
+sub as_email_subject {
+    my $self = shift;
+    return sprintf "$LJ::SITENAMESHORT Notices: A deleted account has been purged";
+}
+
+sub subscription_as_html {
+    my ($class, $subscr) = @_;
+
+    my $journal = $subscr->journal;
+
+    my $ljuser = $subscr->journal->ljuser_display;
+    return "$ljuser has been purged";
+}
+
+1;
