3d59a3adc6b12deb67bfd0ef188e0b58f13fb4c5
Jonathan Steinert <github@hachi.kuiki.net>
2007-05-10 18:47:42 +0000

    Add config option: $LJ::MEMCACHE_USE_GETPARSERXS to enable the use of Cache::Memcached's new XS parser we have available. Switch from implicit object notation (eeeew) Check to make sure the GetParser(XS) class is sane before we explitly set it, and if not we don't actually force a specific class.
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r7@11354 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/MemCache.pm
+++ cgi-bin/LJ/MemCache.pm
@@ -29,7 +29,23 @@ $GET_DISABLED = 0;
 my $memc;  # memcache object
 
 sub init {
-    $memc = new Cache::Memcached;
+    my $opts = {};
+
+    my $parser_class = LJ::conf_test($LJ::MEMCACHE_USE_GETPARSERXS) ? 'Cache::Memcached::GetParserXS'
+                                                                    : 'Cache::Memcached::GetParser';
+    # Eval, but we don't care about the result here. Loading errors will have been encountered
+    # when Cache::Memcached was loaded, so we won't even see them here. This may not even return
+    # true.
+    eval "use $parser_class";
+
+    # Check to see if the 'new' function/method is defined in the proper namespace, othewise don't
+    # explicitly set a parser class. Cached::Memcached may have attempted to load the XS module, and
+    # failed. This is a reasonable check to make sure it all went OK.
+    if (eval 'defined &' . $parser_class . '::new') {
+        $opts->{'parser_class'} = $parser_class;
+    }
+
+    $memc = Cache::Memcached->new($opts);
     reload_conf();
 }
 
