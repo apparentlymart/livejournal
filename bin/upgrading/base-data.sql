e9208359beac667d45e4c9582f6cccc2c3ab65b2
Henry Lyne <henrylyne@yahoo.com>
2007-06-13 23:26:11 +0000

    [int: promo] Add rate limiter for free vgifts.
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r9@11666 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/upgrading/base-data.sql
+++ bin/upgrading/base-data.sql
@@ -499,6 +499,8 @@ INSERT IGNORE INTO ratelist (des, name) VALUES ('Logged when a user adds someone
 UPDATE ratelist SET des='Logged when a user adds someone to their Friends list' WHERE name='addfriend';
 INSERT IGNORE INTO ratelist (des, name) VALUES ('Logged when wrong username/password is used.', 'failed_login');
 UPDATE ratelist SET des='Logged when wrong username/password is used.' WHERE name='failed_login';
+INSERT IGNORE INTO ratelist (des, name) VALUES ('Logged when a user sends a free VGift', 'freevgifts');
+UPDATE ratelist SET des='Logged when a user sends a free VGift' WHERE name='freevgifts';
 INSERT IGNORE INTO ratelist (des, name) VALUES ('Logged when a user sends a friend invite', 'invitefriend');
 UPDATE ratelist SET des='Logged when a user sends a friend invite' WHERE name='invitefriend';
 INSERT IGNORE INTO ratelist (des, name) VALUES ('Logged when a forgotten password or username e-mail is requested', 'lostinfo');
