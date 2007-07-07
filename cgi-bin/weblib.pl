91cae160588be3927340d3f36b62020a76e6ff0e
Abe Hassan <burr86@gmail.com>
2007-07-07 23:10:37 +0000

    Fix bug where ?usejournal=foo would stlil display "post to burr86" in the submit button -- expand out the logic a bit more instead of trying to do it all in one line. :P
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@11909 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -1539,8 +1539,16 @@ PREVIEW
         if ($opts->{'mode'} eq "update") {
             my $onclick = "";
             $onclick .= "convert_post('draft');return sendForm('updateForm');" if ! $LJ::IS_SSL;
-            my $defaultjournal = $opts->{'usejournal'} || 'Journal';
-            $defaultjournal = $remote->{user} if $remote && $opts->{auth_as_remote};
+
+            my $defaultjournal;
+            if ($opts->{'usejournal'}) {
+                $defaultjournal = $opts->{'usejournal'};
+            } elsif ($remote && $opts->{auth_as_remote}) {
+                $defaultjournal = $remote->user;
+            } else {
+                $defaultjournal = "Journal";
+            }
+
             $$onload .= " changeSubmit('" . BML::ml('entryform.update3') . "', '$defaultjournal');";
             $out .= LJ::html_submit('action:update', BML::ml('entryform.update4'),
                     { 'onclick' => $onclick, 'class' => 'submit', 'id' => 'formsubmit',
