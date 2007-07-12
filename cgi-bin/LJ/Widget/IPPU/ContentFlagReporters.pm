dc168f4bc316dc8c272836edc3b2e9170752448a
Mischa Spiegelmock <revmischa@cpan.org>
2007-07-12 20:33:52 +0000

    widget IPPU for list of content flag reporters
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@11947 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Widget/IPPU/ContentFlagReporters.pm
@@ -0,0 +1,58 @@
+package LJ::Widget::IPPU::ContentFlagReporters;
+use base "LJ::Widget::IPPU";
+use strict;
+
+sub render_body {
+    my ($class, %opts) = @_;
+
+    my $remote = LJ::get_remote();
+
+    return "Unauthorized" unless $remote && $remote->can_admin_content_flagging;
+    return "invalid params" unless $opts{journalid} && $opts{typeid} && $opts{itemid};
+
+    my $ret = '';
+
+    my $dbr = LJ::get_db_reader();
+    my $rows = $dbr->selectcol_arrayref('SELECT reporterid FROM content_flag WHERE ' .
+                                        'journalid=? AND typeid=? AND itemid=? ORDER BY instime DESC LIMIT 1000',
+                                        undef, $opts{journalid}, $opts{typeid}, $opts{itemid});
+    die $dbr->errstr if $dbr->err;
+
+    my $users = LJ::load_userids(@$rows);
+
+    my @reporters = values %$users;
+
+    my $usernames = '';
+
+    my $i = 0;
+    foreach my $u (@reporters) {
+        my $border = $i ? 1 : 0;
+
+        my $rowcolor = $i % 2 == 0 ? 'EEEEEE' : 'CCCCCC';
+        $usernames .= "<div style='padding: 2px; border-top: ${border}px solid #DDDDDD; background-color: #$rowcolor;>";
+            $usernames .= $u->ljuser_display . ' - ' . LJ::ehtml($u->display_name) . '</div>';
+
+        $i++;
+    }
+
+    $ret .= qq {
+        <div class="su_username_list" style="overflow-y: scroll; height: 20em; margin: 4px; border: 1px solid #EEEEEE;">
+            $usernames
+        </div>
+    };
+
+    return $ret;
+}
+
+sub extra_info {
+    my ($self) = shift;
+
+    my $remote = LJ::get_remote()
+        or return undef;
+
+    my @friends = map { $_->info_for_js } $remote->friends;
+
+    return {friends => \@friends};
+}
+
+1;
---
+++ htdocs/js/widget_ippu/contentflagreporters.js
@@ -0,0 +1,6 @@
+LJWidgetIPPU_ContentFlagReporters = new Class(LJWidgetIPPU, {
+  init: function (opts, params) {
+    opts.widgetClass = "ContentFlagReporters";
+    LJWidgetIPPU_ContentFlagReporters.superClass.init.apply(this, arguments);
+  }
+});
