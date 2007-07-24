f196e1d2438bdb7f232c0e6dd8681a3676844ac6
Mischa Spiegelmock <revmischa@cpan.org>
2007-07-24 19:26:47 +0000

    animate loading
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@12110 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/ljwidget_ippu.js
+++ htdocs/js/ljwidget_ippu.js
@@ -8,7 +8,7 @@ LJWidgetIPPU = new Class(LJWidget, {
         this.reqParams = reqParams;
 
         // construct a container ippu for this widget
-        var ippu = new LJ_IPPU(title);
+        var ippu = new LJ_IPPU(title, nearEle);
         this.ippu = ippu;
         var c = document.createElement("div");
         c.id = "LJWidgetIPPU_" + Unique.id();
@@ -16,7 +16,17 @@ LJWidgetIPPU = new Class(LJWidget, {
 
         ippu.show();
 
-        c.innerHTML = "<big>Loading...</big>";
+        var loadingText = document.createElement("div");
+        loadingText.style.fontSize = '1.5em';
+        loadingText.style.fontWeight = 'bold';
+        loadingText.style.margin = '5px';
+        loadingText.style.textAlign = 'center';
+
+        loadingText.innerHTML = "Loading...";
+
+        this.loadingText = loadingText;
+
+        c.appendChild(loadingText);
 
         // id, widgetClass, authToken (not needed)
         var widgetArgs = [c.id, widgetClass]
@@ -29,8 +39,32 @@ LJWidgetIPPU = new Class(LJWidget, {
         this.title = title;
         this.nearEle = nearEle;
 
+        window.setInterval(this.animateLoading.bind(this), 20);
+
+        this.loaded = false;
+
         // start request for this widget now
         this.loadContent();
+    },
+
+    animateCount: 0,
+
+    animateLoading: function (i) {
+      var ele = this.loadingText;
+
+      if (this.loaded || ! ele) {
+        window.clearInterval(i);
+        return;
+      }
+
+      this.animateCount += 0.05;
+      var intensity = ((Math.sin(this.animateCount) + 1) / 2) * 255;
+      var hexColor = Math.round(intensity).toString(16);
+
+      if (hexColor.length == 1) hexColor = "0" + hexColor;
+      hexColor += hexColor + hexColor;
+
+      ele.style.color = "#" + hexColor;
     },
 
     // override doAjaxRequest to add _widget_ippu = 1
@@ -53,7 +87,7 @@ LJWidgetIPPU = new Class(LJWidget, {
 
     // request finished
     onData: function (data) {
-
+      this.loaded = true;
     },
 
     render: function (params) {
