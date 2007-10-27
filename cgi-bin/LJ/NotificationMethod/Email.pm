a903f1ef99ca7f575f5829b28e58e93768272082
Abe Hassan <burr86@gmail.com>
2007-10-27 02:05:17 +0000

    BugID:60726 Include X-LJ-Recipient header in notifications so that we can always figure out who a message is destined for, even if we don't explicitly mention their username in the body of the message.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@12924 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/NotificationMethod/Email.pm
+++ cgi-bin/LJ/NotificationMethod/Email.pm
@@ -79,9 +79,11 @@ sub notify {
         # run transform hook on plain body
         LJ::run_hook("esn_email_text_transform", event => $ev, rcpt_u => $u, bodyref => \$plain_body);
 
-        my %headers = $self->{_debug_headers} ? %{$self->{_debug_headers}} : ();
-        my $extra_headers = $ev->as_email_headers($u) || {};
-        %headers = (%$extra_headers, %headers);
+        my %headers = (
+                       "X-LJ-Recipient" => $u->user,
+                       %{$ev->as_email_headers($u) || {}},
+                       %{$self->{_debug_headers}   || {}}
+                       );
 
         my $email_subject =
             LJ::run_hook("esn_email_subject", $ev, $u) ||
