766579124a65f83e123a8942d4eb3842f0b23dda
Brad Whitaker <brad.whitaker@gmail.com>
2007-11-01 17:59:37 +0000

    -- maintain a vertical alignment, but don't distort images
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@12953 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Event/UserMessageSent.pm
+++ cgi-bin/LJ/Event/UserMessageSent.pm
@@ -118,7 +118,7 @@ sub display_pic {
     my $ret;
     $ret .= '<img src="';
     $ret .= $pic ? $pic->url : "$LJ::STATPREFIX/horizon/nouserpic.png";
-    $ret .= '" width="50" height="50" align="top" />';
+    $ret .= '" width="50" align="top" />';
 
     return $ret;
 }
