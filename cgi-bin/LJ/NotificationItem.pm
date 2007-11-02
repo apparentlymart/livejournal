e48230c01c2d333e7c057ab2e694a93384d92010
Abe Hassan <burr86@gmail.com>
2007-11-02 07:18:29 +0000

    BugID:61069: Resolve circular reference that resulted in hella memory leakage on process-esn workers.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@12959 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/NotificationItem.pm
+++ cgi-bin/LJ/NotificationItem.pm
@@ -27,7 +27,7 @@ sub instance {
     return $u->{_inbox_items}->{$singletonkey} if $u->{_inbox_items}->{$singletonkey};
 
     my $self = {
-        u       => $u,
+        userid  => $u->id,
         qid     => $qid,
         state   => undef,
         event   => undef,
@@ -42,7 +42,7 @@ sub instance {
 
 # returns whose notification this is
 *u = \&owner;
-sub owner { $_[0]->{u} }
+sub owner { LJ::load_userid($_[0]->{userid}) }
 
 # returns this item's id in the notification queue
 sub qid { $_[0]->{qid} }
