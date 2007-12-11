beda922eeec43983fa67f87787a9c292e3ea36fb
SUP <noreply@livejournal.com>
2007-12-11 14:21:07 +0000

    [int: Bug fixing: continous 'page loading' in FF browser; tabs are replaced by spaces]
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@13217 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/thread_expander.js
+++ htdocs/js/thread_expander.js
@@ -1,70 +1,71 @@
 
-	window.threadExpanders	= []
+    window.threadExpanders  = []
 
-	function threadExpander( url, id, srcObj )
-	{
+    function threadExpander( url, id, srcObj )
+    {
 
-		this.url			= url.replace( /#.*$/, '' )
-		this.threadId		= id
-		this.caller			= srcObj
-		this.destination	= document.getElementById( 'expand' + id )
-		this.id				= window.threadExpanders.length
+        this.url            = url.replace( /#.*$/, '' )
+        this.threadId       = id
+        this.caller         = srcObj
+        this.destination    = document.getElementById( 'expand' + id )
+        this.id             = window.threadExpanders.length
 
-		window.threadExpanders.push( this )
-		return this.get()
-	}
+        window.threadExpanders.push( this )
+        return this.get()
+    }
 
 
-	threadExpander.prototype.get	= function()
-	{
-		var thisTE		= this
+    threadExpander.prototype.get    = function()
+    {
+        var thisTE      = this
 
-		this.inProgress( true )
-		if (browser.isIE)
-			this.iObj		= document.createElement('<iframe onload="threadExpanders['+this.id+'].parse()" src="'+this.url+'" style="width:1px;height:1px;disaply:none;">')
-		else
-		{
-			this.iObj		= document.createElement('iframe')
-			with (this.iObj.style)
-			{
-				height	= '1px'
-				widht	= '1px'
-				display	= 'none'
-			}
-			this.iObj.onload	= function() { return thisTE.parse() }
-			this.iObj.src		= this.url
-		}
+        this.inProgress( true )
+        if (browser.isIE)
+            this.iObj       = document.createElement('<iframe onload="threadExpanders['+this.id+'].parse()" src="'+this.url+'" style="width:1px;height:1px;disaply:none;">')
+        else
+        {
+            this.iObj       = document.createElement('iframe')
+            with (this.iObj.style)
+            {
+                height  = '1px'
+                widht   = '1px'
+                display = 'none'
+            }
+            this.iObj.onload    = function() { return thisTE.parse() }
+            this.iObj.src       = this.url
+        }
 
-		document.body.appendChild( this.iObj )
-		return true
-	}
+        document.body.appendChild( this.iObj )
+        return true
+    }
 
-	threadExpander.prototype.parse	= function()
-	{
-		var iDoc	= this.iObj.contentDocument || this.iObj.contentWindow
-		if (iDoc.document)
-			iDoc	= iDoc.document
+    threadExpander.prototype.parse  = function()
+    {
+        var iDoc    = this.iObj.contentDocument || this.iObj.contentWindow
+        if (iDoc.document)
+            iDoc    = iDoc.document
 
-		var comment	= iDoc.getElementById( 'expand' + this.threadId )
-		if (comment)
-		{
-			this.destination.innerHTML	= comment.innerHTML
+        var comment = iDoc.getElementById( 'expand' + this.threadId )
+        if (comment)
+        {
+            this.destination.innerHTML  = comment.innerHTML
             for (var k in this.iObj.contentWindow.LJ_cmtinfo)
                 LJ_cmtinfo[k] = this.iObj.contentWindow.LJ_cmtinfo[ k ]
             ContextualPopup.setup()
-		}
+        }
+        var iObjLocal = this.iObj;
+        setTimeout(function() { iObjLocal.parentNode.removeChild(iObjLocal) }, 10000);
+        //this.iObj.parentNode.removeChild( this.iObj )
+        return true
+    }
 
-		this.iObj.parentNode.removeChild( this.iObj )
-		return true
-	}
-
-	threadExpander.prototype.inProgress	= function( inProgress )
-	{
-		if (inProgress)
-			this.caller.appendChild
-			(
-				document.getElementById('thread_loader_img').firstChild.cloneNode( false )
-			)
-		return true
-	}
+    threadExpander.prototype.inProgress = function( inProgress )
+    {
+        if (inProgress)
+            this.caller.appendChild
+            (
+                document.getElementById('thread_loader_img').firstChild.cloneNode( false )
+            )
+        return true
+    }
 
