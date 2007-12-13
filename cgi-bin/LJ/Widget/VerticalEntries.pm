dcd3812eeced93a7bfb7c8f3c45eada0b5320209
Janine Costanzo <jcostanzo@iparadigms.com>
2007-12-13 21:36:08 +0000

    Pass the right number of parameters to LJ::html_trim().
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@13252 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/VerticalEntries.pm
+++ cgi-bin/LJ/Widget/VerticalEntries.pm
@@ -102,7 +102,7 @@ sub print_entry {
     }
 
     # entry text
-    $ret .= "<p class='event'>" . LJ::html_trim($entry->event_text, 0, 400) . " &hellip;</p>";
+    $ret .= "<p class='event'>" . LJ::html_trim($entry->event_text, 400) . " &hellip;</p>";
 
     # tags
     my @tags = $entry->tags;
