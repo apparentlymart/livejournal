0fc77c73275b2e300b9c626d61ff85243915885f
SUP <noreply@livejournal.com>
2007-12-13 18:39:59 +0000

    [int: resolved issue of OpenID headers in adult flagged journals. Note: this is still not tested!]
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@13250 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -4548,7 +4548,7 @@ sub openid_tags {
 
     # OpenID Server and Yadis
     if (LJ::OpenID->server_enabled and defined $u) {
-        my $journalbase = LJ::journal_base($u->{name});
+        my $journalbase = $u->journal_base;
         $head .= qq{<link rel="openid.server" href="$LJ::OPENID_SERVER" />\n};
         $head .= qq{<meta http-equiv="X-XRDS-Location" content="$journalbase/data/yadis" />\n};
     }
