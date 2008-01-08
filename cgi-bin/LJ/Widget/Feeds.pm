ebacf7a113854e00df706dbb94d2367935511b78
Janine Costanzo <jcostanzo@iparadigms.com>
2008-01-08 22:27:00 +0000

    Bug ID: 63518
    Styling changes.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@13352 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/Feeds.pm
+++ cgi-bin/LJ/Widget/Feeds.pm
@@ -4,6 +4,8 @@ use strict;
 use base qw(LJ::Widget);
 use Carp qw(croak);
 
+sub need_res { qw( stc/widgets/feeds.css ) }
+
 sub render_body {
     my $class = shift;
     my %opts = @_;
@@ -12,7 +14,7 @@ sub render_body {
     my $get = $class->get_args;
     my $cart = $get->{'cart'} || $BML::COOKIE{cart};
     my $body;
-    $body .= "<div><h2 class='solid-neutral'>Feeds</h2>";
+    $body .= "<h2 class='solid-neutral'>Feeds</h2>";
 
     # get user IDs of most popular feeds
     my $popsyn = LJ::Syn::get_popular_feed_ids();
@@ -20,8 +22,8 @@ sub render_body {
 
     my $feednum = 10;
     my $max = ((scalar @rand) < $feednum) ? (scalar @rand) : $feednum;
-    $body .= "<div style='margin-left: 5px'>";
-    $body .= "<table>";
+    $body .= "<div class='feeds-content'>";
+    $body .= "<table cellpadding='0' cellspacing='0'>";
     my $odd = 1;
     foreach my $userid (@rand[0..$max-1]) {
         my $u = LJ::load_userid($userid);
@@ -34,12 +36,12 @@ sub render_body {
     $body .= "<td>&nbsp;</td></tr>" unless ($odd);
 
     $body .= "</table>";
-    $body .= "<p style='padding-left: 4px'>&raquo; <a href='$LJ::SITEROOT/syn/list.bml'>" .
+    $body .= "<p class='viewall'>&raquo; <a href='$LJ::SITEROOT/syn/list.bml'>" .
              BML::ml('widget.feeds.viewall') . "</a></p>";
 
     # Form to add or find feeds
     if ($remote) {
-        $body .= "<div style='padding-left: 4px'><form method='post' action='$LJ::SITEROOT/syn/'>";
+        $body .= "<form method='post' action='$LJ::SITEROOT/syn/'>";
         $body .= LJ::html_hidden('userid', $remote->userid);
         $body .= "<b>" . BML::ml('widget.feeds.find') . "</b><br />";
         my $prompt = BML::ml('widget.feeds.enterRSS');
@@ -49,10 +51,9 @@ sub render_body {
                                  onfocus => "if(this.value=='$prompt')this.value='';",
                                  onblur  => "if(this.value=='')this.value='$prompt';"});
         $body .= " " . LJ::html_submit("action:addcustom", BML::ml('btn.add'));
-        $body .= "</form></div>";
+        $body .= "</form>";
     }
 
-    $body .= "</div>";
     $body .= "</div>";
 
     return $body;
