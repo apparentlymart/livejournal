ab19f36d85fb023fc96dca1261a55ba5686cf38f
SUP <noreply@livejournal.com>
2008-06-25 11:33:40 +0000

    LJSUP-2362:
    - Retrieve information about now listening track from <track nowplaying="true"> attribute of xml-file.
    - Recognize <error> tag and display error message from it.
    - Xml parsed in unicode, cyrillic titles retrieved without any problems.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@13997 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/LastFM.pm
+++ cgi-bin/LJ/LastFM.pm
@@ -6,15 +6,18 @@ package LJ::LastFM;
 use strict;
 
 use LWP::UserAgent;
-use HTML::TokeParser;
 use HTML::Entities;
+use XML::Parser;
 
 # Get current track
 sub current {
-    my $username = shift;
-    
+    my $username = LJ::eurl(shift);
+
+    return { error => "Missing last.fm credentials" }
+        unless $LJ::LAST_FM_API_KEY && $LJ::LAST_FM_BASE_URL;
+
     my $ua = LJ::get_useragent( role=>'last_fm', timeout=>$LJ::LAST_FM_TIMEOUT );
-    my $url = 'http://www.lastfm.com/user/' . LJ::eurl($username) . '/';
+    my $url = "$LJ::LAST_FM_BASE_URL&api_key=$LJ::LAST_FM_API_KEY&user=$username";
     my $response = $ua->get($url);
     unless ($response->is_success) {
         warn "Can't get data from last.fm: " . $response->status_line;
@@ -22,35 +25,102 @@ sub current {
     }
     
     my $content = $response->content();
-    my $p = HTML::TokeParser->new(\$content);
-    my $first = 0;
-    my $subject = 0;
-    my @nowlistening;
 
-    while ( my $token = $p->get_token ) {
-        if ( $subject ) {
-             last if $token->[0] eq 'E' && $token->[1] eq 'td'; 
-             if ( $token->[0] eq 'T' ) {
-                push @nowlistening, $token->[1] if $token->[1] =~ /\w/ && $token->[0] eq 'T';
-             }
-        } elsif ( $first ) {
-            if ( $token->[0] eq 'S' && $token->[4] =~ 'subject' && $token->[4] !~ 'colspan') {
-                $subject = 1;
-            }
-        } elsif ( $token->[0] eq 'S' && $token->[1] eq 'tr' && $token->[4] =~ /\"nowListening\"/) {
-            $first = 1;
+    # process xml
+    my $in_current_play_tag = 0;    # if we are inside track tag with nowplaying attribute
+    my $current_tag = '';           # a name of a current tag
+
+    # error
+    my $error_code  = 0;
+    my $error_message = '';
+
+    # now plaing song attributes
+    my $artist = '';
+    my $name = '';
+
+    # Handlers.
+    # on tag start:
+    my $handler_start = sub {
+        my $expat = shift;
+        my $element = shift;
+        my %attr = @_;
+
+        # catch tag 'track' with 'nowplaying=true'
+        if ($element eq "track"
+            && exists($attr{'nowplaying'}) && $attr{'nowplaying'} eq "true") {
+            $in_current_play_tag = 1;
+            return;
         }
+
+        $error_code = $attr{'code'}
+            if ($element eq "error" && exists($attr{'code'}));
+
+        # for all other tags just remember name
+        $current_tag = $element;
+    };
+
+    # on tag end:
+    my $handler_end = sub {
+        my $expat = shift;
+        my $element = shift;
+        my %attr = @_;
+
+        # if we leave 'track' tag
+        if ($element eq "track") {
+            $in_current_play_tag = 0;
+        }
+
+        # forget a name of a current tag
+        $current_tag = '';
+    };
+
+    # inside a tag:
+    my $handler_char = sub {
+        my $expat = shift;
+        my $string = shift;
+
+        # 'error'
+        if ($current_tag eq 'error') {
+            $error_message = $string;
+            return;
+        }
+
+        # pay attention only on current playing tracks
+        return unless($in_current_play_tag);
+
+        # remember song attributes
+        if ($current_tag eq "artist") {
+            $artist = $string;
+            return;
+        }
+
+        if ($current_tag eq "name") {
+            $name = $string;
+            return;
+        }
+    };
+
+    my $parser = new XML::Parser(Handlers => {
+            Start => $handler_start,
+            End   => $handler_end,
+            Char  => $handler_char,
+        });
+    eval { $parser->parse($content); };
+    if ($@) { # invalid xml
+        return { error => "Can't retrieve data from last.fm: wrong response from server" };
     }
-    
-    if (@nowlistening) {
-        my $track = ($nowlistening[1]) ? "$nowlistening[0] - $nowlistening[1]" : $nowlistening[0];
+
+    if ($error_message) {
+        return { error => "Can't retrieve data from last.fm: $error_message" };
+    }
+
+    if ($artist || $name) {
+        my $track = ($artist ? "$artist - $name" : $name) . ' | Scrobbled by Last.fm';
         $track = HTML::Entities::decode($track) . ' | Scrobbled by Last.fm';
         return { data => $track };
-    }
-    else {
+    } else {
         return { error => 'No "now listening" track in last.fm data' };
     }
 }
-
 
 1;
