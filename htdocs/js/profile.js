1a91c7f67b65063b502d2fd9a11fa0758b4bdff5
Janine Costanzo <jcostanzo@iparadigms.com>
2008-10-15 00:32:17 +0000

    LJSUP-2892
    * Allow sections of the profile to collapse/expand.
    * Remove the link to the Edit Profile page from the top of the profile.
    
    
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@14408 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ htdocs/js/profile.js
@@ -0,0 +1,32 @@
+var Profile = new Object();
+
+Profile.init = function () {
+    // add event listeners to all of the headers
+    var headers = DOM.getElementsByClassName(document, "expandcollapse");
+    headers.forEach(function (header) {
+        DOM.addEventListener(header, "click", function (evt) { Profile.expandCollapse(header.id) });
+    });
+}
+
+Profile.expandCollapse = function (headerid) {
+    var self = this;
+    var bodyid = headerid.replace(/header/, 'body');
+    var arrowid = headerid.replace(/header/, 'arrow');
+
+    // figure out whether to expand or collapse
+    var expand = !DOM.hasClassName($(headerid), 'on');
+
+    if (expand) {
+        // expand
+        DOM.addClassName($(headerid), 'on');
+        $(arrowid).src = Site.imgprefix + "/profile_icons/arrow-down.gif";
+        $(bodyid).style.display = "block";
+    } else {
+        // collapse
+        DOM.removeClassName($(headerid), 'on');
+        $(arrowid).src = Site.imgprefix + "/profile_icons/arrow-right.gif";
+        $(bodyid).style.display = "none";
+    }
+}
+
+LiveJournal.register_hook("page_load", Profile.init);
