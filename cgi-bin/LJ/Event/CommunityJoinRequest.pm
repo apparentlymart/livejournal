f94515727ac15f2a4a2a5682a8787a578be445d1
Igor Gariev <gariev@hotmail.com>
2008-11-05 19:02:29 +0000

    Adding ML key 'esn.approve_join_request' to deadphrases
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@14508 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Event/CommunityJoinRequest.pm
+++ cgi-bin/LJ/Event/CommunityJoinRequest.pm
@@ -72,7 +72,6 @@ sub as_string {
 
 my @_ml_strings_en = (
     'esn.community_join_requst.subject',    # '[[comm]] membership request by [[who]]!',
-    'esn.approve_join_request',             # '[[openlink]]Approve [[username]]\'s request to join[[closelink]]',
     'esn.manage_membership_reqs',           # '[[openlink]]Manage [[communityname]]\'s membership requests[[closelink]]',
     'esn.manage_community',                 # '[[openlink]]Manage your communities[[closelink]]',
     'esn.community_join_requst.email_text', # 'Hi [[maintainer]],
