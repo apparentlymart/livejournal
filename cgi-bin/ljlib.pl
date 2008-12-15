d3bf79d7ba81125f5cc318ed2fbeb809279e7b57
ssafronova <ssafronova@livejournal.com>
2008-12-15 10:57:24 +0000

    LJSUP-3411: Modify LJ code so that several TheScwartz databases can be used by role name -- safe option for old config
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@14657 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljlib.pl
+++ cgi-bin/ljlib.pl
@@ -324,6 +324,12 @@ sub theschwartz {
 
     return $LJ::SchwartzClient{$role} if $LJ::SchwartzClient{$role};
 
+    unless (scalar grep { defined $_->{role} } @LJ::THESCHWARTZ_DBS) { # old config
+        warn "ATTENTION! old config used - no roles for TheSchwartz DB servers";
+        $LJ::SchwartzClient{$role} = TheSchwartz->new(databases => \@LJ::THESCHWARTZ_DBS);
+        return $LJ::SchwartzClient{$role};
+    }
+
     my @dbs = grep { $_->{role}->{$role} } @LJ::THESCHWARTZ_DBS;
     die "Unknown role in LJ::theschwartz: '$role'" unless @dbs;
 
