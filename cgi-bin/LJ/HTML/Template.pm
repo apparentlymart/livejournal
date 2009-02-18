e40fa20af11b430e7a564a6f92d9afd0cd60647b
ssafronova <ssafronova@livejournal.com>
2009-02-18 06:58:59 +0000

    LJSUP-3846: create debian package for HTML::Template::Pro (LJSUP-3788)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@14903 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/HTML/Template.pm
+++ cgi-bin/LJ/HTML/Template.pm
@@ -1,14 +1,17 @@
 package LJ::HTML::Template;
 use strict;
 use HTML::Template;
+use HTML::Template::Pro;
 
 
 # Returns a new HTML::Template object
 # with some redefined default values.
 sub new {
     my $class = shift;
-    return 
-        HTML::Template->new(
+    my $opts = shift;
+    
+    if ($opts->{'use_expr'}) {
+        return HTML::Template::Pro->new(
             global_vars => 1, # normally variables declared outside a loop are not available inside
                               # a loop.  This option makes <TMPL_VAR>s like global variables in Perl 
                               # - they have unlimited scope.  
@@ -20,6 +23,20 @@ sub new {
                                     # Defaults to 1.
             @_
         );
+    } else {
+        return HTML::Template->new(
+            global_vars => 1, # normally variables declared outside a loop are not available inside
+                              # a loop.  This option makes <TMPL_VAR>s like global variables in Perl 
+                              # - they have unlimited scope.  
+                              # This option also affects <TMPL_IF> and <TMPL_UNLESS>
+
+            die_on_bad_params => 0, # if set to 0 the module will let you call 
+                                    # $template->param(param_name => 'value') even 
+                                    # if 'param_name' doesn't exist in the template body.
+                                    # Defaults to 1.
+            @_
+        );
+    }
 }
 
 
