06a0cf7b10b567f513aaf0f55bf1340e034b879f
Igor Gariev <gariev@hotmail.com>
2009-04-06 08:39:48 +0000

    LJSUP-4101: OpenID-link should be encoding correctly
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15101 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -1226,6 +1226,10 @@ sub display_name {
         $url = $id->value;
         $name = Net::OpenID::VerifiedIdentity::DisplayOfURL($url, $LJ::IS_DEV_SERVER);
         $name = LJ::run_hook("identity_display_name", $name) || $name;
+            
+        ## Decode URL's like below to human-readable names
+        ## http://blog.k.python.ru/accounts/5/%D0%94%D0%BC%D0%B8
+        $name =~ s/%([\dA-Fa-f]{2})/chr(hex($1))/ge;
     }
     return $name;
 }
