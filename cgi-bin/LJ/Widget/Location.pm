646aa8aa6337d4a080eda8be8f109a5fc3c6cd65
nefimenko <rithm_of_samba@livejournal.com>
2009-04-13 03:23:23 +0000

    LJSUP-3977 Geo-location identification - at the edit profile page
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15120 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/Location.pm
+++ cgi-bin/LJ/Widget/Location.pm
@@ -195,6 +195,11 @@ sub render_body {
 
     $ret .= "</table>\n" unless $minimal_display;
 
+		$ret .= "<div class='lc_detect'><span>" . $class->ml('.section.location.detect') . "</span>";
+		$ret .= "<input type='button' name='detect' value='" . $class->ml('.location.detect.btn') . "' id='detect'>";
+		## $ret .= " " . LJ::help_icon('upic_keywords');
+		$ret .= "</div>";
+
     $ret .= $class->html_hidden({ name => "minimal_display", value => $minimal_display, id => "minimal_display" });
 
     # javascript code in js/countryregions.js accepts list of countries with regions as a space-delimited list
