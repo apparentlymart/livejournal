a6f9de221ebc6dccfaf8fd91c98374bc39772b05
Roman Komarov <kizmarh@ya.ru>
2009-05-08 10:20:41 +0000

    LJSUP-4117: edited bday error mark-up
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15165 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/CreateAccount.pm
+++ cgi-bin/LJ/Widget/CreateAccount.pm
@@ -189,9 +189,7 @@ sub render_body {
                 notime => 1,
                 default => sprintf("%04d-%02d-%02d", $post->{bday_yyyy}, $post->{bday_mm}, $post->{bday_dd}),
             );
-			$ret .= "<div id='bdayy_check'>" . $class->ml('widget.createaccount.yearcheck', { aopts => "href='$LJ::SITEROOT/support/faqbrowse.bml?faqid=244'" }) . "<span>";
-			$ret .= "<input type='button' name='proc' value='Proceed'> 
-					<input type='button' name='wait' value='Wait! I want to change my birth date'></span></div>";
+			$ret .= "<div id='bdayy_error' class='formitemFlag'>" . $class->ml('widget.createaccount.yearcheck', { aopts => "href='$LJ::SITEROOT/support/faqbrowse.bml?faqid=244'" }) . "<span>";
         }
         $ret .= $error_msg->('bday', '<br /><span class="formitemFlag">', '</span>');
         $ret .= "</td></tr>\n" unless $alt_layout;
