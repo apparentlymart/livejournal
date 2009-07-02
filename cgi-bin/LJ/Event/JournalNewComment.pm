8ebd69a34afe85359bddb4a6a8e107c3eb769cec
mchernyshev <mchernyshev@livejournal.com>
2009-07-02 06:32:39 +0000

    LJSUP-4390: Inclusion of messenger-alert to translation system  - bugfix: s/edited/edited_reply/, e.g. '_edited_to_a_comment_' -> '_edited_reply_to_a_comment_'.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15362 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Event/JournalNewComment.pm
+++ cgi-bin/LJ/Event/JournalNewComment.pm
@@ -206,7 +206,7 @@ sub as_alert {
     return LJ::Lang::get_text($u->prop('browselang'),
         'esn.mail_comments.alert.' .
             ($comment->poster ? 'user' : 'anonymous') . '_' .
-            ($edited ? 'edited' : 'reply') . '_' .
+            ($edited ? 'edited' : '') . '_reply_' .
             (LJ::u_equals($comment->entry->poster, $u) ? 'to_your' : 'to_a') . '_' .
             ($comment->parent ? 'post' : 'comment'), undef,
                 {
