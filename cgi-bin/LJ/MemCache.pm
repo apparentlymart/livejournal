a96df700a5a7867b41067f661405eb99911ec254
vsukhanov <vsukhanov@1dfba125-df10-0410-8a77-af1a8d6d074f>
2009-09-04 07:09:22 +0000

    LJSUP-4840: add Cache::Memcached::Fast as an alternate interface to memcached.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15672 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/MemCache.pm
+++ cgi-bin/LJ/MemCache.pm
@@ -2,10 +2,16 @@
 # Wrapper around MemCachedClient
 
 use lib "$ENV{LJHOME}/cgi-bin";
-use Cache::Memcached;
+package LJ::MemCache;
 use strict;
 
-package LJ::MemCache;
+my $use_fast = not $LJ::DISABLED{disabled_cache_memcached_fast};
+if ($use_fast){
+    require Cache::Memcached::Fast;
+} else {
+    require Cache::Memcached;
+}
+
 
 use vars qw($GET_DISABLED);
 $GET_DISABLED = 0;
@@ -30,24 +36,50 @@ $GET_DISABLED = 0;
 my $memc;  # memcache object
 
 sub init {
-    my $opts = {};
 
-    my $parser_class = LJ::conf_test($LJ::MEMCACHE_USE_GETPARSERXS) ? 'Cache::Memcached::GetParserXS'
-                                                                    : 'Cache::Memcached::GetParser';
-    # Eval, but we don't care about the result here. Loading errors will have been encountered
-    # when Cache::Memcached was loaded, so we won't even see them here. This may not even return
-    # true.
-    eval "use $parser_class";
+    my $opts = _configure_opts();
+    if ($use_fast){
+        ## Init Fast (written in C) interface to Memcached
+        $memc = Cache::Memcached::Fast->new($opts);
+    
+    } else {
+        ## Init pure perl Memcached interface
 
-    # Check to see if the 'new' function/method is defined in the proper namespace, othewise don't
-    # explicitly set a parser class. Cached::Memcached may have attempted to load the XS module, and
-    # failed. This is a reasonable check to make sure it all went OK.
-    if (eval 'defined &' . $parser_class . '::new') {
-        $opts->{'parser_class'} = $parser_class;
+        ##
+        my $parser_class = LJ::conf_test($LJ::MEMCACHE_USE_GETPARSERXS) ? 'Cache::Memcached::GetParserXS'
+                                                                        : 'Cache::Memcached::GetParser';
+        eval "use $parser_class";
+
+        # Check to see if the 'new' function/method is defined in the proper namespace, othewise don't
+        # explicitly set a parser class. Cached::Memcached may have attempted to load the XS module, and
+        # failed. This is a reasonable check to make sure it all went OK.
+        if (eval 'defined &' . $parser_class . '::new') {
+            $opts->{'parser_class'} = $parser_class;
+        }
+
+        ## connect
+        $memc = Cache::Memcached->new($opts);
+
+        ##
+        if (LJ::_using_blockwatch()) {
+           eval { LJ::Blockwatch->setup_memcache_hooks($memc) };
+
+            warn "Unable to add Blockwatch hooks to Cache::Memcached client object: $@"
+                if $@;
+        }
+
+        ##
+        if ($LJ::DB_LOG_HOST) {
+            my $stat_callback = sub {
+                my ($stime, $etime, $host, $action) = @_;
+                LJ::blocking_report($host, 'memcache', $etime - $stime, "memcache: $action");
+            };
+            $memc->set_stat_callback($stat_callback);
+        }
+
     }
 
-    $memc = Cache::Memcached->new($opts);
-    reload_conf();
+    return $memc;
 }
 
 sub set_memcache {
@@ -63,37 +95,31 @@ sub client_stats {
     return $memc->{'stats'} || {};
 }
 
+
+sub _configure_opts {
+    my @servers = $use_fast
+        ? map { { address => $_, weight => 1 } } @LJ::MEMCACHE_SERVERS
+        : @LJ::MEMCACHE_SERVERS;
+        
+    return {
+        servers => \@servers,
+        compress_threshold => $LJ::MEMCACHE_COMPRESS_THRESHOLD,
+        connect_timeout    => $LJ::MEMCACHE_CONNECT_TIMEOUT,
+        nowait => 1,
+        ($use_fast
+            ? () # Cache::Memcached::Fast specefic options
+            : (  
+                # Cache::Memcached specific options
+                debug           => $LJ::DEBUG{'memcached'},
+                pref_ip         => \%LJ::MEMCACHE_PREF_IP,
+                cb_connect_fail => $LJ::MEMCACHE_CB_CONNECT_FAIL,
+                readonly        => $ENV{LJ_MEMC_READONLY} ? 1 : 0,
+                )
+        ),
+    };
+}
 sub reload_conf {
-    my $stat_callback;
-    return $memc if eval { $memc->doesnt_want_configuration; };
-
-    $memc->set_servers(\@LJ::MEMCACHE_SERVERS);
-    $memc->set_debug($LJ::DEBUG{'memcached'});
-    $memc->set_pref_ip(\%LJ::MEMCACHE_PREF_IP);
-    $memc->set_compress_threshold($LJ::MEMCACHE_COMPRESS_THRESHOLD);
-
-    $memc->set_connect_timeout($LJ::MEMCACHE_CONNECT_TIMEOUT);
-    $memc->set_cb_connect_fail($LJ::MEMCACHE_CB_CONNECT_FAIL);
-
-    if ($LJ::DB_LOG_HOST) {
-        $stat_callback = sub {
-            my ($stime, $etime, $host, $action) = @_;
-            LJ::blocking_report($host, 'memcache', $etime - $stime, "memcache: $action");
-        };
-    } else {
-        $stat_callback = undef;
-    }
-    $memc->set_stat_callback($stat_callback);
-    $memc->set_readonly(1) if $ENV{LJ_MEMC_READONLY};
-
-    if (LJ::_using_blockwatch()) {
-        eval { LJ::Blockwatch->setup_memcache_hooks($memc) };
-
-        warn "Unable to add Blockwatch hooks to Cache::Memcached client object: $@"
-            if $@;
-    }
-
-    return $memc;
+    return init();
 }
 
 sub forget_dead_hosts { $memc->forget_dead_hosts(); }
@@ -108,20 +134,104 @@ sub delete {
     $memc->delete(@_, 4) || $memc->delete(@_);
 }
 
-sub add       { (defined $_[1]) ? $memc->add(@_)    : $memc->add($_[0],     '', $_[2]) }
-sub replace   { (defined $_[1]) ? $memc->replace(@_): $memc->replace($_[0], '', $_[2]) }
-sub set       { (defined $_[1]) ? $memc->set(@_)    : $memc->set($_[0],     '', $_[2]) }
+sub add       { 
+    my ($key, $val, $exp) = @_;
+    $key = $key->[0][1]     # Cache::Memcached::Fast does not support combo [int, key] keys.
+        if $use_fast and ref $key eq 'ARRAY';
+    $val = '' unless defined $val;
+    return $memc->add($key, $val, $exp);
+}
+sub replace   { 
+    my ($key, $val) = @_;
+    $key = $key->[0][1]     # Cache::Memcached::Fast does not support combo [int, key] keys.
+        if $use_fast and ref $key eq 'ARRAY';
+    $val = '' unless defined $val;
+    return $memc->replace($key, $val);
+}
+sub set       { 
+    my ($key, $val, $exp) = @_;
+    $key = $key->[0][1]     # Cache::Memcached::Fast does not support combo [int, key] keys.
+        if $use_fast and ref $key eq 'ARRAY';
+    $val = '' unless defined $val;
+    $memc->set($key, $val, $exp);
+}
 sub incr      { $memc->incr(@_);      }
 sub decr      { $memc->decr(@_);      }
 
 sub get       {
     return undef if $GET_DISABLED;
+    if ($use_fast and ref $_[0] eq 'ARRAY'){ # Cache::Memcached::Fast does not support combo [int, key] keys.
+        $_[0] = $_[0][1];
+    }
     $memc->get(@_);
 }
+
+## gets supported only by ::Fast interface
+sub can_gets { return $use_fast }
+
+## @@ret: reference to an array [$cas, $value], or nothing.
+sub gets {
+    return $memc->gets(@_) if $use_fast and not $GET_DISABLED;
+    return; 
+}
+## @ret: reference to hash, where $href->{$key} holds a reference to an array [$cas, $value].
+sub gets_multi {
+    return $memc->gets_multi(@_) if $use_fast and not $GET_DISABLED;
+    return;
+}
+
+##
 sub get_multi {
     return {} if $GET_DISABLED;
+    if ($use_fast){ # Cache::Memcached::Fast does not support combo [int, key] keys.
+        @_ = map { ref $_ eq 'ARRAY' ? $_[0][1] : $_ } @_;
+    }
     $memc->get_multi(@_);
 }
+
+sub append {
+    my ($key, $val) = @_;
+    $val = '' unless defined $val;
+    return $use_fast
+        ? $memc->append($key, $val)
+        : _extended_set("append", $key, $val);
+}
+
+sub prepend {
+    my ($key, $val) = @_;
+    $val = '' unless defined $val;
+    return $use_fast
+        ? $memc->prepend($key, $val)
+        : _extended_set("prepend", $key, $val);
+}
+
+sub cas {
+    my ($key, $cas, $val) = @_;
+    $val = '' unless defined $val;
+    return $use_fast 
+        ? $memc->cas($key, $cas, $val)
+        : _extended_set("cas", $key, $cas, $val);
+}
+
+# Pureperl memcached interface Cache::Memcache v 1.26 does not support some memcached commands.
+# this method uses private methods of Cache::Memcache to provide new functionality
+sub _extended_set {
+    my ($cmd, @args) = @_;
+    my $append_func = ref($memc) . "::_set";
+    my $res = undef;
+
+    # Cache::Memcached::Fast has usefull flag 'nowt' - no wait for response
+    no strict 'refs';
+    if (defined wantarray()){ # scalar or list context
+        $res = &$append_func("append", $memc, @args);
+    } else {
+        &$append_func("append", $memc, @args); # void context
+    }
+    use strict 'refs';
+    return $res;
+}
+
+
 
 sub _get_sock { $memc->get_sock(@_);   }
 
