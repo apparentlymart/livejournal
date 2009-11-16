86aff77927543e082152ecc5444019996abf269e
Pavel Kornilov <ufokorpas@gmail.com>
2009-11-16 09:55:08 +0000

    LJSUP-5162: VGifts in LJ Shop: changes for AdNectar counting.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@15894 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/contextualhover.js
+++ htdocs/js/contextualhover.js
@@ -450,35 +450,7 @@ LJWidgetIPPU_AddAlias = new Class(LJWidgetIPPU, {
 });
 
 
-var Aliases = {};
-
-Aliases.init_catnav = function() {
-    var navCats = DOM.getElementsByClassName(document, "vg_cat") || [];
-    Array.prototype.forEach.call(navCats, function(cat){
-        DOM.addEventListener(cat, "click", Aliases.navClicked.bindEventListener(cat));
-    Aliases.link = Aliases.link || $('vg_cat_featured');
-    DOM.addClassName(Aliases.link, "on");
-    });
-};
-
-// Handle the event where the nav is clicked on
-Aliases.navClicked = function(evt) {
-    var id = this.id;
-    if (Aliases.nav) Aliases.nav.style.display = "none";
-    if (Aliases.link) DOM.removeClassName(Aliases.link, "on");
-
-    Aliases.nav = $('show_'+id);
-    Aliases.nav.style.display = "block";
-    Aliases.link = this;
-    DOM.addClassName(Aliases.link, "on");
-
-    Event.stop(evt);
-    return false;
-};
-
-LiveJournal.register_hook("page_load", function () {
-    Aliases.init_catnav() });
-
+Aliases = {}
 function addAlias(target, ptitle, ljusername, oldalias) {
     if (! ptitle) return true;
 	
