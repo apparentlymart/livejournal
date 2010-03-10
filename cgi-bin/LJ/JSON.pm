b9f56d089f8972fe49a6f05fc0a35544a80370d0
Andrew Ilyin <jemmix@gmail.com>
2010-03-10 12:08:26 +0000

    LJSUP-5608 (create an abstraction layer for JSON subroutines)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16346 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/JSON.pm
@@ -0,0 +1,28 @@
+package LJ::JSON;
+
+my $wrap = undef;
+
+sub to_json {
+    my ($class, @args) = @_;
+
+    return $wrap->encode(@args);
+}
+
+sub from_json {
+    my ($class, $dump) = @_;
+
+    return $wrap->decode($dump);
+}
+
+BEGIN {
+    my @classes = qw(JSON JSON::XS);
+    foreach my $class (@classes) {
+        eval "require $class";
+        unless ($@) {
+            $wrap = new $class;
+            last;
+        }
+    }
+}
+
+1;
