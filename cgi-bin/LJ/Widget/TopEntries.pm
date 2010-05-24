2f82efc9b42c7ef0fe35cf7040801dc281cbf7fe
Yegor Savintsev <egor.savintsev@gmail.com>
2010-05-24 07:48:33 +0000

    LJSUP-6079: ONTD
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16717 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/TopEntries.pm
+++ cgi-bin/LJ/Widget/TopEntries.pm
@@ -29,14 +29,14 @@ sub render_body {
     my $classname = '';
 
     foreach my $post ($top_entries->get_featured_posts()) {
-        my $comments = $post->{comments} ? '<p class="b-posts-comments">'.
+        my $comments = $post->{comments} ? '<span class="b-posts-comments">, '.
             '<a href=\''.$post->{comments_url}.'\'>'.
             BML::ml('widget.topentries.comments', { count => $post->{comments} }) .
             '</a>'.
-            '</p>' : '';
+            '</span>' : '';
         my $subj = ($post->{subj} ne '') ? $post->{subj} : $class->ml('widget.officialjournals.nosubject');
         if ($counter % 2) {$classname = 'odd';} else {$classname = 'even';}
-        $ret .= '<li class="'.$classname.'"><dl><dt><img src="'.$post->{userpic}.'" /></dt><dd><h3 class="b-posts-head"><a href="'.$post->{url}.'">'.$subj.'</a></h3>'.$comments.'<p class="b-posts-user">'.$class->ml('widget.topentries.postedby').' '.$post->{poster}.'</p></dd></dl></li>';
+        $ret .= '<li class="'.$classname.'"><dl><dt><img src="'.$post->{userpic}.'" /></dt><dd><h3 class="b-posts-head"><a href="'.$post->{url}.'">'.$subj.'</a></h3><p class="b-posts-user">'.$class->ml('widget.topentries.postedby').' '.$post->{poster}.$comments.'</p></dd></dl></li>';
         $counter = $counter + 1;
     }
 
