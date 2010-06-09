d7986a68389e2a5e040f8904aecb826e39f9edfd
mchernyshev <mchernyshev@livejournal.com>
2010-06-09 09:01:49 +0000

    LJSUP-5596: Create design of community catalogue  - multilang for AddCommunity widget.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16770 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/AddCommunity.pm
+++ cgi-bin/LJ/Widget/AddCommunity.pm
@@ -10,12 +10,19 @@ sub render_body {
     my $class = shift;
     my %opts = @_;
 
+    my ($caption, $form_text, $submit_caption) =
+        map { BML::ml('widget.addcommunity.' . $_,
+            {
+                openlink    => '<a href="#">',  # TODO: This is a url to Spotlight
+                closelink   => '</a>',
+            })
+        } qw(caption form_text submit_button_caption);
+
     return <<EOT;
-<h2>Add community</h2>
+<h2>$caption</h2>
 <form action='$LJ::SITEROOT/search/' method='post'>
-Please submit your suggestion for Community<br />
-Directory. Or submit your suggestion for <a href="#">Spotlight</a>.<br />
-<input type="submit">
+$form_text
+<input type="submit" value="$submit_caption">
 </form>
 EOT
 }
