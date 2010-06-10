4f7f57617b4e3369a156916912b92a057c4fbcc3
mchernyshev <mchernyshev@livejournal.com>
2010-06-10 03:36:36 +0000

    LJSUP-5596: Create design of community catalogue  - search widget.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16773 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/Search.pm
+++ cgi-bin/LJ/Widget/Search.pm
@@ -2,13 +2,59 @@ package LJ::Widget::Search;
 
 use strict;
 use base qw(LJ::Widget);
+use vars qw(%GET %POST);
+
 use Carp qw(croak);
 
-sub need_res { qw( stc/widgets/search.css ) }
+sub need_res { qw(stc/widgets/search.css) }
 
 sub render_body {
     my $class = shift;
     my %opts = @_;
+
+    if ($opts{type} eq 'yandex') {
+        return "" unless LJ::is_enabled('yandex_search_page');
+
+        # ML-vars
+        my %ML;
+        foreach my $k (qw(name find findall findposts findcomments findusers findfaq)) {
+            $ML{"widget.search.yandex.$k"} = $class->ml("widget.search.yandex.$k")
+        }
+
+return <<EOF
+<div class="search-forms">
+<h1>$ML{'widget.search.yandex.name'}</h1>
+<form action="/search/" method="get" id="search_form_basic" class="form-on">
+<div class="search-item search-query">
+<table>
+<tbody>
+<tr>
+<td width="60%">
+    <label for="basic_query"><input id="basic_query" name="q" class="type-text" value="" type="search"></label>
+</td>
+<td width="5%">
+    <button type="submit">$ML{'widget.search.yandex.find'}</button>
+</td>
+</tr>
+<tr>
+<td width="35%">
+    <select id="area_basic_query" class="type-select" name="area">
+        <option value="default">$ML{'widget.search.yandex.findall'}</option>
+        <option value="posts">$ML{'widget.search.yandex.findposts'}</option>
+        <option value="comments">$ML{'widget.search.yandex.findcomments'}</option>
+        <option value="journals">$ML{'widget.search.yandex.findusers'}</option>
+        <option value="faq">$ML{'widget.search.yandex.findfaq'}</option>
+    </select>
+</td>
+</tr>
+</tbody></table>
+</div>
+</fieldset>
+</form>
+</div>
+EOF
+    }
+
     my $ret;
 
     my $single_search = $opts{single_search};
@@ -36,11 +82,7 @@ sub render_body {
         $search_btn = LJ::html_submit($class->ml('.widget.search.submit'));
     }
 
-    if ($opts{type} eq 'yandex') {
-        $ret .= "<form action='$LJ::SITEROOT/search/' method='post'>\n";
-    } else {
-        $ret .= "<form action='$LJ::SITEROOT/multisearch.bml' method='post'>\n";
-    }
+    $ret .= "<form action='$LJ::SITEROOT/multisearch.bml' method='post'>\n";
     $ret .= $select_box;
     $ret .= LJ::html_text({name => 'q', 'class' => 'text', 'size' => 30}) . " ";
     $ret .= $search_btn;
