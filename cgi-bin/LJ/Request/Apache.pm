88a489b4bf42245d5a28bd49d465f5f9fba3d578
gprochaev <gprochaev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2010-06-11 11:00:08 +0000

    LJSUP-5995
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16800 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Request/Apache.pm
+++ cgi-bin/LJ/Request/Apache.pm
@@ -24,7 +24,7 @@ sub LJ::Request::M_POST                    { return Apache::Constants::M_POST() 
 sub LJ::Request::NOT_FOUND                 { return Apache::Constants::NOT_FOUND() }
 sub LJ::Request::SERVER_ERROR              { return Apache::Constants::SERVER_ERROR() }
 sub LJ::Request::BAD_REQUEST               { return Apache::Constants::BAD_REQUEST() }
-sub LJ::Request::HTTP_GONE                 { return Apache::Constants::HTTP_GONE }
+sub LJ::Request::HTTP_GONE                 { return Apache::Constants::NOT_FOUND() }
 
 my $instance = '';
 
