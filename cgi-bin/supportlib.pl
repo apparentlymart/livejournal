4afdc782a68e14a39f55a56fea93ef1be9c9dc01
Andrew Ilyin <jemmix@gmail.com>
2010-06-21 09:58:09 +0000

    LJSV-1070 (If there are encoding problems in support request, notifications are sent empty)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16851 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/supportlib.pl
+++ cgi-bin/supportlib.pl
@@ -1121,6 +1121,8 @@ sub support_notify {
 package LJ::Worker::SupportNotify;
 use base 'TheSchwartz::Worker';
 
+use LJ::Text;
+
 sub work {
     my ($class, $job) = @_;
     my $a = $job->arg;
@@ -1146,16 +1148,17 @@ sub work {
     my $body;
     my @emails;
 
+    my $req_subject = LJ::Text->fix_utf8($sp->{'subject'});
     if ($type eq 'new') {
         $body = "A $LJ::SITENAME support request has been submitted regarding the following:\n\n";
         $body .= "Category: $sp->{_cat}{catname}\n";
-        $body .= "Subject:  $sp->{subject}\n\n";
+        $body .= "Subject:  $req_subject\n\n";
         $body .= "You can track its progress or add information here:\n\n";
         $body .= "$LJ::SITEROOT/support/see_request.bml?id=$spid";
         $body .= "\n\nIf you do not wish to receive notifications of incoming support requests, you may change your notification settings here:\n\n";
         $body .= "$LJ::SITEROOT/support/changenotify.bml";
         $body .= "\n\n" . "="x70 . "\n\n";
-        $body .= $sp->{body};
+        $body .= LJ::Text->fix_utf8($sp->{body});
 
         foreach my $u (values %$userids) {
             next unless $u->is_visible;
@@ -1171,12 +1174,12 @@ sub work {
                                   undef, $sp->{spid}, $a->{splid}+0);
 
         # build body
-        $body = "A follow-up to the request regarding \"$sp->{subject}\" has ";
+        $body = "A follow-up to the request regarding \"$req_subject\" has ";
         $body .= "been submitted.  You can track its progress or add ";
         $body .= "information here:\n\n  ";
         $body .= "$LJ::SITEROOT/support/see_request.bml?id=$spid";
         $body .= "\n\n" . "="x70 . "\n\n";
-        $body .= $resp;
+        $body .= LJ::Text->fix_utf8($resp);
 
         # now see who this should be sent to
         foreach my $u (values %$userids) {
