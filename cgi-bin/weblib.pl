db7593d5474c051735909953130ed977595e06be
Natasha Arefyeva <floatboxcss@gmail.com>
2010-06-22 09:13:52 +0000

    LJSUP-6296: The bugfix for tags cloude.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16856 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -1461,7 +1461,7 @@ sub tag_cloud {
         my $pt     = int($base_font_size + $percentile->($ct) * $font_size_range);
         $ret .= "<a ";
         $ret .= "id='taglink_$tag' " unless $opts->{ignore_ids};
-        $ret .= "href='" . LJ::ehtml($tagurl) . "' style='color: <?altcolor2?>; font-size: ${pt}pt; text-decoration: none'><span style='color: <?altcolor2?>'>";
+        $ret .= "href='" . LJ::ehtml($tagurl) . "' style='font-size: ${pt}pt;'><span>";
         $ret .= LJ::ehtml($tag) . "</span></a>\n";
 
         # build hash of tagname => final point size for refresh
