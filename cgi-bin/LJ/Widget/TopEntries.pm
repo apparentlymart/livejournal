db76646a5336b339c5952f2a5ccc01917e1082c0
mchernyshev <mchernyshev@livejournal.com>
2010-06-23 06:57:36 +0000

    LJSUP-6309: Additional developing of home page widgets  - add post time to ONTD widget.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@16859 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/TopEntries.pm
+++ cgi-bin/LJ/Widget/TopEntries.pm
@@ -95,9 +95,13 @@ sub render_ontd_homepage {
         my $subj = $post->{subj} ne '' 
                     ? $post->{subj} 
                     : $class->ml('widget.officialjournals.nosubject');
-        
+
+        ## period of time in well readable format.
+        my $secondsago = time() - $post->{logtime};
+        my $posttime = LJ::TimeUtil->ago_text($secondsago);
+
         ## Spotlight row
-        $ret .= qq|<li class="$classname"><dl><dt><img src="$post->{userpic}" /></dt><dd><h3 class="b-posts-head"><a href="$post->{url}">$subj</a></h3><p class="b-posts-data"><span class="i-posts-user"> $post->{poster}$comments</p></dd></dl></li>|;
+        $ret .= qq(<li class="$classname"><dl><dt><img src="$post->{userpic}" /></dt><dd><h3 class="b-posts-head"><a href="$post->{url}">$subj</a></h3><p class="b-posts-data">$posttime | <span class="i-posts-user">$post->{poster}$comments</p></dd></dl></li>);
         
         ## switch classname
         $classname = $classname eq 'even' ? 'odd' : 'even';
