46acf3c479cdadb023403c177b2a3b3f3aa4a822
Pavel Kornilov <ufokorpas@gmail.com>
2010-07-27 07:33:09 +0000

    LJSUP-6368: JS for LJ LIVE.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17038 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/basic.js
+++ htdocs/js/basic.js
@@ -1,5 +1,49 @@
 document.documentElement.id = 'js';
 
+/* Cookie plugin. Copyright (c) 2006 Klaus Hartl (stilbuero.de) */
+function Cookie(name, value, options)
+{
+	if (value !== undefined) { // name and value given, set/delete cookie
+		options = options || {};
+		if (value === null) {
+			value = '';
+			options.expires = -1;
+		}
+		var expires = '';
+		if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+			var date;
+			if (typeof options.expires == 'number') {
+				date = new Date;
+				date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+			} else {
+				date = options.expires;
+			}
+			expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+		}
+		// CAUTION: Needed to parenthesize options.path and options.domain
+		// in the following expressions, otherwise they evaluate to undefined
+		// in the packed version for some reason...
+		var path = options.path ? '; path=' + (options.path) : '',
+			domain = options.domain ? '; domain=' + (options.domain) : '',
+			secure = options.secure ? '; secure' : '';
+		document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+	} else { // only name given, get cookie
+		var cookieValue = null;
+		if (document.cookie && document.cookie != '') {
+			var cookies = document.cookie.split(';');
+			for (var i = 0; i < cookies.length; i++) {
+				var cookie = cookies[i].trim();
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, name.length + 1) == (name + '=')) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+}
+
 //core.js
 
 /**
