4fea3be9b93be40cd76a0c3d27aa299d4b408e27
Igor Gariev <gariev@hotmail.com>
2010-08-24 04:36:12 +0000

    LJSUP-6574: Identity accounts can't to join community
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17184 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/communitylib.pl
+++ cgi-bin/communitylib.pl
@@ -344,7 +344,7 @@ sub join_community {
     return LJ::error('comm_not_found') unless $u && $cu;
     return LJ::error('comm_not_comm') unless $cu->{journaltype} eq 'C';
 
-    return LJ::error(qq|Sorry, you aren't allowed to join communities until your email address has been validated. If you've lost the confirmation email to do this, you can <a href="http://www.livejournal.com/register.bml">have it re-sent.</a>|)
+    return LJ::error(qq|Sorry, you aren't allowed to join communities until your email address has been validated. If you've lost the confirmation email to do this, you can <a href="$LJ::SITEROOT/register.bml">have it re-sent.</a>|)
         unless $u->is_validated;
 
     # friend comm -> user
