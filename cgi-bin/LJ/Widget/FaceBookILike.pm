76df95770f2592e38fde062ea289813b717848b7
Yegor Savintsev <egor.savintsev@gmail.com>
2010-09-07 08:52:40 +0000

    LJSUP-6535: Facebook Like Button With Faces
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17297 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/FaceBookILike.pm
+++ cgi-bin/LJ/Widget/FaceBookILike.pm
@@ -12,19 +12,12 @@ sub render_body {
     my $class = shift;
     my %opts = @_;
 
-#use Data::Dumper;
-#warn Dumper \%opts;
-
-#    return '';
-#    return '' unless LJ::is_enabled('widget_fb_i_like');
-
-=head
-    my $entry = LJ::Entry->new($journalu->{'userid'}, ditemid => $this->{'itemid'});
-    return $null_link unless $entry->security eq 'public';
-=cut
     my $entry_url = LJ::eurl($opts{journal}->journal_base);
+    my $width = $opts{width} || 450;
+    my $height = $opts{height} || 80;
+    my $color = $opts{color} || "light";
 
-    my $ret = qq|<iframe src="http://www.facebook.com/plugins/like.php?href=$entry_url&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>|;
+    my $ret = qq|<iframe src="http://www.facebook.com/plugins/like.php?href=$entry_url&amp;layout=standard&amp;show_faces=true&amp;width=$width&amp;action=like&amp;colorscheme=$color&amp;height=$height" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:${width}px; height:${height}px;" allowTransparency="true"></iframe>|;
 
     return $ret;
 
