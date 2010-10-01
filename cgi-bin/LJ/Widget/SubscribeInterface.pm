4cbd0c23e2482bae3cfafe3aa95d96b38cd54d29
gprochaev <gprochaev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2010-10-01 03:53:11 +0000

    LJSUP-7017. SMS notification column must be visible always for RU IP
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17479 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/SubscribeInterface.pm
+++ cgi-bin/LJ/Widget/SubscribeInterface.pm
@@ -23,8 +23,9 @@ sub render_body {
     my @ntypes = @LJ::NOTIFY_TYPES;
     my (undef, $country) = LJ::GeoLocation->ip_class;
     if ($LJ::DISABLED{smsru} or
-        ($country ne 'RU' and
-         not LJ::SMS::API::RU::Phone->is_users_number_supported($u))
+        ($country ne 'RU'
+#        and not LJ::SMS::API::RU::Phone->is_users_number_supported($u)
+        )
     ){
         @ntypes = grep { $_ ne 'LJ::NotificationMethod::SMSru' ? 1 : 0 } @ntypes;
     }
