7c71b2763ffadea02048e1547b22d973c1472b0f
gprochaev <gprochaev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2010-11-24 07:19:49 +0000

    LJSUP-7413. incorrect url in block "Featured posts"
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17765 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/Browse.pm
+++ cgi-bin/LJ/Widget/Browse.pm
@@ -335,6 +335,7 @@ sub render_body {
                 updated_ago     => LJ::TimeUtil->ago_text($entry->logtime_unix),
                 comments_count  => $entry->reply_count,
                 ljuser          => $entry->journal->ljuser_display,
+                url_to_post     => $entry->url,
             };
         }
     }
