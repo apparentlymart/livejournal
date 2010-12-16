cd4e343e4eb6027b4e780b63135a4a5fe7f52adf
vsukhanov <vsukhanov@1dfba125-df10-0410-8a77-af1a8d6d074f>
2010-12-16 08:37:19 +0000

    LJSUP-7635: clean friendstimes db cache
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@17943 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/maint/clean_caches.pl
+++ bin/maint/clean_caches.pl
@@ -25,6 +25,18 @@ $maint{'clean_caches'} = sub
         $dbh->do("DELETE FROM underage WHERE timeof < (UNIX_TIMESTAMP() - 86400*90) LIMIT 2000");
     }
 
+    print "-I- Cleaning friendstimes.\n";
+    foreach my $c (@LJ::CLUSTERS) {
+        my $dbcm = LJ::get_cluster_master($c);
+        $dbcm->do("DELETE FROM friendstimes WHERE added < UNIX_TIMESTAMP() - 86400*7 LIMIT 100000");
+    }
+
+    print "-I- Cleaning comet_history.\n";
+    foreach my $c (@LJ::CLUSTERS) {
+        my $dbcm = LJ::get_cluster_master($c);
+        $dbcm->do("DELETE FROM comet_history WHERE added < UNIX_TIMESTAMP() - 86400*10 LIMIT 100000");
+    }
+
     print "-I- Cleaning captcha sessions.\n";
     foreach my $c (@LJ::CLUSTERS) {
         my $dbcm = LJ::get_cluster_master($c);
