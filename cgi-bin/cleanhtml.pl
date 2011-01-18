124022514d4fe90ec4ee2f160dde44e776091ae3
afedorov <red_alf@livejournal.com>
2011-01-18 10:40:59 +0000

    OPSC-230: Some new corrections
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@18044 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/cleanhtml.pl
+++ cgi-bin/cleanhtml.pl
@@ -634,12 +634,12 @@ sub clean
             }
             elsif ($tag eq "lj-app")
             {
-        	next TOKEN if $LJ::DISABLED{'userapps'};
-         	map { $attr->{$_} = Encode::encode_utf8($attr->{$_}) } keys %$attr; 
-        	my $app = LJ::UserApps->get_application( %$attr );
-        	next TOKEN unless $app && $app->can_show_restricted;
-            $newdata .= Encode::decode_utf8($app->ljapp_display($attr->{extra}, $attr->{title}), Encode::FB_QUIET);
-        	next TOKEN;
+                next TOKEN if $LJ::DISABLED{'userapps'};
+                my %app_attr = map { $_ => Encode::encode_utf8($attr->{$_}) } keys %$attr;
+                my $app = LJ::UserApps->get_application( %app_attr );
+                next TOKEN unless $app && $app->can_show_restricted;
+                $newdata .= Encode::decode_utf8($app->ljapp_display($attr->{extra}, $attr->{title}), Encode::FB_QUIET);
+                next TOKEN;
             }
             elsif ($tag eq "lj")
             {
