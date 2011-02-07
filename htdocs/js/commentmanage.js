ae9a4a8ba391f6bc9f87e4e8cb6745f142759d34
Dmitry Petrov <dpetroff@gmail.com>
2011-02-07 07:12:15 +0000

    LJSUP-7866: ML-variables for deleting comment bubble
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@18222 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/commentmanage.js
+++ htdocs/js/commentmanage.js
@@ -306,7 +306,7 @@ function createDeleteFunction(ae, dItemid, isS1) {
             }
             if (canAdmin&&com.username) {
                 lbl = "ljpopdel" + dItemid + "author";
-                inHTML += "<input type='checkbox' name='delauthor' id='" + lbl + "'> <label for='" + lbl + "'>" + getLocalizedStr( 'comment.delete.all.sub', "<b>" + ( (com.username == remoteUser ? 'my' : com.u) ) + "</b>" ) + "</label><br />";
+                inHTML += "<input type='checkbox' name='delauthor' id='" + lbl + "'> <label for='" + lbl + "'>" + getLocalizedStr( 'comment.delete.all', "<b>" + ( (com.username == remoteUser ? 'my' : com.u) ) + "</b>" ) + "</label><br />";
             } else {
                 finalHeight -= 15;
             }
