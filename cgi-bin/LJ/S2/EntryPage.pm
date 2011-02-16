a8583a048c0e4ab2a7ba026d0b3ee1b2e87c8e57
Igor Askarov <juks@juks.ru>
2011-02-16 11:21:17 +0000

    LJSUP-7848
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@18332 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2/EntryPage.pm
+++ cgi-bin/LJ/S2/EntryPage.pm
@@ -82,6 +82,8 @@ sub EntryPage
                             comment.delete.no.options
                             comment.ban.user
                             comment.mark.spam
+							comment.mark.spam.title
+							comment.mark.spam.subject
                             comment.delete/)
     }
 
