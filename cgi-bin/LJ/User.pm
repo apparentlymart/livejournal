60ecc203ef93ff455b3617f5189105b9ead6c6dc
Natasha Arefyeva <floatboxcss@gmail.com>
2011-04-14 03:29:24 +0000

    LJSUP-8448: mailru-icon changed
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@18873 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -7367,7 +7367,7 @@ sub ljuser {
     ### populate userhead data
     if ($userhead !~ /^https?:\/\//) {
         my $imgroot = $opts->{'imgroot'} || $LJ::IMGPREFIX;
-        $userhead = $imgroot . '/' . $userhead . "?v=2";
+        $userhead = $imgroot . '/' . $userhead . "?v=3";
     }
 
     $userhead_h ||= $userhead_w;  # make square if only one dimension given
