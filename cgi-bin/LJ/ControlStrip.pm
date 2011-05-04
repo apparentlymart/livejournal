0ca21764f8d1bc8ddc3580adcac92e3cb3e433f5
dnikolaev <wildkin@livejournal.com>
2011-05-04 04:18:25 +0000

    LJSUP-8738: Community Directory is showing for sup-users
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@19015 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/ControlStrip.pm
+++ cgi-bin/LJ/ControlStrip.pm
@@ -85,6 +85,7 @@ sub render
         $data_remote->{user}         = $remote->{user};
         $data_remote->{display}      = LJ::ljuser($remote);
         $data_remote->{sessid}       = ($remote->session ? $remote->{_session}->{sessid} : undef);
+        $data_remote->{is_sup}       = (LJ::SUP->is_sup_enabled($remote) ? 1 : 0);
         $data_remote->{is_paid}      = $remote->in_class('paid') || $remote->in_class('sponsored');
         $data_remote->{is_personal}  = ($remote->is_personal() ? 1 : 0); 
         $data_remote->{is_identity}  = ($remote->is_identity() ? 1 : 0);
