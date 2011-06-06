423a709ec760560c13f752d5dba0fd73645304fd
vadvs <vadvs@livejournal.com>
2011-06-06 09:49:30 +0000

    LJINT-435: fix template filename.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@19239 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/LoginMulti.pm
+++ cgi-bin/LJ/Widget/LoginMulti.pm
@@ -30,7 +30,7 @@ sub render_body {
     my $filename = 'Login';
     if ( $opts{'embedded'} ) {
         my $partner = $opts{'partner'};
-        $filename = 'ExternalLogin/v' . $partner->widget_version . '.tmpl';
+        $filename = 'ExternalLogin/v' . $partner->widget_version;
     }
 
     ## Draw widget
