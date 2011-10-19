a85ba1f831ead9a582bbf50372b290496583a93a
Dmitry Petrov <dpetroff@gmail.com>
2011-10-19 12:21:30 +0000

    LJSUP-10181: incorrect link in contextual pop-up
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@20383 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/contextualhover.js
+++ htdocs/js/contextualhover.js
@@ -349,7 +349,7 @@ function addAlias(target, ptitle, ljusername, oldalias, callback) {
 			// member of community
 			if (data.is_logged_in && data.is_comm) {
 				linkGroup.push({
-					selector: 'a[href={url}]',
+					selector: 'a[href="{url}"]',
 					url: data.is_member ? data.url_leavecomm : data.url_joincomm,
 					text: data.is_member ? data.ml_leave : data.ml_join_community,
 					click: function(e)
