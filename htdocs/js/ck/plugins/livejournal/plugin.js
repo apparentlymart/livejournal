4421c4c2f27a16f0b82ef5b4bad5340f027f3d9c
Vlad Kurkin <b-vladi@cs-console.ru>
2011-11-03 13:27:24 +0000

    LJSUP-10299: Disappeared text after click button lj-cut
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@20476 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/ck/plugins/livejournal/plugin.js
+++ htdocs/js/ck/plugins/livejournal/plugin.js
@@ -623,26 +623,28 @@
 						}
 					} else {
 						if(text = prompt(top.CKLang.CutPrompt, top.CKLang.ReadMore)){
-							var selection = new CKEDITOR.dom.selection(editor.document),
-								ranges = selection.getRanges();
+							var selection = new CKEDITOR.dom.selection(editor.document);
 
-							var ljCutNode = new CKEDITOR.dom.element.get(editor.document.$.createElement('lj:cut'));
-							ljCutNode.setAttribute('lj-cmd', 'LJCut');
+							ljTagsData.LJCut.node = new CKEDITOR.dom.element.get(document.createElement('lj:cut'));
+							ljTagsData.LJCut.node.setAttribute('lj-cmd', 'LJCut');
 
 							if(text != top.CKLang.ReadMore){
-								ljCutNode.setAttribute('text', text);
+								ljTagsData.LJCut.node.setAttribute('text', text);
 							}
 
+							var ranges = selection.getRanges();
+							selection.lock();
 							for(var i = 0, l = ranges.length; i < l; i++){
 								var range = ranges[i];
-								ljCutNode.append(range.extractContents());
+								range.cloneContents().appendTo(ljTagsData.LJCut.node);
 							}
-
-							editor.insertElement(ljCutNode);
+							selection.unlock();
+							editor.insertElement(ljTagsData.LJCut.node);
+							selection.selectElement(ljTagsData.LJCut.node);
 						}
-
-						CKEDITOR.note && CKEDITOR.note.hide(true);
 					}
+
+					CKEDITOR.note && CKEDITOR.note.hide(true);
 				},
 				editorFocus: false
 			});
