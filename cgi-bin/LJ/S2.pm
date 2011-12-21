4a3dca836f2db467c8f6d8af066d743b478347c0
Alexander Nazarov <n4kz@n4kz.com>
2011-12-21 06:56:37 +0000

    LJSUP-10840: Dont work mass action in s2
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@20826 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2.pm
+++ cgi-bin/LJ/S2.pm
@@ -4321,7 +4321,8 @@ sub EntryPage__print_multiform_start
                         "no_comments: \"" . LJ::ejs($pr->{'text_multiform_no_comments'}) . "\"," .
                         "conf_delete: \"" . LJ::ejs($pr->{'text_multiform_conf_delete'}) . "\"" .
                     "})'>\n" .
-                LJ::html_hidden("ditemid", $this->{'entry'}->{'itemid'},
+                LJ::html_hidden("lj_form_auth", LJ::form_auth(1),
+                                "ditemid", $this->{'entry'}->{'itemid'},
                                 "journal", $this->{'entry'}->{'journal'}->{'username'}) . "\n");
 }
 
