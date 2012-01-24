7768402733eb5e7ed5c0bd1aee8015820d312d91
Alexander Nazarov <n4kz@n4kz.com>
2012-01-24 09:12:38 +0000

    int: LJSUP-11023: S1 Comments profiling and optimization
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@20966 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -5467,11 +5467,11 @@ sub can_manage {
     # is same user?
     return 1 if LJ::u_equals($u, $remote);
 
-    # do not allow suspended users manage other accounts
-    return 0 if $remote->is_suspended;
-
     # people/syn/rename accounts can only be managed by the one account
     return undef if $u->{journaltype} =~ /^[PYR]$/;
+
+    # do not allow suspended users manage other accounts
+    return 0 if $remote->is_suspended;
 
     # check for supermaintainer
     return 1 if $remote->can_super_manage($u);
