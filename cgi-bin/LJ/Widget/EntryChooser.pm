2cf2fdf0ce2d32e441f226f64e1cad9c474a2a47
madeon <madeon@livejournal.com>
2012-01-24 11:35:10 +0000

    LJSUP-10432: removing functionality from edittags.bml.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@20968 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/EntryChooser.pm
+++ cgi-bin/LJ/Widget/EntryChooser.pm
@@ -21,6 +21,7 @@ sub prepare_template_params {
     my @entries_display;
 
     foreach my $entry (@$entries) {
+        my $entry_id = $entry->is_delayed ? $entry->delayedid : $entry->itemid ;
         my $entry_can_edit = 
             $entry->poster->equals($remote) &&
             ! $entry->journal->is_readonly &&
@@ -48,16 +49,12 @@ sub prepare_template_params {
             }
         }
 
-        my ($edittags_link, $entry_can_edittags);
         my $edit_link_base = "$LJ::SITEROOT/editjournal.bml?";
         my $usejournal = $entry->journal->username;
         $edit_link_base .= 'usejournal=' . $usejournal . '&';
 
         if ( $entry->is_delayed ) {
             $edit_link_base .= 'delayedid=' . $entry->delayedid . '&';
-            $edittags_link  = "$LJ::SITEROOT/edittags.bml?" .
-                              "journal=$usejournal&delayedid=" .
-                              $entry->delayedid;
         } else {
             $edit_link_base .= 'itemid=' . $entry->ditemid . '&';
         }
@@ -96,19 +93,14 @@ sub prepare_template_params {
             $entry_taglist = join( ', ', @taglist );
         }
 
-        if ( $entry->is_delayed ) {
-            $entry_can_edittags = $entry_can_edit;
-        }
-
         push @entries_display, {
+            'entry_id'           => $entry_id,
             'entry_can_edit'     => $entry_can_edit,
-            'entry_can_edittags' => $entry_can_edittags,
             'poster_ljuser'      => $poster_ljuser,
             'entry_is_delayed'   => $entry_is_delayed,
             'entry_is_sticky'    => $entry_is_sticky,
             'entry_security'     => $entry_security,
             'edit_link'          => $edit_link,
-            'edittags_link'      => $edittags_link,
             'delete_link'        => $delete_link,
             'entry_url'          => $entry_url,
             'entry_subject'      => $entry_subject,
