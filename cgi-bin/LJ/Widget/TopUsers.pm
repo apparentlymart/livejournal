ab90b5a00b66545ca12ba8785bdf1e4b0bf685eb
afedorov <red_alf@livejournal.com>
2012-02-16 14:50:55 +0000

    LJSUP-11289: Adjust 'top commenters' widget for anythingdiz community
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@21204 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/TopUsers.pm
+++ cgi-bin/LJ/Widget/TopUsers.pm
@@ -18,7 +18,7 @@ my $use_debug_data  = 0;
 my %debug_data = (
     'ontd_authors'      => '[{"count":"2","userid":"3"},{"count":"1","userid":"4"}]',
     'ontd_commenters'   => '[{"count":"276","userid":"2"},{"count":"170","userid":"3"},{"count":"139","userid":"4"},{"count":"124","userid":"5"},{"count":"123","userid":"6"},{"count":"120","userid":"6"}]',
-    'adisney_comm' => '[{"count":"2","userid":"3"},{"count":"1","userid":"4"}]',
+    'adisney_comm'      => '[{"count":"2","userid":"3"},{"count":"1","userid":"4"}]',
 
 );
 
@@ -26,7 +26,7 @@ sub _fetch_data {
     %keys = (
         #'ontd_authors'      => { title => "widget.topusers.top5posters.title",   order => 1, data => [] domain => 'ontd' },
         'ontd_commenters' => { title => "widget.topusers.top5commenters.title", order => 2, data => [], domain => 'hmp_ontd' },
-        'adisney_comm'    => { title => "widget.topusers.top5commenters.title", order => 3, data => [], domain => 'anythingdisney' },
+        'adisney_comm'    => { title => "widget.topusers.top5commenters.title", order => 3, data => [], domain => 'anythingdiz' },
     );
 
     foreach my $key (keys %keys) {
@@ -86,7 +86,7 @@ sub render_body {
                grep { $keys{$_}->{'domain'} eq $domain }
                keys %keys;
 
-    $ret .= sprintf '<div class=\'w-topcommenters %s\'>', $domain eq 'anythingdisney'? '' : 'w-ontd';
+    $ret .= sprintf '<div class=\'w-topcommenters %s\'>', $domain eq 'anythingdiz'? '' : 'w-ontd';
     $ret .= "<div class='w-head'>";
     $ret .= "<h2><span class='w-head-in'>Top commenters</span></h2>";
     $ret .= "<i class='w-head-corner'></i>";
