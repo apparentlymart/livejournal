5dc9b3480ba93b4d781ce90dff31eef7d275534e
vadvs <vadvs@livejournal.com>
2012-03-30 08:15:46 +0000

    LJSUP-11448: Use scissors instead of () in the lj-cut
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@21603 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/cleanhtml.pl
+++ cgi-bin/cleanhtml.pl
@@ -704,7 +704,7 @@ sub clean {
                     my $etext = $link_text->();
                     my $url = LJ::ehtml($cut);
                     $newdata .= "<div>" if $tag eq "div";
-                    $newdata .= "<b class=\"ljcut-link\">(&nbsp;<a href=\"$url#cutid$cutcount\">$etext</a>&nbsp;)</b>";
+                    $newdata .= "<b class=\"ljcut-link\">&#9988;&nbsp;<a href=\"$url#cutid$cutcount\">$etext</a>&nbsp;</b>";
                     $newdata .= "</div>" if $tag eq "div";
                     unless ($opts->{'cutpreview'}) {
                         push @eatuntil, $tag;
