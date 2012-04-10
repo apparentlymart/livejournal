c943e095f896c7bccfaec33c9ec8006256386887
Dmitry Petrov <dpetroff@gmail.com>
2012-04-10 08:37:18 +0000

    LJSUP-11753: Tags from personal account instead community tags showed for moderator when adding tag from query page
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@21684 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/community/moderate.js
+++ htdocs/js/community/moderate.js
@@ -1,12 +1,17 @@
-function selectModTags(node) {
-	var widget = new LJWidgetIPPU_SelectTags({
-		title: node.firstChild.nodeValue,
-		height: 329,
-		width: jQuery(window).width() / 2
-	}, {
-		user: jQuery(document.forms.authForm.authas).val()
-	});
+(function($) {
+	var user = LiveJournal.parseGetArgs().authas;
 
-	return false;
-}
+	window.selectModTags = function(node) {
+		var widget = new LJWidgetIPPU_SelectTags({
+			title: node.firstChild.nodeValue,
+			height: 329,
+			width: jQuery(window).width() / 2
+		}, {
+			user: user || jQuery(document.forms.authForm.authas).val()
+		});
+
+		return false;
+	}
+
+})(jQuery);
 
