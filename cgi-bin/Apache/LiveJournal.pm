b070627a40e66970c4c0a76b5af206352fcbbe86
Andrew Kanashin <holydrew@gmail.com>
2012-04-26 10:45:29 +0000

    LJSUP-11857: Cyr sub domains
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@21804 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/Apache/LiveJournal.pm
+++ cgi-bin/Apache/LiveJournal.pm
@@ -1209,8 +1209,13 @@ sub trans {
 
         $u = LJ::want_user($u);
 
-        my $view = $determine_view->($u->user, "other:$host$hostport", $uri);
-        return $view if defined $view;
+        if ($LJ::DOMAIN_JOURNALS_REVERSE{$u->user} || $LJ::DOMAIN_JOURNALS{$u->user}) {
+            my $view = $determine_view->($u->user, "other:$host$hostport", $uri);
+            return $view if defined $view;
+        } else {
+            return redir("http://".$u->user.$LJ::USER_DOMAIN);
+        }
+
         LJ::Request->pnotes ('error' => 'baduser');
         LJ::Request->pnotes ('remote' => LJ::get_remote());
         return LJ::Request::NOT_FOUND;
