28e2ae100689b85f56412ee1e0324d801f856521
wisest-owl <wisest-owl@1dfba125-df10-0410-8a77-af1a8d6d074f>
2012-05-10 09:25:23 +0000

    LJSUP-11807: Photohosting branding
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@21909 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -22,6 +22,7 @@ use Class::Autouse qw(
                       );
 use LJ::ControlStrip;
 use LJ::SiteScheme;
+use LJ::Pics::Album;
 use Apache::WURFL;
 use Encode;
 
@@ -2383,7 +2384,19 @@ sub get_body_class_for_service_pages {
     } elsif ($uri =~ m!^/shop(/.*)?$!) {
         push @classes, "shop-page";
     } elsif ($uri =~ m!^/pics(/.*)?$!) {
-        push @classes, "framework-page";
+        if (LJ::_is_pics_branding_active()) {
+            my $remote = LJ::get_remote ();
+            if (LJ::Pics::Album->list( 'userid' => $remote->userid )) {
+                ## photos exists
+                push @classes, "framework-page-view";
+            } else {
+                ## no photos
+                push @classes, "framework-page-promo";
+            }
+        } else {
+            ## branding is not active
+            push @classes, "framework-page";
+        }
     } elsif ($uri =~ m!^/browse(/.*)?$!) {
         push @classes, "catalogue-page";
     } elsif ($uri =~ m!^/games(/.*)?$! || $host eq "$LJ::USERAPPS_SUBDOMAIN.$LJ::DOMAIN") {
