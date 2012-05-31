4526a52dc35ce383b6a6dfae76248e1011981b50
vadvs <vadvs@livejournal.com>
2012-05-31 13:35:47 +0000

    LJSUP-12416: Do not show 'Recent Comments' widget for identity user
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22141 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Widget/RecentComments.pm
+++ cgi-bin/LJ/Widget/RecentComments.pm
@@ -18,6 +18,7 @@ sub render_body {
 
     my $u = $opts{user} && LJ::isu($opts{user}) ? $opts{user} : LJ::get_remote();
     return "" unless $u;
+    return "" if $u->is_identity;
 
     my $limit = defined $opts{limit} ? $opts{limit} : 3;
 
