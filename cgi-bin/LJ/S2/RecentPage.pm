b7f9738535af8b8eaff02c97eed9239fb42301a3
madeon <madeon@livejournal.com>
2012-05-31 13:46:57 +0000

    LJSUP-11909: Fast Repost feature
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22142 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/S2/RecentPage.pm
+++ cgi-bin/LJ/S2/RecentPage.pm
@@ -55,6 +55,10 @@ sub RecentPage
 
     LJ::load_user_props($remote, "opt_nctalklinks", "opt_ljcut_disable_lastn");
 
+    if ($remote) {
+        LJ::need_string(qw/repost.confirm.delete/);
+    }
+
     my $get = $opts->{'getargs'};
 
     if ($opts->{'pathextra'}) {
