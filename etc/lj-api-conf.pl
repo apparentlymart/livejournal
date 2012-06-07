b2f44aed8678ea4c76c92d5b349eb6f185edc69f
Natalya Marfina <nikulochkina@livejournal.com>
2012-06-07 07:00:43 +0000

    LJSUP-12247: Implement requested scopes/permissions mapping for XML-RPC functions
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22185 1dfba125-df10-0410-8a77-af1a8d6d074f

--- etc/lj-api-conf.pl
+++ etc/lj-api-conf.pl
@@ -23,6 +23,49 @@
         sessiongenerate  => 0,
         syncitems        => 0,
     );
+
+    %LJ::XMLRPC_USER_ACCESS = (
+        login            => 'profile_ro',
+        getfriendgroups  => 'friends_ro',
+        getfriends       => 'friends_ro',
+        friendof         => 'friends_ro',
+        checkfriends     => 'friends_ro',
+        getdaycounts     => 'journal_ro',
+        postevent        => 'journal_rw',
+        editevent        => 'journal_rw',
+        syncitems        => 'journal_ro',
+        getevents        => 'journal_ro',
+        editfriends      => 'friends_rw',
+        editfriendgroups => 'friends_rw',
+        consolecommand   => 'console',
+        getusertags      => 'profile_ro',
+        getfriendspage   => ['journal_ro', 'friends_ro'],
+        getinbox         => 'messages',
+        sendmessage      => 'messages',
+        setmessageread   => 'messages',
+        addcomment       => 'journal_rw',
+
+        getrecentcomments => 'journal_ro',
+        getcomments       => 'journal_ro',
+        deletecomments    => 'journal_rw',
+        updatecomments    => 'journal_rw',
+        editcomment       => 'journal_rw',
+
+        getuserpics       => 'profile_ro',
+        createpoll        => 'journal_rw',
+        getpoll           => 'journal_ro',
+        editpoll          => 'journal_rw',
+        votepoll          => 'journal_rw',
+        registerpush      => 'push',
+        unregisterpush    => 'push',
+        pushsubscriptions => 'push',
+        resetpushcounter  => 'push',
+        getpushlist       => 'push',
+
+        geteventsrating   => 'ratings',
+        getusersrating   => 'ratings',
+    );
+
 }
 
 1;
