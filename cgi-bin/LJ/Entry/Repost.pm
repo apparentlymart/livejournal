0b983dc165d1284b541f3bd19c3b03fe83a9195d
madeon <madeon@livejournal.com>
2012-06-15 11:51:44 +0000

    LJSUP-12531: Repost: Error: Not able to create new entry
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22260 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Entry/Repost.pm
+++ cgi-bin/LJ/Entry/Repost.pm
@@ -127,28 +127,23 @@ sub __create_post {
     # move to LJ::API
     my $res = LJ::Protocol::do_request("postevent", \%req, \$err, $flags);
 
-
+    $flags->{u} = undef;
     my $fail = !defined $res->{itemid} && $res->{message};
     if ($fail) {
-         warn "----------------------------";
-         warn "request " . LJ::D(%req);
-         warn "flags: " . LJ::D($flags);
-         warn "result: " . LJ::D($res);
-         warn "error: $err" if $err;
-         warn "---------------------------------";
+         warn "repost_create: 1, request " . LJ::compact_dumper(%req);
+              " flags: " . LJ::compact_dumper($flags) .
+              " result: " . LJ::compact_dumper($res) .
+              " error: $err" if $err;
 
          $$error = LJ::API::Error->make_error( $res->{message},($err || -10000) );
          return;
     }
 
-    if ($err) {
-            
-        warn "----------------------------";
-        warn "request " . LJ::D(%req);
-        warn "flags: " . LJ::D($flags);
-        warn "result: " . LJ::D($res);
-        warn "error: $err";
-        warn "---------------------------------";
+    if ($err) {    
+        warn "repost_create: 2, request " . LJ::compact_dumper(%req) .
+            " flags: " . LJ::compact_dumper($flags) .
+            " result: " . LJ::compact_dumper($res) .
+            " error: $err";
 
         $$error = LJ::API::Error->get_error('create_entry_failed');
         return;
