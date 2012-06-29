443d55c21db969c2abaa9daaac416c63b3e6c8e5
Natalya Marfina <nikulochkina@livejournal.com>
2012-06-29 07:01:56 +0000

    LJSUP-11578: XML RPC, функция getevents: parseljtags не корректно работает с lj user
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22348 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljtextutil.pl
+++ cgi-bin/ljtextutil.pl
@@ -855,12 +855,14 @@ sub convert_lj_tags_to_links {
     $args{event} =~ s#<lj\-embed[^>]+/>#<a href="$args{embed_url}">View movie.</a>#g;
     while ( $args{event} =~ /<lj\s+user="([^>"]+)"\s*\/?>/g ){
         # follow the documentation - no about communites, openid or syndicated, just user
-        my $user = LJ::load_user($1); 
+        my $username = $1;
+        my $user = LJ::load_user($username); 
+        # $username may be not equal to $name (if $username is not canonical username)
         my $name = $user->username;
         my $html = '<a href="' . $user->profile_url . '" target="_blank"><img src="' 
         . $LJ::IMGPREFIX . '/userinfo.gif?v=17080" alt=""></a><a href="'
         . $user->journal_base . '" target="_blank">' . $name . '</a>';
-        $args{event} =~ s#<lj\s+user="$name"\s*\/?>#$html#g;
+        $args{event} =~ s#<lj\s+user="$username"\s*\/?>#$html#g;
     }
     $args{event} =~ s#</?lj-cut[^>]*>##g;
     
