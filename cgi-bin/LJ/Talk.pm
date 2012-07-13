d29447bb3a2b52e74496a7ce738c68116865e24c
wisest-owl <wisest-owl@1dfba125-df10-0410-8a77-af1a8d6d074f>
2012-07-13 12:12:33 +0000

    LJSUP-12973: Prop 'custom_usericon_individual' is necessary to load for all users at one time
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22441 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Talk.pm
+++ cgi-bin/LJ/Talk.pm
@@ -1683,7 +1683,7 @@ sub load_comments
     my %up = ();
     if (%users_to_load) {
         LJ::load_userids_multiple([ map { $_, \$up{$_} } keys %users_to_load ]);
-        LJ::load_user_props_multi([values %up], [qw{ custom_usericon }]);
+        LJ::load_user_props_multi([values %up], [qw{ custom_usericon custom_usericon_individual }]);
  
         # fill in the 'userpost' member on each post being shown
         while (my ($id, $post) = each %$posts) {
