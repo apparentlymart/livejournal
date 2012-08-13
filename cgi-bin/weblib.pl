625865c2d9be5b6cc39f1d69f77e2454574d7753
Alexander Nazarov <n4kz@n4kz.com>
2012-08-13 07:41:24 +0000

    LJSUP-13271: Trace messages from basicWidget pub/sub system in real time
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22624 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/weblib.pl
+++ cgi-bin/weblib.pl
@@ -1558,6 +1558,8 @@ sub res_includes {
 
         LJ::run_hooks('add_to_site_js', \%site);
 
+        LJ::need_var(D => \%LJ::JS_D);
+
         my $site_params = LJ::js_dumper(\%site);
 
         my $journal_info_json = LJ::JSON->to_json(\%journal_info);
