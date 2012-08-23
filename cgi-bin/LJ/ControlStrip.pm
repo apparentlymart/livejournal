67dcf95cc3997e9e5808fd1d7194de974e571d1a
madeon <madeon@livejournal.com>
2012-08-23 09:45:58 +0000

    LJSUP-13387: Fix error in ControlStrip
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22725 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/ControlStrip.pm
+++ cgi-bin/LJ/ControlStrip.pm
@@ -363,8 +363,8 @@ sub render
         pop @early_date;
         pop @last_date;
 
-        if (@last_date[1] != 0) {
-            @last_date[1] -= 1;
+        if ($last_date[1] != 0) {
+            $last_date[1] -= 1;
         }
 
         if (@early_date[1] != 0) {
@@ -373,9 +373,9 @@ sub render
 
         my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
             gmtime();
-        if ( @last_date[0] < ($year + 1900) ||
-             @last_date[1] < $mon     ||
-             @last_date[2] < $mday )
+        if ( $last_date[0] < ($year + 1900) ||
+             $last_date[1] < $mon     ||
+             $last_date[2] < $mday )
         {
             @last_date = ($year + 1900, $mon, $mday);
         }
