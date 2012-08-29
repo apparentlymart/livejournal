05c78d163aa0527b45c6bf236fdd0b552ff7d673
wisest-owl <wisest-owl@1dfba125-df10-0410-8a77-af1a8d6d074f>
2012-08-29 11:12:18 +0000

    LJSUP-13336: Capital letter in the tags
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@22764 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljlib.pl
+++ cgi-bin/ljlib.pl
@@ -2510,7 +2510,7 @@ sub get_keyword_id
     # setup the keyword for use
     unless ($kw =~ /\S/) { return 0; }
     $kw = LJ::text_trim($kw, LJ::BMAX_KEYWORD, LJ::CMAX_KEYWORD);
-    $kw = LJ::Text->normalize_tag_name ($kw);
+    #$kw = LJ::Text->normalize_tag_name ($kw);
 
     # get the keyword and insert it if necessary
     my $kwid;
