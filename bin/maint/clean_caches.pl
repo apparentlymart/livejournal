b71fcff1e1bed81e73f2583a845bab9774202f43
Andreas Echavez <oceanplexian@livejournal.com>
2012-12-30 20:59:22 +0000

    See http://rt.corp.livejournal.org/Ticket/Display.html?id=2096#txn-45877. DO NOT check this out on admin02 until we've had time to properly flush the DB manually
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23513 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/maint/clean_caches.pl
+++ bin/maint/clean_caches.pl
@@ -205,7 +205,7 @@ $maint{'clean_caches'} = sub
     print "-I- Cleaning comet_history.\n";
     foreach my $c (@LJ::CLUSTERS) {
         my $dbcm = LJ::get_cluster_master($c);
-        $dbcm->do("DELETE FROM comet_history WHERE added < UNIX_TIMESTAMP() - 86400*10 LIMIT 100000");
+        $dbcm->do("DELETE FROM comet_history WHERE added < FROM_UNIXTIME( UNIX_TIMESTAMP() - 86400*10 ) LIMIT 100000");
     }
     LJ::disconnect_dbs();
 
