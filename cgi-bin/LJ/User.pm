6ea08d29e107a83264a0416a89acacb02e2a1028
madeon <madeon@livejournal.com>
2013-02-04 12:59:34 +0000

    LJSUP-14982: Clear cache of SC in the ratings on the moment of SC counting
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23636 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -2893,13 +2893,14 @@ sub get_social_capital {
         return $getter->($u);
     }
 
-    my $soc_capital = LJ::MemCache::get( $u->user."_soc_cap" );
+    my $key = $u->userid . ":sccap",;
+    my $soc_capital = LJ::MemCache::get( $key );
     if (defined $soc_capital) {
         return $soc_capital;
     } else {  
         $soc_capital = LJ::PersonalStats::DB->fetch_raw('ratings', {func => 'get_authority', journal_id => $u->userid}); 
         my $value = $soc_capital ? int($soc_capital->{result}->{authority}/1000) : return 0;
-        LJ::MemCache::set( $u->user."_soc_cap", $value, 60*60);
+        LJ::MemCache::set( $key, $valuccap, 60*60);
         return $value;
     }
 }
