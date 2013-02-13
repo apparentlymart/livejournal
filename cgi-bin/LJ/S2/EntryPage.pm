dreamwidth/dreamwidth

(Bug 3523) Turn on new JS implementation (jQuery) by default
- temporary opt-out still available just in case of last minute stuff
- reverse the text on the button if the beta feature key ends with "_optout"

cgi-bin/LJ/S2/EntryPage.pm
@@ -76,7 +76,7 @@ sub EntryPage
     $p->{head_content} .= LJ::canonical_link( $permalink, $get->{thread} );
 
     # quickreply js libs
-    my $beta = LJ::BetaFeatures->user_in_beta( $remote => "journaljquery" );
+    my $beta = ! LJ::BetaFeatures->user_in_beta( $remote => "journaljquery_optout4" );
     LJ::need_res( LJ::Talk::init_iconbrowser_js( $beta, $beta ? 'stc/jquery/jquery.ui.theme.smoothness.css' : 'stc/lj_base.css' ) )
         if $remote && $remote->can_use_userpic_select;
 

apparentlymart/livejournal

cgi-bin/LJ/S2/EntryPage.pm
@@ -76,7 +76,7 @@ sub EntryPage
     $p->{head_content} .= LJ::canonical_link( $permalink, $get->{thread} );
 
     # quickreply js libs
-    my $beta = LJ::BetaFeatures->user_in_beta( $remote => "journaljquery" );
+    my $beta = ! LJ::BetaFeatures->user_in_beta( $remote => "journaljquery_optout" );
 
     my @iconbrowser_extra_stylesheet;
     if ( $beta ) {