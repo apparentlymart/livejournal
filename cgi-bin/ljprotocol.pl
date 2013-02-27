d85dcdf32cc3e4c453439f0224cb7c3ba26f2fe8
madeon <madeon@livejournal.com>
2013-02-27 09:36:00 +0000

    LJSUP-15224: [internal] ESN: Invalid Event Notification Type
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23768 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljprotocol.pl
+++ cgi-bin/ljprotocol.pl
@@ -5803,10 +5803,10 @@ sub registerpush {
     my $u = $flags->{u};
 
     return fail($err, 200)
-        unless $u && $req->{platform} && $req->{deviceid};
+        unless $u && $req->{platform} && $req->{registrationid};
 
-    my $error = LJ::PushNotification->subscribe($u, $req); 
-    return fail($error, 412) if $error;
+    my $res = LJ::PushNotification->subscribe($u, $req) 
+        || return fail($err, 412);
 
     return { status => 'OK' }
 }
@@ -5830,8 +5830,10 @@ sub unregisterpush {
     return fail($err,200)
         unless $req->{platform};
 
-    my $error = LJ::PushNotification->unsubscribe($u, $req);
-    return $error if $error;
+    my $res = LJ::PushNotification->unsubscribe($u, $req);
+
+    return fail($err,200)
+        unless $res;
 
     return { status => 'OK' };
 }
@@ -5849,9 +5851,9 @@ sub pushsubscriptions {
             my $res = eval{ 
                 LJ::PushNotification->manage(
                     $u,
-                    app_name        => $req->{app_name},
                     platform        => $req->{platform},
                     deviceid        => $req->{deviceid},
+                    registrationid  => $req->{registrationid},
                     %$event,
                 )
             };
