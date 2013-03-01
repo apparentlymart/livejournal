66d7966f98c5e3e6fe56a1fcc02badefd5e4031a
Andrew Ilyin <jemmix@gmail.com>
2013-03-01 13:14:05 +0000

    LJSUP-15218 ([Internal] New site scheme parameter: logo_element)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23781 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/SiteScheme.pm
+++ cgi-bin/LJ/SiteScheme.pm
@@ -415,7 +415,7 @@ sub common_template_params {
         'error_list'         => $error_list,
         'warning_list'       => $warning_list,
         'breadcrumbs'        => $class->template_param_breadcrumbs,
-        'logo_element'       => $args->{'logo_element'},
+        'index_page'         => $args->{'index_page'},
         'chalresp_js'        => $chalresp_js,
         'after_bml_title'    => $after_bml_title,
         'show_mobile_link'   => $show_mobile_link,
