2f01e1042efcf198a0dc6df30c15574a817ae80c
Valerii Vasin <valerii.vasin@gmail.com>
2013-03-21 15:36:17 +0000

    LJSUP-15465: Extract community filter functionality from template. Add filters functionality for fetched control strip.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23883 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/controlstrip.js
+++ htdocs/js/controlstrip.js
@@ -1,55 +1,75 @@
-jQuery(function ($) {
-	if (!document.getElementById('lj_controlstrip') && !document.getElementById('lj_controlstrip_new')) {
-		$.get(LiveJournal.getAjaxUrl('controlstrip'), { user: Site.currentJournal }, function (data) {
-				$(data).appendTo(document.body).ljAddContextualPopup();
-			}
-		);
-	}
+;(function ($) {
+    'use strict';
 
-    // add placeholders for login, password and search fields
-    $('#lj_controlstrip_new input[placeholder]').labeledPlaceholder();
+    /**
+     * Add community filter functionality for control strip
+     */
+    function addFilterFunctionality() {
+        var bubble,
+            form,
+            input,
+            submit;
 
-    // community filter functionality
-    if ( Site.remoteUser ) {
-        (function () {
-            var bubble,
-                form,
-                input,
-                submit;
+        // filter is available only for logged in users
+        if ( !Site.remoteUser ) {
+            return;
+        }
 
-            bubble = $('#lj_controlstrip_new .w-cs-filter-inner');
+        bubble = $('#lj_controlstrip_new .w-cs-filter-inner');
 
-            // exit if filter content is not currently on the page
-            if ( bubble.length === 0 ) {
-                return;
+        // exit if filter content is not currently on the page
+        if ( bubble.length === 0 ) {
+            return;
+        }
+
+        form = $('#sortByPoster');
+        input = form.find('[name=poster]');
+        submit = form.find('[type=image]');
+
+        bubble.bubble({
+            target: '#lj_controlstrip_new .w-cs-filter-icon',
+            showOn: 'click',
+            closeControl: false
+        });
+
+        input.input(function () {
+            if( this.value.length ) {
+                submit.css('opacity', 1)
+                    .prop('disabled', false);
+            } else {
+                submit.css('opacity', 0)
+                    .prop('disabled', true);
             }
+        });
 
-            form = $('#sortByPoster');
-            input = form.find('[name=poster]');
-            submit = form.find('[type=image]');
-
-            bubble.bubble({
-                target: '#lj_controlstrip_new .w-cs-filter-icon',
-                showOn: 'click',
-                closeControl: false
-            });
-
-            input.input(function () {
-                if( this.value.length ) {
-                    submit.css('opacity', 1)
-                        .prop( 'disabled', false );
-                } else {
-                    submit.css('opacity', 0)
-                        .prop( 'disabled', true );
-                }
-            });
-
-            form.on('submit', function (e) {
-                if( !input.val().length ) {
-                    e.preventDefault();
-                }
-            });
-        }());
+        form.on('submit', function (e) {
+            if( !input.val().length ) {
+                e.preventDefault();
+            }
+        });
     }
 
-});
+    /**
+     * Add labled placeholders for the control strip
+     */
+    function addLabledPlaceholders() {
+        $('#lj_controlstrip_new input[placeholder]').labeledPlaceholder();
+    }
+
+    $(function () {
+        // load control strip if it's not available on document ready
+        // Notice: use cases are not clear
+        if (!document.getElementById('lj_controlstrip') && !document.getElementById('lj_controlstrip_new')) {
+            $.get(LiveJournal.getAjaxUrl('controlstrip'), { user: Site.currentJournal }, function (data) {
+                    $(data).appendTo(document.body).ljAddContextualPopup();
+                    addFilterFunctionality();
+                    addLabledPlaceholders();
+                }
+            );
+        } else {
+            addLabledPlaceholders();
+            addFilterFunctionality();
+        }
+    });
+
+}(jQuery));
