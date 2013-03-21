35f124a02c12e21dca3a619897de8b05b5827593
Valerii Vasin <valerii.vasin@gmail.com>
2013-03-21 14:24:55 +0000

    LJSUP-15465: Extract community filter functionality from template.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23877 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/controlstrip.js
+++ htdocs/js/controlstrip.js
@@ -3,6 +3,53 @@ jQuery(function ($) {
 		$.get(LiveJournal.getAjaxUrl('controlstrip'), { user: Site.currentJournal }, function (data) {
 				$(data).appendTo(document.body).ljAddContextualPopup();
 			}
-		);		
+		);
 	}
+
+    // add placeholders for login, password and search fields
+    $('#lj_controlstrip_new input[placeholder]').labeledPlaceholder();
+
+    // community filter functionality
+    if ( Site.remoteUser ) {
+        (function () {
+            var bubble,
+                form,
+                input,
+                submit;
+
+            bubble = $('#lj_controlstrip_new .w-cs-filter-inner');
+
+            // exit if filter content is not currently on the page
+            if ( bubble.length === 0 ) {
+                return;
+            }
+
+            form = $('#sortByPoster');
+            input = form.find('[name=poster]');
+            submit = form.find('[type=image]');
+
+            bubble.bubble({
+                target: '#lj_controlstrip_new .w-cs-filter-icon',
+                showOn: 'click',
+                closeControl: false
+            });
+
+            input.input(function () {
+                if( this.value.length ) {
+                    submit.css('opacity', 1)
+                        .prop( 'disabled', false );
+                } else {
+                    submit.css('opacity', 0)
+                        .prop( 'disabled', true );
+                }
+            });
+
+            form.on('submit', function (e) {
+                if( !input.val().length ) {
+                    e.preventDefault();
+                }
+            });
+        }());
+    }
+
 });
