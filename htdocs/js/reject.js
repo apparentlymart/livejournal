c9f8196ad1fcbce8b37caf4721c44307bd78b7f7
Valerii Vasin <valerii.vasin@gmail.com>
2013-03-25 12:44:50 +0000

    LJSV-2641: Checkbox doesn't work in Horizon and Vertigo. Replace indentation. Add use strict.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23899 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/reject.js
+++ htdocs/js/reject.js
@@ -1,98 +1,100 @@
 ;(function ($) {
-	$(function () {
-		rejectFormManager.init();
-	});
+    'use strict';
 
-	var rejectFormManager = (function () {
-		var config,
-			_container,
-			_hiddenWithIds,
-			_removeControls,
-			_userList,
-			_submitButton,
-			_returnLink;
+    $(function () {
+        rejectFormManager.init();
+    });
 
-		config = {
-			containerSelector: '.b-pending',
-			removeControlsSelector: '.i-pending-close',
-			textareaSelector: '.b-pending-reason',
-			hiddenWithIdsSelector: 'input[name=ids]',
-			userListSelector: '.b-pending-users',
-			submitButtonSelector: '.i-pending-reject',
-			returnLinkSelector: '.i-pending-returnlink'
-		};
+    var rejectFormManager = (function () {
+        var config,
+            _container,
+            _hiddenWithIds,
+            _removeControls,
+            _userList,
+            _submitButton,
+            _returnLink;
 
-		function findElems () {
-			_container = $(config.containerSelector);
-			_hiddenWithIds = _container.find(config.hiddenWithIdsSelector);
-			_userList = _container.find(config.userListSelector);
-			_removeControls = _userList.find(config.removeControlsSelector);
-			_submitButton = _container.find(config.submitButtonSelector);
-			_returnLink = _container.find(config.returnLinkSelector);
-		}
+        config = {
+            containerSelector: '.b-pending',
+            removeControlsSelector: '.i-pending-close',
+            textareaSelector: '.b-pending-reason',
+            hiddenWithIdsSelector: 'input[name=ids]',
+            userListSelector: '.b-pending-users',
+            submitButtonSelector: '.i-pending-reject',
+            returnLinkSelector: '.i-pending-returnlink'
+        };
 
-		function bindRemoveUser () {
-			_removeControls.bind('click', removeUser);
-		}
+        function findElems () {
+            _container = $(config.containerSelector);
+            _hiddenWithIds = _container.find(config.hiddenWithIdsSelector);
+            _userList = _container.find(config.userListSelector);
+            _removeControls = _userList.find(config.removeControlsSelector);
+            _submitButton = _container.find(config.submitButtonSelector);
+            _returnLink = _container.find(config.returnLinkSelector);
+        }
 
-		function removeUser (event) {
-			event.preventDefault();
+        function bindRemoveUser () {
+            _removeControls.bind('click', removeUser);
+        }
 
-			var currentControl = $(this),
-				elemToRemove = currentControl.closest('li'),
-				userId = currentControl.attr('id').replace(/\D+/g, ''),
-				userListLength = _userList.find('li').length;
+        function removeUser(event) {
+            var currentControl = $(this),
+                elemToRemove = currentControl.closest('li'),
+                userId = currentControl.attr('id').replace(/\D+/g, ''),
+                userListLength = _userList.find('li').length;
+
+            event.preventDefault();
 
             /*
-			if (userListLength === 1) {
-				return false;
-			}
+            if (userListLength === 1) {
+                return false;
+            }
             */
 
-			removeUserIdFromHidden(userId);
+            removeUserIdFromHidden(userId);
 
-			elemToRemove.remove();
+            elemToRemove.remove();
 
-			if (userListLength === 1) {
-				_returnLink.removeAttr('style');
-				_submitButton.attr('disabled', true);
-			} else {
-				removeUnwantedCommas();
-			}
-		}
+            if (userListLength === 1) {
+                _returnLink.removeAttr('style');
+                _submitButton.attr('disabled', true);
+            } else {
+                removeUnwantedCommas();
+            }
+        }
 
-		function removeUnwantedCommas () {
-			var lastUserElem,
-				contentWithoutCommas;
+        function removeUnwantedCommas () {
+            var lastUserElem,
+                contentWithoutCommas;
 
-			lastUserElem = _userList.find('li').last();
-			contentWithoutCommas = lastUserElem.html().replace(/\,/g, '');
-			lastUserElem
-				.html(contentWithoutCommas)
-				.find(config.removeControlsSelector)
-					.click(removeUser);
-		}
+            lastUserElem = _userList.find('li').last();
+            contentWithoutCommas = lastUserElem.html().replace(/\,/g, '');
+            lastUserElem
+                .html(contentWithoutCommas)
+                .find(config.removeControlsSelector)
+                    .click(removeUser);
+        }
 
-		function removeUserIdFromHidden (userId) {
-			var hiddenValue = _hiddenWithIds.val(),
-				regExpToCheck = new RegExp('(' + userId + ')' + '(\,)*', 'g'),
-				newHiddenValue = hiddenValue.replace(regExpToCheck, '');
+        function removeUserIdFromHidden (userId) {
+            var hiddenValue = _hiddenWithIds.val(),
+                regExpToCheck = new RegExp('(' + userId + ')' + '(\,)*', 'g'),
+                newHiddenValue = hiddenValue.replace(regExpToCheck, '');
 
-			_hiddenWithIds.val(newHiddenValue);
-		}
+            _hiddenWithIds.val(newHiddenValue);
+        }
 
-		function initPlaceholder () {
-			var textarea = _container.find(config.textareaSelector);
+        function initPlaceholder () {
+            var textarea = _container.find(config.textareaSelector);
 
-			textarea.placeholder();
-		}
+            textarea.placeholder();
+        }
 
-		return {
-			init : function () {
-				findElems();
-				bindRemoveUser();
-				initPlaceholder();
-			}
-		};
-	})();
-})(jQuery);
+        return {
+            init : function () {
+                findElems();
+                bindRemoveUser();
+                initPlaceholder();
+            }
+        };
+    })();
+}(jQuery));
