6bc8dbb23a6d2021dd763e56d8535774e92dba10
Valerii Vasin <valerii.vasin@gmail.com>
2013-03-25 12:31:38 +0000

    LJSV-2641: Checkbox doesn't work in Horizon and Vertigo. Prettify reject file.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23896 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/reject.js
+++ htdocs/js/reject.js
@@ -1,50 +1,56 @@
-(function ($) {
+;(function ($) {
 	$(function () {
 		rejectFormManager.init();
 	});
-	
+
 	var rejectFormManager = (function () {
-		var CONFIG = {
-			containerSelector : '.b-pending',
-			removeControlsSelector : '.i-pending-close',
-			textareaSelector : '.b-pending-reason',
-			hiddenWithIdsSelector : 'input[name=ids]',
-			userListSelector : '.b-pending-users',
-			submitButtonSelector : '.i-pending-reject',
-			returnLinkSelector : '.i-pending-returnlink'
+		var config,
+			_container,
+			_hiddenWithIds,
+			_removeControls,
+			_userList,
+			_submitButton,
+			_returnLink;
+
+		config = {
+			containerSelector: '.b-pending',
+			removeControlsSelector: '.i-pending-close',
+			textareaSelector: '.b-pending-reason',
+			hiddenWithIdsSelector: 'input[name=ids]',
+			userListSelector: '.b-pending-users',
+			submitButtonSelector: '.i-pending-reject',
+			returnLinkSelector: '.i-pending-returnlink'
 		};
-		
-		var _container, _hiddenWithIds, _removeControls, _userList;
-		
+
 		function findElems () {
-			_container = $(CONFIG.containerSelector);
-			_hiddenWithIds = _container.find(CONFIG.hiddenWithIdsSelector);
-			_userList = _container.find(CONFIG.userListSelector);
-			_removeControls = _userList.find(CONFIG.removeControlsSelector);
-			_submitButton = _container.find(CONFIG.submitButtonSelector);
-			_returnLink = _container.find(CONFIG.returnLinkSelector);
+			_container = $(config.containerSelector);
+			_hiddenWithIds = _container.find(config.hiddenWithIdsSelector);
+			_userList = _container.find(config.userListSelector);
+			_removeControls = _userList.find(config.removeControlsSelector);
+			_submitButton = _container.find(config.submitButtonSelector);
+			_returnLink = _container.find(config.returnLinkSelector);
 		}
-		
+
 		function bindRemoveUser () {
 			_removeControls.bind('click', removeUser);
 		}
-		
+
 		function removeUser (event) {
 			event.preventDefault();
-			
+
 			var currentControl = $(this),
 				elemToRemove = currentControl.closest('li'),
 				userId = currentControl.attr('id').replace(/\D+/g, ''),
 				userListLength = _userList.find('li').length;
-				
+
             /*
 			if (userListLength === 1) {
 				return false;
 			}
             */
-				
+
 			removeUserIdFromHidden(userId);
-			
+
 			elemToRemove.remove();
 
 			if (userListLength === 1) {
@@ -54,33 +60,33 @@
 				removeUnwantedCommas();
 			}
 		}
-		
+
 		function removeUnwantedCommas () {
-			var userListLength = _userList.find('li').length,
-				lastUserElem, lastUserContent;
-			
+			var lastUserElem,
+				contentWithoutCommas;
+
 			lastUserElem = _userList.find('li').last();
 			contentWithoutCommas = lastUserElem.html().replace(/\,/g, '');
 			lastUserElem
 				.html(contentWithoutCommas)
-				.find(CONFIG.removeControlsSelector)
+				.find(config.removeControlsSelector)
 					.click(removeUser);
 		}
-		
+
 		function removeUserIdFromHidden (userId) {
 			var hiddenValue = _hiddenWithIds.val(),
 				regExpToCheck = new RegExp('(' + userId + ')' + '(\,)*', 'g'),
 				newHiddenValue = hiddenValue.replace(regExpToCheck, '');
-			
+
 			_hiddenWithIds.val(newHiddenValue);
 		}
-		
+
 		function initPlaceholder () {
-			var textarea = _container.find(CONFIG.textareaSelector);
-			
-			textarea.placeholder(); 
+			var textarea = _container.find(config.textareaSelector);
+
+			textarea.placeholder();
 		}
-		
+
 		return {
 			init : function () {
 				findElems();
