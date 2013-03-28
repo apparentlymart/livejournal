c7db5e0bc820bbf89b08573049e66bb8c6eb61e7
Victor Troitsky <vtroitsky@livejournal.com>
2013-03-28 15:13:50 +0000

    LJSUP-15517: [internal] Upload authority into global Redis from XC servers (create upload utility)
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23938 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/User.pm
+++ cgi-bin/LJ/User.pm
@@ -2821,7 +2821,7 @@ sub get_social_capital {
         # TODO: Check the date of social capital
         my $redis = LJ::Redis->get_connection;
         if ($redis) {
-            my $authority = $redis->get('authority'.$u->userid) || 0;
+            my $authority = $redis->get('authority.'.$u->userid) || 0;
             warn "Redis authority (".$u->userid."): $authority";
             $soc_capital = int($authority / 1000);
         }
