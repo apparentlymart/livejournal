8b4416015d721cc62fd34374e8597416b2ef1231
Natalya Marfina <nikulochkina@livejournal.com>
2013-04-05 14:44:53 +0000

    LJSUP-15552: Privacy level of repost
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@23974 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/ljprotocol.pl
+++ cgi-bin/ljprotocol.pl
@@ -2414,11 +2414,13 @@ sub postevent {
 
     ## if newpost_minsecurity is set, new entries have to be
     ## a minimum security level
-    $security = "private"
-        if $uowner->newpost_minsecurity eq "private";
-    ($security, $qallowmask) = ("usemask", 1)
-        if $uowner->newpost_minsecurity eq "friends"
-        and $security eq "public";
+    unless ($flags->{'entryrepost'}) {
+        $security = "private"
+            if $uowner->newpost_minsecurity eq "private";
+        ($security, $qallowmask) = ("usemask", 1)
+            if $uowner->newpost_minsecurity eq "friends"
+            and $security eq "public";
+    }
 
     my $qsecurity = $dbh->quote($security);
 
