bbb7b1b4759c4cafd21db9998da8ac41f24bb922
Andrew Kanashin <holydrew@gmail.com>
2013-05-07 11:21:38 +0000

    LJSUP-15566: Hash tags for twitter
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@24132 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Share.pm
+++ cgi-bin/LJ/Share.pm
@@ -14,47 +14,47 @@ use warnings;
 use LJ::JSON;
 use LJ::Text;
 
-my $services = {
-    'livejournal' => {
-        'bindLink' => $LJ::SITEROOT . '/update.bml?repost_type=c&repost={url}'
-    },
-    'facebook' => {
-        'bindLink' => 'http://www.facebook.com/sharer.php?u={url}'
-    },
-    'twitter' => {
-        'bindLink' => 'http://twitter.com/share?url={url}&text={title}&hashtags={hashtags}'
-    },
-    'vkontakte' => {
-        'bindLink' => 'http://vkontakte.ru/share.php?url={url}'
-    },
-    'moimir' => {
-        'bindLink' => 'http://connect.mail.ru/share?url={url}'
-    },
-    'stumbleupon' => {
-        'bindLink' => 'http://www.stumbleupon.com/submit?url={url}'
-    },
-    'digg' => {
-        'bindLink' => 'http://digg.com/submit?url={url}'
-    },
-    'email' => {
-        'bindLink' => 'http://api.addthis.com/oexchange/0.8/forward/email/offer?username=internal&url={url}&title={title}'
-    },
-    'tumblr' => {
-        'bindLink' => 'http://www.tumblr.com/share/link?url={url}&name={title}&description={text}'
-    },
-    'odnoklassniki' => {
-        'bindLink' => 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl={url}'
-    }
-};
-
 sub request_resources {
     my ( $class, %opts ) = @_;
 
     return if $LJ::REQ_GLOBAL{'sharing_resources_requested'}++;
 
+    my $services = {
+        'livejournal' => {
+            'bindLink' => $LJ::SITEROOT . '/update.bml?repost_type=c&repost={url}',
+            'openInTab'=> 1,
+        },
+        'facebook' => {
+            'bindLink' => 'http://www.facebook.com/sharer.php?u={url}'
+        },
+        'twitter' => {
+            'bindLink' => 'http://twitter.com/share?url={url}&text={title}&hashtags={hashtags}'
+        },
+        'vkontakte' => {
+            'bindLink' => 'http://vkontakte.ru/share.php?url={url}'
+        },
+        'moimir' => {
+            'bindLink' => 'http://connect.mail.ru/share?url={url}'
+        },
+        'stumbleupon' => {
+            'bindLink' => 'http://www.stumbleupon.com/submit?url={url}'
+        },
+        'digg' => {
+            'bindLink' => 'http://digg.com/submit?url={url}'
+        },
+        'email' => {
+            'bindLink' => 'http://api.addthis.com/oexchange/0.8/forward/email/offer?username=internal&url={url}&title={title}'
+        },
+        'tumblr' => {
+            'bindLink' => 'http://www.tumblr.com/share/link?url={url}&name={title}&description={text}'
+        },
+        'odnoklassniki' => {
+            'bindLink' => 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl={url}'
+        }
+    };
+
     while ( my ( $name, $service ) = each %$services ) {
         $service->{'title'} ||= LJ::Lang::ml("sharing.service.$name");
-        $service->{'openInTab'} = 1 if $name eq 'livejournal';
     }
 
     my $params = {
