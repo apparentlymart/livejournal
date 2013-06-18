2ba0d4419b708950c64d12b5fc34bea6f839692a
vadvs <vadvs@livejournal.com>
2013-06-18 11:30:07 +0000

    LJSUP-16093: [internal] make bin/web_servers_uptime.pl output more informative
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@24317 1dfba125-df10-0410-8a77-af1a8d6d074f

--- bin/web_servers_uptime.pl
+++ bin/web_servers_uptime.pl
@@ -5,6 +5,7 @@ use HTTP::Request;
 use Getopt::Long;
 use lib "$ENV{LJHOME}/cgi-bin";
 use LJ::AdminTools qw(get_hosts_from_varnish_file);
+use Time::HiRes qw//;
 
 my $usage = <<"USAGE";
 $0 - script to check uptime of servers
@@ -27,13 +28,18 @@ my @servers = (@ARGV)
     ? @ARGV 
     : get_hosts_from_varnish_file( $vcl_file || "$ENV{LJHOME}/etc/production.vcl");
 
+my $now = time;
+
 print "Server -> Uptime\n";
 foreach my $server (@servers){
     chomp $server;
     print "$server -> ";
-    if (my $uptime = get_uptime($server)) {
+    my $t0 = [Time::HiRes::gettimeofday()];
+    my $uptime = get_uptime($server);
+    my $req_duration = Time::HiRes::tv_interval($t0);
+    if ($uptime) {
         my $str = localtime($uptime);
-        print "$uptime ($str)";
+        print "$uptime ($str), i.e. " . ($now - $uptime) . " sec, got in $req_duration sec";
     }
     print "\n";
 }
