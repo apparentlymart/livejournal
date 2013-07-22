32d028b74a0d2643698cea778880a72b2ac3ad81
klosev <klosev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2013-07-22 08:10:24 +0000

    LJSV-2738: Embeding of www.google.com/maps
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@24459 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Maps.pm
+++ cgi-bin/LJ/Maps.pm
@@ -20,7 +20,7 @@ sub expand_ljmap_tag {
             my $iframe_url;
             my $width = LJ::ehtml($attr->{'width'}) || 500;
             my $height = LJ::ehtml($attr->{'height'}) || 350;
-            if ($host =~ /^maps\.google\.\w+$/) {
+            if ($host =~ /^(maps|www)\.google\.\w+$/) {
                 $iframe_url = $uri->clone;
                 $url_params{'output'} = 'embed';
                 $iframe_url->query_form(%url_params);
