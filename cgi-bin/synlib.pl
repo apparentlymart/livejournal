ccf438332d8b4abdcd9f5f067ae7e6091b699bbc
klosev <klosev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2013-08-09 10:08:57 +0000

    LJSUP-16321: Ratings Redesign
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@24518 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/synlib.pl
+++ cgi-bin/synlib.pl
@@ -4,6 +4,60 @@
 package LJ::Syn;
 use strict;
 
+#get_feeds_rating ( { offset => '', length => '', sort_by => '', search_name => '' } )
+sub get_feeds_rating_segment {
+    my $args = shift;
+    my $offset = $args->{"offset"} || 0;
+    my $length = $args->{"length"} || 5;
+    my $sort_by = $args->{"sort_by"} || "soccap";
+
+    ##### Search mode if set search_name parameter ####
+    my $search_id;
+    my $search_name = $args->{"search_name"};
+    if ($search_name) {
+        my $user = LJ::load_user($search_name);
+        $search_id = $user->id() if LJ::isu($user);
+    }
+    ###################################################
+
+    my @data = @{_get_feeds_from_db()};
+    my $total = scalar @data;
+
+    my @uids = map { $_->[0]} @data;
+    my $users = LJ::User->get_authority_multi( \@uids );
+    foreach (@data) {
+        $_->[3] = $users->{$_->[0]};
+    }
+
+    ###### by default values sorting by number of watchers, if we are need sorting by social capital then do it.
+    @data = reverse sort { $a->[3] <=> $b->[3] } @data if $sort_by eq "soccap";
+
+    my $search_res;
+    my $position = 1;
+    foreach (@data) {
+        $_ = {
+            position => $position,
+            journal_id => $_->[0],
+            xml_url => $_->[1],
+            watchers => $_->[2],
+            soccap => $_->[3],
+        };
+        $position++;
+
+        ##### in search mode we are trying find needed position
+        if ( $_->{"journal_id"} == $search_id ) {
+            $search_res = $_->{"position"};
+            $_->{"found"} = 1;
+            $offset = POSIX::floor( $search_res / $length ) * $length;
+        }
+    }
+
+    my @data = splice(@data, $offset, $length);
+
+    my $result = \@data;
+    return { total => $total, data => $result, search_res => $search_res };
+}
+
 sub get_popular_feeds
 {
     my $popsyn = LJ::MemCache::get("popsyn");
