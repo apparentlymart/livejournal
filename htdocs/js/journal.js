d4e7fc80a1a2bc56faf431fcdc94834ac0a26fba
Valerii Vasin <valerii.vasin@gmail.com>
2013-08-13 09:34:32 +0000

    LJSUP-16470: [Internal] Remove old LJShare functionality from JavaScript. Apply code style.
    git-svn-id: http://code.livejournal.org/svn/livejournal/trunk@24540 1dfba125-df10-0410-8a77-af1a8d6d074f

--- htdocs/js/journal.js
+++ htdocs/js/journal.js
@@ -2,182 +2,210 @@
 
 DonateButton = {
   buyMore: function(node, ml_message, event) {
-  var bubble = jQuery(node).data("buyMoreCachedBubble");
-  if (!bubble) {
-    bubble = jQuery("<span>" + ml_message + "</span>").bubble({
-    target: node
-    });
-    jQuery(node).data("buyMoreCachedBubble", bubble);
-  }
+    var bubble = jQuery(node).data('buyMoreCachedBubble');
 
-  bubble.bubble("show");
-  event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
-  return false;
+    if (!bubble) {
+      bubble = jQuery('<span>' + ml_message + '</span>').bubble({
+        target: node
+      });
+
+      jQuery(node).data('buyMoreCachedBubble', bubble);
+    }
+
+    bubble.bubble('show');
+
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    } else {
+       event.cancelBubble = true;
+    }
+
+    return false;
   },
 
   donate: function( link, url_data, event ) {
-  var url = link.href,
-    origin, h;
+    var width = 639,
+        height = 230,
+        url = link.href,
+        popupUrl,
+        h;
 
-  var width = 639;
-  var height = 230;
 
-  LJ.rpc.bind( function( ev ) {
-    if( ev.origin && ev.origin != Site.siteroot ) {
-    return;
-    }
-
-    if( ev.data && ev.data.message === "updateWallet" ) {
-    LiveJournal.run_hook( 'update_wallet_balance' );
-    jQuery.getJSON( LiveJournal.getAjaxUrl( 'give_tokens' ) + "?" + url_data + "&mode=js",
-      function( result ) {
-      if( result.html ) {
-        $node = jQuery( link ).closest( '.lj-button' );
-        $node.replaceWith( result.html );
+    LJ.rpc.bind(function(ev) {
+      if ( ev.origin && ev.origin !== Site.siteroot ) {
+        return;
       }
-      } );
+
+      if ( ev.data && ev.data.message === 'updateWallet' ) {
+        LiveJournal.run_hook( 'update_wallet_balance' );
+        jQuery.getJSON(
+          LiveJournal.getAjaxUrl('give_tokens') + '?' + url_data + '&mode=js',
+          function (result) {
+            var $node;
+
+            if ( result.html ) {
+              $node = jQuery( link ).closest( '.lj-button' );
+              $node.replaceWith( result.html );
+            }
+          }
+        );
+      }
+    });
+
+    popupUrl = url + ( url.indexOf( '?' ) === -1 ? '?' : '&' ) + 'usescheme=nonavigation';
+    h = window.open( 'about:blank', 'donate' , 'toolbar=0,status=0,width=' + width + ',height=' + height + ',scrollbars=yes,resizable=yes');
+    h.name = location.href.replace( /#.*$/, '' );
+
+    setTimeout(function() {
+      LJ.rpc.initRecipient( h, popupUrl, location.href.replace( /#.*$/, '' ) );
+    }, 0);
+
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    } else {
+      event.cancelBubble = true;
     }
-  } );
 
-  var popupUrl = url + ( url.indexOf( "?" ) === -1 ? "?" : "&" ) + 'usescheme=nonavigation';
-  h = window.open( 'about:blank', 'donate' , 'toolbar=0,status=0,width=' + width + ',height=' + height + ',scrollbars=yes,resizable=yes');
-  h.name = location.href.replace( /#.*$/, '' );
-
-  setTimeout( function() {
-    LJ.rpc.initRecipient( h, popupUrl, location.href.replace( /#.*$/, '' ) );
-  }, 0 );
-
-  event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
-  return false;
+    return false;
   }
 };
 
 (function() {
   var options = {
-  blockSelector: '.yota-contest'
+    blockSelector: '.yota-contest'
   };
 
   function retrieveContestInfo( element ) {
-  var journal = element.attr( 'data-user' );
-  jQuery.getJSON( LiveJournal.getAjaxUrl( 'yota_widget_post' ),
-      { json: 1, journal: journal }, function( answer ) {
-    if( 'collected' in answer ) {
-    var collected = answer.collected;
-    for( var i = 0; i < collected.length; i += 2 ) {
-      element.find( "." + collected[ i ] ).html( collected[ i + 1 ] );
-    }
-    }
+    var journal = element.attr( 'data-user' );
+    jQuery.getJSON(
+      LiveJournal.getAjaxUrl('yota_widget_post'),
+      { json: 1, journal: journal },
+      function (answer) {
+        var collected,
+            key,
+            i;
 
-    if( 'rating' in answer ) {
-    i = 5;
-    var key;
-    while( --i > 0) {
-      key = "" + i;
-      if( key in answer.rating ) {
-      element.find( '.c' + i ).html( answer.rating[ key ] );
+        if ('collected' in answer) {
+          collected = answer.collected;
+          for (i = 0; i < collected.length; i += 2) {
+            element.find( '.' + collected[ i ] ).html( collected[ i + 1 ] );
+          }
+        }
+
+        if ('rating' in answer ) {
+          i = 5;
+
+          while( --i > 0) {
+            key = '' + i;
+            if( key in answer.rating ) {
+              element.find( '.c' + i ).html( answer.rating[ key ] );
+            }
+          }
+        }
       }
-    }
-    }
-  } );
+    );
   }
 
   function findElement() {
-  var element = jQuery( options.blockSelector );
+    var element = jQuery( options.blockSelector );
 
-  if( element.length ) {
-    element.each( function() {
-    retrieveContestInfo( jQuery( this ) );
-    } );
-  }
-  }
-
-  jQuery(function() {
-  findElement();
-  });
-})();
-
-(function() {
-  var storage = {
-  init: function() {
-    this._store = jQuery.storage.getItem('placeholders') || {};
-  },
-
-  inStorage: function(link) {
-    return this._store.hasOwnProperty(link);
-  },
-
-  addUrl: function(link) {
-    if (!this.inStorage(link)) {
-    this._store[link] = true;
-    jQuery.storage.setItem('placeholders', this._store);
+    if( element.length ) {
+      element.each(function() {
+        retrieveContestInfo( jQuery( this ) );
+      });
     }
   }
+
+  jQuery(findElement);
+}());
+
+(function () {
+  var storage = {
+    init: function() {
+      this._store = jQuery.storage.getItem('placeholders') || {};
+    },
+
+    inStorage: function(link) {
+      return this._store.hasOwnProperty(link);
+    },
+
+    addUrl: function(link) {
+      if ( !this.inStorage(link) ) {
+        this._store[link] = true;
+        jQuery.storage.setItem('placeholders', this._store);
+      }
+    }
   };
 
   storage.init();
 
   var placeholders = {
-  image: {
-    selector: '.b-mediaplaceholder-photo',
-    loading: 'b-mediaplaceholder-processing',
-    init: function() {
-    var self = this;
-    doc.on('click', this.selector, function(ev) {
-      self.handler(this, ev);
-    });
+    image: {
+      selector: '.b-mediaplaceholder-photo',
+      loading: 'b-mediaplaceholder-processing',
+      init: function() {
+        var self = this;
+        doc.on('click', this.selector, function(ev) {
+          self.handler(this, ev);
+        });
+      },
+
+      handler: function(el, html) {
+        var im = new Image();
+
+        im.onload = im.onerror = jQuery.delayedCallback(this.imgLoaded.bind(this, el, im), 500);
+        im.src = el.href;
+        el.className += ' ' + this.loading;
+
+        storage.addUrl(el.href);
+      },
+
+      imgLoaded: function(el, image) {
+        var img = jQuery('<img />').attr('src', image.src),
+            $el = jQuery(el),
+            href = $el.data('href'),
+            imw = $el.data('width'),
+            imh = $el.data('height');
+
+        if (imw) {
+          img.width(imw);
+        }
+
+        if (imh) {
+          img.height(imh);
+        }
+
+        if (href && href.length > 0) {
+          img = jQuery('<a>', { href: href }).append(img);
+          $el.next('.b-mediaplaceholder-external').remove();
+        }
+
+        $el.replaceWith(img);
+      }
     },
 
-    handler: function(el, html) {
-    var im = new Image();
-
-    im.onload = im.onerror = jQuery.delayedCallback(this.imgLoaded.bind(this, el, im), 500);
-    im.src = el.href;
-    el.className += ' ' + this.loading;
-
-    storage.addUrl(el.href);
-    },
-
-    imgLoaded: function(el, image) {
-    var img = jQuery('<img />').attr('src', image.src),
-      $el = jQuery(el),
-      href = $el.data('href'),
-      imw = $el.data('width'),
-      imh = $el.data('height');
-
-    if (imw) { img.width(imw); }
-    if (imh) { img.height(imh); }
-
-    if (href && href.length > 0) {
-      img = jQuery('<a>', { href: href }).append(img);
-      $el.next('.b-mediaplaceholder-external').remove();
+    video: {
+      handler: function(link, html) {
+        link.parentNode.replaceChild(jQuery(unescape(html))[0], link);
+      }
     }
-
-    $el.replaceWith(img);
-    }
-  },
-
-  video: {
-    handler: function(link, html) {
-    link.parentNode.replaceChild(jQuery(unescape(html))[0], link);
-    }
-  }
   };
   // use replaceChild for no blink scroll effect
 
   // Placeholder onclick event
   LiveJournal.placeholderClick = function(el, html) {
-  var type = (html === 'image') ? html : 'video';
+    var type = (html === 'image') ? html : 'video';
 
-  placeholders[type].handler(el, html);
-  return false;
+    placeholders[type].handler(el, html);
+    return false;
   };
 
   LiveJournal.register_hook('page_load', function() {
-  jQuery('.b-mediaplaceholder').each(function() {
-    if(storage.inStorage(this.href)) {
-    this.onclick.apply(this);
-    }
-  });
+    jQuery('.b-mediaplaceholder').each(function() {
+      if (storage.inStorage(this.href)) {
+        this.onclick.apply(this);
+      }
+    });
   });
 })();
 
@@ -281,7 +309,7 @@ LiveJournal.register_hook('page_load', function () {
     var body = $('body');
     convertGists( body );
     body.on('ljcutshow', function(event, ui) {
-        convertGists($(event.target).next(), true );
+      convertGists($(event.target).next(), true );
     });
   });
 
